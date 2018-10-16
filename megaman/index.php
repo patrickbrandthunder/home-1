90719e49f5471fab6d72e76899293a7afdd8a093
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-16 11:55:44 -0400

    Update index.php

--- megaman/index.php
+++ megaman/index.php
@@ -1,5 +1,5 @@
 <?php
-$title = 'Mega Man';
+$title = 'Mega Man New Tab Theme';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = '';
 $tid = basename(__DIR__);
