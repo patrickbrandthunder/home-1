d4d03b8f429c779ba0bff3b772bb0b4d2c330cba
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-17 21:18:45 -0400

    Update index.php

--- vicmensa/index.php
+++ vicmensa/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Vic Mensa';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ipeipoahdlfflpkongkchbbcngmdjpof';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
