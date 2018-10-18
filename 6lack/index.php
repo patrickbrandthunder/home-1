7575b101b3a36b60f03899d6645a71ebaa77f26e
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-18 17:50:14 -0400

    Update index.php

--- 6lack/index.php
+++ 6lack/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = '6lack';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bkeegckpgepjcnbimenmhmdhphhodlli';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
