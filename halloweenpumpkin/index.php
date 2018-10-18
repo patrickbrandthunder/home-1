c5ba127c7235bc3ce16b20f98c5fc96669f0901d
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-17 20:14:23 -0400

    Update index.php

--- halloweenpumpkin/index.php
+++ halloweenpumpkin/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Halloween Pumpkin';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ohbefnkhbefalongdlddjnbenplbalck';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
