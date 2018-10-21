ef4c8ed689e79f737c0636b0d660d253604f9235
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-21 11:39:05 -0400

    Update index.php

--- supermarioparty/index.php
+++ supermarioparty/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Super Mario Party';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jmepnmmeajangmmoppbklkekffdndogi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
