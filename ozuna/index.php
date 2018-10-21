9f7b0fa0b884a249ccd3372e34fa9e866fbff1f0
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-21 11:38:36 -0400

    Update index.php

--- ozuna/index.php
+++ ozuna/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Ozuna';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nimfhmnjbdegbdekckjmkcanoaipebfi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
