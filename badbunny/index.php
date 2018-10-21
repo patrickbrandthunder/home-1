c9ef17a35065498def122e67eafeeaa910e8d25c
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-21 08:08:22 -0400

    Update index.php

--- badbunny/index.php
+++ badbunny/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bad Bunny';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lkpelcmfecdkgnpejgnlkgamamhcacae';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
