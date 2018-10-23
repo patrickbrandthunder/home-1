fdb7a032113ccc1f22db7827f1e0e07ac71caeb1
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-23 10:38:43 -0400

    Update index.php

--- beberexha/index.php
+++ beberexha/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bebe Rexha';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'abgokegdfdedicloccdndgffkpigildm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
