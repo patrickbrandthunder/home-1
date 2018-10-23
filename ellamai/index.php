cf98b39f5c6df5b33d46e7575dc76dc06874d8e1
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-23 16:48:58 -0400

    Update index.php

--- ellamai/index.php
+++ ellamai/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Ella Mai';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'eckjcfgpcmpglnpdcjhhbmpkcjihbfpd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
