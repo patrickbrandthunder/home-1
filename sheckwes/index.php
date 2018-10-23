ba77578a293411df8c5d0220c29469cfc85e5210
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-23 18:28:49 -0400

    Update index.php

--- sheckwes/index.php
+++ sheckwes/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Sheck Wes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'knechnphkjimebjofjlclmaeccedgoko';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
