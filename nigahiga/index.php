48ea9ae31563d09d7e9ebd45e3b4bad2dcaad2c4
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-24 09:09:57 -0400

    Update index.php

--- nigahiga/index.php
+++ nigahiga/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'NigaHiga';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dalpnmmdkfmljlgodlaikgebenbfljip';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
