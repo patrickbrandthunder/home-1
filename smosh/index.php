9513c50c455366b357d4be14eccb282f95558803
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-24 11:17:08 -0400

    Update index.php

--- smosh/index.php
+++ smosh/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Smosh';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nifiegmlpibelpaelnlgeaiakdpianjo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
