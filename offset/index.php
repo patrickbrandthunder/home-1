ea5f75a53245c65e31f66b9116d5e810ab404948
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-24 16:24:31 -0400

    Update index.php

--- offset/index.php
+++ offset/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Offset';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mcghkmcbogkcdcejleipkoellilpbdcb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
