e0ffc4ff0e79353e7327af6de7374ca20b440d1b
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-25 10:08:38 -0400

    Update index.php

--- billieellish/index.php
+++ billieellish/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Billie Ellish';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cibkfmfhbclcdnkmggpalgbfhlcpampo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
