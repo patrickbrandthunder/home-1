20a311910442158f1976666e62c37b3484c3d075
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-26 15:15:40 -0400

    Update index.php

--- hauntinghillhouse/index.php
+++ hauntinghillhouse/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'The Haunting of Hill House';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jeblimegiepngahjdddfpgofjijipdge';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
