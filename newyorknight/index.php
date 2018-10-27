426a6452d94a5bedc06ba5effc9017f1d0837971
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-27 18:08:11 -0400

    Update index.php

--- newyorknight/index.php
+++ newyorknight/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'New York Night';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jmaggaojpmelenchamoiacnmfcibpbnp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
