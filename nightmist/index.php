6ed82b509f2f5aca6bb4c61a085da0cdaf436ce2
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-28 08:11:34 -0400

    Update index.php

--- nightmist/index.php
+++ nightmist/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'NIght Mist';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'glepffbjcojcibkekhmmpgbbeboomldh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
