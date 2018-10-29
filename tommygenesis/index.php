0c3ae697c25b7178ffb2c015bb25e29cea20a351
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-28 21:29:48 -0400

    Update index.php

--- tommygenesis/index.php
+++ tommygenesis/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Tommy Genesis';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'oelkpclgcjohgjcgpngbccjegochkfcd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
