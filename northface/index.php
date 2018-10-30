2424f0b7a7d9a53085efc8bc4a634e8faf085e69
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-29 21:16:10 -0400

    Update index.php

--- northface/index.php
+++ northface/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'NorthFace';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ggbphacppaeopohfddmknjmefmnoaajg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
