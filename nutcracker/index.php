5c2e1f56a81db7c6f8549c65ebefe4d55f695269
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-31 11:02:41 -0400

    Update index.php

--- nutcracker/index.php
+++ nutcracker/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Nutcracker and the Four Realms';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mpklihccjambmenlnanonobepofbkdpf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
