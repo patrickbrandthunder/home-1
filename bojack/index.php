3a3f5fd2c46e666b96b10155b16a5d3e57c8e6a6
pjmurphy777 <pjmurphy77@gmail.com>
2018-10-31 11:09:23 -0400

    Update index.php

--- bojack/index.php
+++ bojack/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bojack Horseman';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jkjhpahklppniieamejogmhoofdkjbmf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
