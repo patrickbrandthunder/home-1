0c56b19439cd8820b005b771dc04db91d51727dd
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-01 13:55:50 -0400

    Update index.php

--- jeremih/index.php
+++ jeremih/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Jeremih';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cgomjkfgkploalgggajkilpbemndhpne';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
