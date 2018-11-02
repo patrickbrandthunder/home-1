6923787a5a10990852397903481e4e1e8701ae7e
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-01 20:41:47 -0400

    Update index.php

--- ralphlauren/index.php
+++ ralphlauren/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Polo Ralph Lauren';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'panoaicpckcbcnlbmlofahkgebjpcpbi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
