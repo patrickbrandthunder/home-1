340ee0e0b71e9866b97e8e2bb67d36cabb59f9b6
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-02 09:22:03 -0400

    Update index.php

--- chive/index.php
+++ chive/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Chive';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ncdcokbohhjeedhjdiahcliceplianij';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
