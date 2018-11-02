1933b4d3f099e32da7b959f6a4956d48a7ac9613
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-01 20:30:14 -0400

    Update index.php

--- creed/index.php
+++ creed/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Creed';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fjonchnfdlioiamiijggafaabkheddkb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
