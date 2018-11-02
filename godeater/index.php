a50297bff9525c3d1bdfc86292f779868ebec8da
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-01 21:10:49 -0400

    Update index.php

--- godeater/index.php
+++ godeater/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'God Eater';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ldpcldncpdibljgfnfhjbmkhnnimpjja';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
