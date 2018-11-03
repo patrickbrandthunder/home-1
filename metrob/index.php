0f76ae9e44b3e4ffdb00824947b7f9e6f0df4374
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-03 17:11:59 -0400

    Update index.php

--- metrob/index.php
+++ metrob/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Metro Boomin';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'eifhalppalgjhejoppnegahcfjdblgof';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
