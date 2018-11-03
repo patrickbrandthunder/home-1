827076ef98e2bf60bacb4bfe380eb995c242e6d9
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-02 22:07:35 -0400

    Update index.php

--- lilbaby/index.php
+++ lilbaby/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Lil Baby';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'odiclklhajmedkecnmpmejhmiklmjeia';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
