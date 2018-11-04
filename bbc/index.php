fc79f709f2a8fccec687ed281a7aef2603f3aea9
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-04 15:02:41 -0500

    Update index.php

--- bbc/index.php
+++ bbc/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Billionaire Boys Club';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ibjlchijnhoohefdfoecmmdodphknfhi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
