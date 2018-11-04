ad3e2e2f0f3e3043541fe216fc60c385b6a89fa1
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-04 15:26:49 -0500

    Update index.php

--- cstapleton/index.php
+++ cstapleton/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Chris Stapleton';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'johllcfbaigmgbbndbpmadigdbboplki';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
