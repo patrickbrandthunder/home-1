5298bce03ed91dbbb565c098eab91770d92910c1
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-04 08:58:17 -0500

    Update index.php

--- princessnokia/index.php
+++ princessnokia/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Princess Nokia';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hiadfdcmhimolajiiellgahghdnggiff';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
