7c4150ac48bb3bf36e80508692f58341925f691a
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-05 09:52:35 -0500

    Update index.php

--- chronicles/index.php
+++ chronicles/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Chronicles of Narnia';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ihmijmhbhfdhfdlncpbdpjbbhlofhgci';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
