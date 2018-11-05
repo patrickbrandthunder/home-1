ce11e7c2144abcdee0c11ed781bf58f508be0b8c
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-05 10:41:55 -0500

    Update index.php

--- poppy/index.php
+++ poppy/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Poppy';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gobjondncilpjbbckohfmpgdijicepaj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
