745114258e86eb92d7840ce36996212116478bee
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-06 14:06:48 -0500

    Update index.php

--- thegoodplace/index.php
+++ thegoodplace/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'The Good PLace';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jcfofijkcgkpgimpjehaopiafndppoao';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
