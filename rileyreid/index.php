2b135f4212f820c0201136b265fe75a43ac1e0b1
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-06 07:29:50 -0500

    Update index.php

--- rileyreid/index.php
+++ rileyreid/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Riley Reid';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pcjpkacdpblobbaegkpicahdjeoebhjm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
