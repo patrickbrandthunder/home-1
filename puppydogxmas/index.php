28cf586b3122f6cc060831cee47247ce9eb48d3e
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-05 22:07:21 -0500

    Create index.php

---
+++ puppydogxmas/index.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'Puppy and Dog XMAS';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
