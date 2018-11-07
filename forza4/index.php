5ca6c147b1508c311bb6bab7e23ef851f5a21b83
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-06 21:16:58 -0500

    Update index.php

--- forza4/index.php
+++ forza4/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Forza 4';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kcmlijkmaijfaahncbaclhecepmoclkl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
