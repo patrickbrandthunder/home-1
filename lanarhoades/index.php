563be11663dee0aaf10ebdcfe648c47bf30b4f85
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-06 20:06:13 -0500

    Update index.php

--- lanarhoades/index.php
+++ lanarhoades/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Lana Rhoades';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'opccmpfmcjghkdpdjlfnedimkfffpmcn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
