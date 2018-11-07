bc8f542a2ce09180bc27866b0f99442f089a3228
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-07 08:35:52 -0500

    Update index.php

--- yg/index.php
+++ yg/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'YG';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'idjmmpaooacmoajkmfncebkimjoeamkl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
