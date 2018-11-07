12ad363009d6d0f346b5646dce0fb7225c749142
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-06 20:49:02 -0500

    Update index.php

--- blackout/index.php
+++ blackout/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'COD Blackout';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ciphfbajiaakopdeklldinhglgjefeji';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
