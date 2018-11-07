385ffb955c6a7bc7290bd9894eca3e20f5f5c649
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-07 12:46:57 -0500

    Update index.php

--- yellabeezy/index.php
+++ yellabeezy/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Yella Beezy';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hfengnhhehahokikjpcohpamdbejlpbi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
