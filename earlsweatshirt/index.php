9d53fafd91716722f08aa9e0b0758a4a0b148fcd
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-08 16:54:23 -0500

    Update index.php

--- earlsweatshirt/index.php
+++ earlsweatshirt/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Earl Sweatshirt';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cjpmkbodipjeljobppaaihjdebpabhpl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
