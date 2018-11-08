b5d55c1208d092298467a60e7563388feba29eb5
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-08 08:21:41 -0500

    Update index.php

--- remyma/index.php
+++ remyma/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Remy Ma';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fjlllhmoicibeomngdpfjpekjgndcnja';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
