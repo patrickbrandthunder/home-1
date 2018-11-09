43dc0ad20091576fab2ee5d1e71c7130730136fa
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-09 16:12:42 -0500

    Update index.php

--- torylanez/index.php
+++ torylanez/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Tory Lanez';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'phpmimigeiognpamanaekmnfmkgdochc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
