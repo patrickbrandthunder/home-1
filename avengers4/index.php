5933edf5347fd9252dba139cad31436ce76c0ae3
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-10 14:28:59 -0500

    Update index.php

--- avengers4/index.php
+++ avengers4/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Avengers4';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jknhohjoeggoddclaihlnkgplopcjpdm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
