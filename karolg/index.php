9f10cf09975e648a7694a3c1bb2c9720cb567e41
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-10 21:34:35 -0500

    Update index.php

--- karolg/index.php
+++ karolg/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Karol G';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hopdpfimehnndimandkicdbddgdececj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
