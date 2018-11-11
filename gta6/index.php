62f29598cbe147b9b4b472392354ee6bc63dc9fe
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-10 21:23:28 -0500

    Update index.php

--- gta6/index.php
+++ gta6/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'GTA 6';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gdfgnpeefafacijgcaidhoodccejedmj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
