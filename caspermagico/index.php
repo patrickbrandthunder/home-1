d1fdcb620c1a4f567319c04460335a5a65635a2d
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-10 21:35:02 -0500

    Update index.php

--- caspermagico/index.php
+++ caspermagico/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Casper Magico';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nopjopbgfahpplojjgfiokmioaodpmkg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
