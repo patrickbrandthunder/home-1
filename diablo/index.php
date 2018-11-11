4b2f7b31cd61f5779b0e992205c8f24b6ef920fe
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-10 21:45:48 -0500

    Update index.php

--- diablo/index.php
+++ diablo/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Diablo Immortal';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'einnjjnmcamdelnnnjodejobafoelbji';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
