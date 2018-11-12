44a38c171333297f00743df3e229a773f85b0a0c
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-12 12:30:36 -0500

    Update index.php

--- chiefkeef/index.php
+++ chiefkeef/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Chief Keef';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'adbaljkeenbiclkafhpndofgpgkghbbl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
