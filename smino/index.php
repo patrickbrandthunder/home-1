592bb04635c08abb68d5e56916e4685eb72b6048
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-12 09:02:20 -0500

    Update index.php

--- smino/index.php
+++ smino/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Smino';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bkkpgcfplamnohbcnbnhaipldafoelkj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
