4df9a7138916120e4601be044e6393e5a8cef8a1
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-12 13:13:34 -0500

    Update index.php

--- jessiej/index.php
+++ jessiej/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Jessie J';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'chfhdnikojmanicpbpnaogakjjgopnkc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
