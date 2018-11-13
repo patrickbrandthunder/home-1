68e07e26fd77e3d3cf85ee05a0d47e752164ace1
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-13 07:40:40 -0500

    Update index.php

--- schoolboy/index.php
+++ schoolboy/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Schoolboy Q';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jmdpfgfokmmpambflbbokbnmclannmhm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
