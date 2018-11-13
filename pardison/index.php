7a22204108c5f86f598383c4f6a9418c34a5460a
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-13 16:22:56 -0500

    Update index.php

--- pardison/index.php
+++ pardison/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Pardison';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ahanlgkkcabkiglgfgdeppgpopdkedmg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
