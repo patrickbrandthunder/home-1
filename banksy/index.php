c83c54f804d384eee6345149e00cfafbf59605f9
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-12 21:26:06 -0500

    Update index.php

--- banksy/index.php
+++ banksy/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bamksy';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mcenpgnmkpgdjcklbbklbofhapfgafgh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
