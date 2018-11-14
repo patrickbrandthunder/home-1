914cc3287d00a3e0254909c16ba86568faab361a
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-14 09:21:44 -0500

    Update index.php

--- mija/index.php
+++ mija/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mija';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'akcojeekmchlaolmdmiekbkfndpnlofo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
