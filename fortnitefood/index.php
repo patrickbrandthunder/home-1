0705c80e648a72d1e347280daeea2378fdd8608a
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-14 08:31:58 -0500

    Update index.php

--- fortnitefood/index.php
+++ fortnitefood/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Fortnite Food Fight';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ffpbnlngblocaeonkdfneooalmjmnihm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
