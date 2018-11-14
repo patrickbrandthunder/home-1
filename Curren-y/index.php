8399101fb51acf93132afd1b4edbe818f3169859
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-14 14:42:39 -0500

    Create index.php

---
+++ Curren-y/index.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'Curren$y';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
