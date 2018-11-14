a134c04600245352811275cea6a70fd62cc94952
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-14 16:00:06 -0500

    Update index.php

--- pharrell/index.php
+++ pharrell/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Pharrell';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dlelapdaieafgdkejahpnkpmnldcpibp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
