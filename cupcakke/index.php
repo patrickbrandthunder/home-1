ad7bdf3c8eb82250b40bedb12cf647498c5a8c1a
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-14 15:17:27 -0500

    Update index.php

--- cupcakke/index.php
+++ cupcakke/index.php
@@ -1,7 +1,7 @@
 <?php
-$title = 'Cupcakket';
+$title = 'Cupcakke';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bgolcbnjkdopahokabnhgeolbcieapll';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
