6135a23e9099bbc3cac9132a6dca6daee50766f3
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-14 08:57:42 -0500

    Update index.php

--- spyro/index.php
+++ spyro/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Spyro';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'alfiomkdnebpcdedeiomfpnenoemkpne';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
