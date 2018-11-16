a690c7b42d5ed3a7301255157b858bbaa705447c
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-16 08:38:52 -0500

    Update index.php

--- captainmarvel/index.php
+++ captainmarvel/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Captain Marvel';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jnemchagcajjlmbngkfpbdlmpbiflhbp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
