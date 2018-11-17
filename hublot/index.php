d259f23b53295c18590b3c4d090743cd060bc4ae
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-17 18:46:33 -0500

    Update index.php

--- hublot/index.php
+++ hublot/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Hublot';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ickgolpmddkpemamgmliakmchbambjac';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
