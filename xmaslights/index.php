ebc06dd50dc3c84a9ee8d34faa43363f4e337356
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-19 14:50:07 -0500

    Update index.php

--- xmaslights/index.php
+++ xmaslights/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Christmas Lights';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lmggckjhkpgcodaamkaphbalgogebgmj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
