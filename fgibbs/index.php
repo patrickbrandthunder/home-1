a1a7077741fcc942f29016bbdb4369cd900cc3a4
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-19 09:58:21 -0500

    Update index.php

--- fgibbs/index.php
+++ fgibbs/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Freddie Gibbs';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bdoieichogbecoehbhkoinbgmaclgime';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
