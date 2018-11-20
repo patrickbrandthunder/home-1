8df4fb33eae62f970b7606fe5ec46dfde311e878
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-20 10:03:49 -0500

    Update index.php

--- marcecko/index.php
+++ marcecko/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Marc Ecko';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dbkkgadoalcgojgpnbjbcohhmkgipeml';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
