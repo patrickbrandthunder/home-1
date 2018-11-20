e1ac54ae7d20258d5d4319b2ea1d4576ce362bb4
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-20 12:04:49 -0500

    Update index.php

--- starisborn/index.php
+++ starisborn/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'A Star is Born';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nofjmmhkihdfdifimgpgdfmlmcdfedff';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
