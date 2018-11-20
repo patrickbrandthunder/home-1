82e112ed098633ca3197d7f220791dd5bb053e87
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-20 10:19:21 -0500

    Update index.php

--- justcause4/index.php
+++ justcause4/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Just Cause 4';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ipbecieleecmmiemlnhmhkooabmhjlgm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
