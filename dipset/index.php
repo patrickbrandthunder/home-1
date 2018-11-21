396c601a6fc65d1d65fb7f18aac014f6c2c9bf42
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-20 22:09:05 -0500

    Update index.php

--- dipset/index.php
+++ dipset/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dipset';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ljlkbbpjalhdbcnljippgopgllkefgie';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
