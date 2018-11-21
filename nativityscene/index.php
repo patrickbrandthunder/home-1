494a61e48d0b2a7bdd40eaaeeed3d97578840244
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-21 13:37:47 -0500

    Update index.php

--- nativityscene/index.php
+++ nativityscene/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Nativity Scene';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ognfbakkimbgbdkkonbiijpdgpcdncej';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
