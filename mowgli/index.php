ce9a4cafee371f8cdda3a529b1b8a946fef1cebd
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-20 21:57:52 -0500

    Update index.php

--- mowgli/index.php
+++ mowgli/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mowgli';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jbolbonfajfoopnljpofgikcpigfdghi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
