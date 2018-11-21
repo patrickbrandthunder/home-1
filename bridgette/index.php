3b542b019def02fd8fed9480c4ae6d64d9c0a918
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-21 16:41:48 -0500

    Update index.php

--- bridgette/index.php
+++ bridgette/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bridgette B';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kofollhbiobboeeijbogkeknhllpkncd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
