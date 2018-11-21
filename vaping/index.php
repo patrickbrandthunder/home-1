4dd362be330eeb1ef3b76f18455b78b21e830ed9
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-21 08:25:23 -0500

    Update index.php

--- vaping/index.php
+++ vaping/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Vaping';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pilcddkfcpffmkbbcclhcbioihkegahl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
