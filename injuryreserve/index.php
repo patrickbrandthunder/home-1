0eb2c4285772f596177f1f6c698bc61c37bc16c2
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-21 16:51:59 -0500

    Update index.php

--- injuryreserve/index.php
+++ injuryreserve/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Injury Reserve';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'doiifejbnagciicglejlgnjkcffjfgco';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
