1afae08e3515fdce8caf91f0b8bbe44d0e3fe0bc
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-22 12:05:59 -0500

    Update index.php

--- cityskyline/index.php
+++ cityskyline/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'City Skyline';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kkdmfhgmphpmhcfmfmpoanfeecjpemaj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
