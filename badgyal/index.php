dc79c018105905fa8ccde75826b4854f90b505b6
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-22 18:47:15 -0500

    Update index.php

--- badgyal/index.php
+++ badgyal/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bad Gyal';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ogeenanjljfffpgkmcebdkoaiekllplh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
