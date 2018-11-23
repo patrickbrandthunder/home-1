cd7451ca1a8cb465c0be979d8db0b72168c57d7b
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-22 21:48:09 -0500

    Update index.php

--- jadensmith/index.php
+++ jadensmith/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Jaden Smith';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'money$$money##';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
