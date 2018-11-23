782963272ff9ebb9ecd9cd054a85986fb3de9fe9
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-23 09:49:13 -0500

    Update index.php

--- bigkrit/index.php
+++ bigkrit/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Big Krit';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ofkibanklmfklkkdmcjldecclnlbldag';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
