ccc9af5d501bb22aa3f8c2458055c0bf2eb43ebc
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-23 09:19:58 -0500

    Update index.php

--- joynerlucas/index.php
+++ joynerlucas/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Joyner Lucas';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'eofhbmgogmlndeikggligjpdkafppibi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
