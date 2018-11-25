66132922746c2f1d156ac6de5f04638c5a59fb51
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-24 22:02:08 -0500

    Update index.php

--- legocity/index.php
+++ legocity/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Lego City';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ciklgbgglapgcbcejengkdjfllggpghi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
