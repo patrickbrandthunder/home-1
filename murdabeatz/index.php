6f09a58c42347b1a6154c138cf5549ff13ce924d
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-25 13:58:38 -0500

    Update index.php

--- murdabeatz/index.php
+++ murdabeatz/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Murda Beatz';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gpdglbpleeplljpfmfccpmomdkddedmn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
