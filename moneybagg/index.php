27635bdd1f546bbcab9939d3ed1aae8f62f901b8
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-25 13:59:44 -0500

    Update index.php

--- moneybagg/index.php
+++ moneybagg/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Moneybagg Yo';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kbopchnamfgalhdljimfmnmlibfbcfdd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
