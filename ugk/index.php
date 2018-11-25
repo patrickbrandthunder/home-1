f15e68a2b25c72c2ce19dd0784e8464ba3fc908d
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-25 12:43:52 -0500

    Update index.php

--- ugk/index.php
+++ ugk/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'UGK';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jgbleogmlmodbpbknpbicdnconmgecag';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
