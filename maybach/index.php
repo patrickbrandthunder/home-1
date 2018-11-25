7d4fbbb6589888e194132eaa727f98b20a1ae7ac
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-24 22:25:06 -0500

    Update index.php

--- maybach/index.php
+++ maybach/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Maybach';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hcakflinincljoepghkcmdejmaiedfln';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
