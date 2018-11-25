5b260e3627d936d8d9a83c48b5b731ad0711d37e
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-25 08:11:20 -0500

    Update index.php

--- roccat/index.php
+++ roccat/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Roccat';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ejjnnmofkkmjoghfkccikgpfhpelapci';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
