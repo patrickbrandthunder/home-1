75a09e319bf7666596fb8f3423c18b082abecdf2
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-25 12:47:22 -0500

    Update index.php

--- tommycash/index.php
+++ tommycash/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Tommy Cash';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lbkfhhilceplifpfejhiiepombdkjimm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
