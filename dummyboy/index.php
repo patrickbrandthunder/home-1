ddbb37f23c2fe9429a58e63abb923bd182476e52
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-25 11:20:39 -0500

    Update index.php

--- dummyboy/index.php
+++ dummyboy/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dummy Boy';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mkffmccjipppibomchcpckegbhibpige';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
