061c06e93b8c4ce6ed8c22d79c819d6f42226db3
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-24 21:25:56 -0500

    Update index.php

--- famousart/index.php
+++ famousart/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Famous Art';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jgnhmnihgncnfknecolgjlhfkofnlimk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
