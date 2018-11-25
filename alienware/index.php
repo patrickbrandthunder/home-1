a5a40943047b1cd5d94576ccbabd53fe26ef52fe
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-24 20:47:49 -0500

    Update index.php

--- alienware/index.php
+++ alienware/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Alienware';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'efjgbapjfmoibkojlfompcmghcceonno';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
