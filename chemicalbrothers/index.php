53911c69270b2a07e04e328976b0d369cc0a2a86
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-25 20:15:04 -0500

    Update index.php

--- chemicalbrothers/index.php
+++ chemicalbrothers/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Chemical Brothers';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ljmenglajogaoepekfkdldnllchablbb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
