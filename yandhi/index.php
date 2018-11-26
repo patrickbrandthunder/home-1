35dc6abf431de4e93dddfc4d6c6a7191439b8065
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-26 11:07:30 -0500

    Update index.php

--- yandhi/index.php
+++ yandhi/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Yandhi';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mocoejfllfimkclhjhmknaclcjdpplbl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
