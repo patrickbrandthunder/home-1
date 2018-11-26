cb019dad51c233cdbfb85125e473a38b1886ce70
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-26 10:34:09 -0500

    Update index.php

--- canecorso/index.php
+++ canecorso/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Cane Corso';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jcppgeehkkmehdioibobklfmdafgfffi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
