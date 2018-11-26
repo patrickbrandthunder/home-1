925841517137dc7d66ef7337b324d156459eb2f1
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-26 11:47:34 -0500

    Update index.php

--- remybanks/index.php
+++ remybanks/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Remy Banks';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ihhdjpmeelmghckdhemjldhnoihfagoi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
