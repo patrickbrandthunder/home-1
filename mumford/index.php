e2cf54f4d02e6ef5399f77e9bd5758494e608542
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-26 11:49:15 -0500

    Update index.php

--- mumford/index.php
+++ mumford/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Mumford';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'inlekinijhfhbcijknlfnfppeoobgbhd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
