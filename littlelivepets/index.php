3fb82715e7ced0c2d7f1c93151ee63a211aad421
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-25 20:45:49 -0500

    Update index.php

--- littlelivepets/index.php
+++ littlelivepets/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Little Live Petst';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bdcklbinemhkllgaolohjjopaogbjhnb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
