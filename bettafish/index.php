e6bd94c25b8bdef26fc4cb1cf8a8e5ae8fe61757
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-25 20:04:13 -0500

    Update index.php

--- bettafish/index.php
+++ bettafish/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Betta Fish';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mnkjkkagkcnbcgnigoncgpnaihlejjid';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
