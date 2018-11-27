244d7f4e8fd135fb154cebd19fb79b0f24f1988c
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-27 07:21:21 -0500

    Update index.php

--- narcosmexico/index.php
+++ narcosmexico/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Name of your new Project';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lbmnbnlajjcdenpmoklfmlomgkpbajad';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
