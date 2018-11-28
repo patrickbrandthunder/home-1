e2e98ceac790ffa2a7daeeead4e1a41b329926bd
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-28 16:21:51 -0500

    Update index.php

--- saharanight/index.php
+++ saharanight/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Sahara Desert Night';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kbnfkkfaopciibopbgchhlpmjdcgcfcm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
