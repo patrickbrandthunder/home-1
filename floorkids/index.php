03848bba596ea6025195ea88893f244cfe842f47
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-27 21:19:39 -0500

    Update index.php

--- floorkids/index.php
+++ floorkids/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Floor Kids';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'amepgjkbbihnofpliigjjlodgdoolada';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
