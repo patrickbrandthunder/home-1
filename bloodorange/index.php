9f72b853b0d57b5fcef49637639a8671266dc2d5
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-29 16:26:49 -0500

    Update index.php

--- bloodorange/index.php
+++ bloodorange/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Blood Orange';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nbadmpiopjojkmafonjlnnacadfmmlfk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
