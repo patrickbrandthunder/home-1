2e9f0181e50d052f375cdfed6947cf718c613d7e
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-29 16:56:22 -0500

    Update index.php

--- cherries/index.php
+++ cherries/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Cherries';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hfnjonajldgmkgbmlfmcpecliofochmk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
