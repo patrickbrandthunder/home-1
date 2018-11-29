06bff88c1cf1ef77453ddef5de9f57c3a978227c
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-29 16:46:02 -0500

    Update index.php

--- bluegreen/index.php
+++ bluegreen/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Blue Green Cubes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kgclegamhchknpakecnffcliedofdofd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
