6e4334182fee2090d1a08aab9a4f06c9f6c03cfe
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-30 09:03:10 -0500

    Update index.php

--- gstarraw/index.php
+++ gstarraw/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'G Star Raw';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hgaamdbdocpleoleajblbeopbojcdkgh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
