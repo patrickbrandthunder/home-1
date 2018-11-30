b207060fb1c2eed9b9f6ab7041f83869edc57489
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-29 22:11:24 -0500

    Update index.php

--- danidaniels/index.php
+++ danidaniels/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dani Daniels';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fnikcdabekkdcaccjegfbhbfcdiammcj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
