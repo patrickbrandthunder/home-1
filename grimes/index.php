2b52cf8d20d9a337493033819d3ea219210721dc
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-29 22:45:29 -0500

    Update index.php

--- grimes/index.php
+++ grimes/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Grimes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kcjnpeadipagjdmlmlpbebmhbigbahjd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
