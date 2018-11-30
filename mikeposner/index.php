26feaa64cbf906c64e222dd05add90ed295a50f4
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-30 10:53:12 -0500

    Update index.php

--- mikeposner/index.php
+++ mikeposner/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mike Posner';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kmgilabepmdembgfhflhjccgcbfobbii';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
