f0cbed291492dfae1eecc10e8c29316b9b4ecf3e
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-30 10:22:44 -0500

    Update index.php

--- goyard/index.php
+++ goyard/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Goyard';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kmgilabepmdembgfhflhjccgcbfobbii';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
