72c9b7230d2e8fa60d6969e7a4bf80cafd3268b5
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-30 09:59:13 -0500

    Update index.php

--- amggt/index.php
+++ amggt/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mercedes AMG GT';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lcnibnlahbkkpghgbcjcpekconikjmpa';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
