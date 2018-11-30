7553ba0ce5a02ff807ade3a0edd0875b90945bdb
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-30 07:43:48 -0500

    Update index.php

--- mars/index.php
+++ mars/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mars';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jhafoilhalafpaecjpefikkbdmmelein';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
