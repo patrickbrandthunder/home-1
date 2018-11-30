1203143019a4d16b884f68d1675f8bee8950ecaa
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-30 15:39:49 -0500

    Update index.php

--- masterchief/index.php
+++ masterchief/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Master Chief';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'odniflkjnjdmkjbcenbcmoaanhblgdfi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
