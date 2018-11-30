f00adf72a0ad3b30ebf2527abdf996b0f251315a
pjmurphy777 <pjmurphy77@gmail.com>
2018-11-30 09:11:47 -0500

    Update index.php

--- guac2/index.php
+++ guac2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Guacamelee! 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'codnfmmoecalmfgooihmdoefmiohodpn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
