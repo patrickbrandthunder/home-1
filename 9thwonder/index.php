39d379e35870dad5da72472ddc394acb3d81073e
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-02 16:03:15 -0500

    Update index.php

--- 9thwonder/index.php
+++ 9thwonder/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = '9th Wonder';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ignmclfeehnfcfcmimmanfcofikiecee';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
