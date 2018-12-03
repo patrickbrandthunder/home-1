3f71401809080f4deccc3652d6b0a23aaa15621a
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-03 12:39:54 -0500

    Update index.php

--- mickjenkins/index.php
+++ mickjenkins/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mick Jenkins';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'iokekafeblmpngdlkgmnedncgkomjkhi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
