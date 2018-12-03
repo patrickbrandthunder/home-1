85a25689162b52514b04c852c0271b319ede7713
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-02 20:15:42 -0500

    Update index.php

--- blackthought/index.php
+++ blackthought/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Black Thought';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ehjmmignpemnbohgcnpmkkodaplekokj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
