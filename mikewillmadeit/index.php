f3352abc74c649350173da2124e744f29f5a18bf
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-03 12:48:29 -0500

    Update index.php

--- mikewillmadeit/index.php
+++ mikewillmadeit/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Mike WIll Made It';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ecbodmmonjmjbcmggligmjnohcmdgfea';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
