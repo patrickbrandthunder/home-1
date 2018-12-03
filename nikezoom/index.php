8857b8edfaa887f689cc31b2e9a77ee8fa645d8f
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-02 19:47:11 -0500

    Update index.php

--- nikezoom/index.php
+++ nikezoom/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Niek Zoom';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nkahegkeoejffnpoodlnkdegibbdpood';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
