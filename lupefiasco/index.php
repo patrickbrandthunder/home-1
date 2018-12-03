797c0a9846bfdcd8b2443532e06d5a1a557655c1
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-03 12:39:37 -0500

    Update index.php

--- lupefiasco/index.php
+++ lupefiasco/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Lupe Fiasco';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gcdljappfnjdplemggbpilblliomffel';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
