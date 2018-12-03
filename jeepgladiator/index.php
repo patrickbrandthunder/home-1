a4aee2970e6a89356d5c1a9dd90f7b8486391289
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-02 19:09:22 -0500

    Update index.php

--- jeepgladiator/index.php
+++ jeepgladiator/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Jeep Gladiator';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jomphlphkcbnbbjahhnakhbfigihgfci';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
