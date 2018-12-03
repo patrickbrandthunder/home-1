f61cdb1bc861c3fcac8cf7ce7a45b89b44b4586a
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-02 19:09:47 -0500

    Update index.php

--- fireemble/index.php
+++ fireemble/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Fire Emblem';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fimpnkindbpgjkncjknjknllkampafmk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
