8a21800c4f88c0876c2c410bdf4939ea3170ad91
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-03 13:23:26 -0500

    Update index.php

--- rachetclank/index.php
+++ rachetclank/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Rachet and Clank';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nfnmmdpnncfpajgfndpnjjkfnlhmncac';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
