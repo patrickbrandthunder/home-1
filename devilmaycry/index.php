b221ee2f4846ff663fb8fe4da5a8b2d4e4ba1f87
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-03 21:16:47 -0500

    Create index.php

---
+++ devilmaycry/index.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'DEvil May Cry 5';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
