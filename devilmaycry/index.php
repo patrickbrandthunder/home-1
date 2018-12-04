b71b84160a5cb9f87b21f41e1ab1bae4a495640e
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-03 21:31:40 -0500

    Update index.php

--- devilmaycry/index.php
+++ devilmaycry/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'DEvil May Cry 5';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'aolmkojnghnkkihhngofkfamodleilhg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
