7447dc6f8857d0c6f04e65fcd9f008ba21e9705f
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-04 11:49:15 -0500

    Update index.php

--- nairi/index.php
+++ nairi/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Nairi: Tower of Shirin';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jpgcclenhcafbddnacfonmibjjhpiahf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
