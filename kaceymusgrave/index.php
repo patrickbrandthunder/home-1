6e116203f306d9af2f58e483641a3512ba2dee48
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-04 16:02:13 -0500

    Update index.php

--- kaceymusgrave/index.php
+++ kaceymusgrave/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Kacey Musgrave';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kioafhphfponomhhfphlimokknlhpdbk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
