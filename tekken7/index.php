7408e97620bcbe254a36d8eaafaccef405ffe1cd
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-04 12:18:25 -0500

    Update index.php

--- tekken7/index.php
+++ tekken7/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Tekken7';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cdnpnkicphjbklljjhiolpoopedplalo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
