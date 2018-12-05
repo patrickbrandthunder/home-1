fb8f44d9ca63729acf9fc720954f6fb8c927e545
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-05 16:23:31 -0500

    Update index.php

--- ancestors/index.php
+++ ancestors/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Ancestors: The Humankind Odyssey';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ncfojepbonjjmnkpjhhealinhiapcefe';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
