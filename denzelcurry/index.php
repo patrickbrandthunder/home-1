915fb1134ac5fe7ad1f97c33fbd3c87a5ee3c5df
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-05 16:35:51 -0500

    Update index.php

--- denzelcurry/index.php
+++ denzelcurry/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Denzel Curry';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mgobanmepmdhlhdekcfegbafngdbkkfa';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
