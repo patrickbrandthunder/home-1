c3f4e59922096ed9f18b557e045dd879c56020cc
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-04 19:35:54 -0500

    Update index.php

--- joebudden/index.php
+++ joebudden/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Joe Budden';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ffpggigpdlhkmekdkkgejigmedfikghe';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
