7b444dd60a7e98a15bc8f5232d86ccc511e2d80b
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-05 16:21:11 -0500

    Update index.php

--- bia/index.php
+++ bia/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'BIA';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cfinnelkcfnanjjeapfbifoibbiknndn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
