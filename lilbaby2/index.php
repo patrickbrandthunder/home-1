8668fa14a45b203b940a33bcfababc7efc91c2e5
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-06 11:20:30 -0500

    Update index.php

--- lilbaby2/index.php
+++ lilbaby2/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Lil baby 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dbaipecjmndpgjebbjpjbifeaadcbmlp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
