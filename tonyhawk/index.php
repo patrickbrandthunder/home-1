d0c251154f3f843787242d731d749b776b63f16e
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-05 21:56:26 -0500

    Update index.php

--- tonyhawk/index.php
+++ tonyhawk/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Tony Hawk Skate';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lopeilnjnehhmgmomgamahecbbgdjbdi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
