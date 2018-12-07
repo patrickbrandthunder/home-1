0ffc11d81723675119143dac191709175c9c361c
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-07 14:33:59 -0500

    Update index.php

--- versace2/index.php
+++ versace2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Versace 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nfkimnpneallpgglcfiehmlpcoecjbbp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
