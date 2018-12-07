b0f8067bbafccf70ee14513b0d850a407e86421f
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-07 09:01:23 -0500

    Update index.php

--- nerfguns2/index.php
+++ nerfguns2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Nerf Guns 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hfonbkclpijleefmoilkopkddllepklp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
