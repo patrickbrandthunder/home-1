e8998379631dd85d16c91087efe3b072ecd2fa96
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-07 11:42:55 -0500

    Update index.php

--- shangchi/index.php
+++ shangchi/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Shang Chi';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ffinggepkkdadgahfeiioiapbfafiohe';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
