f25903eda95e39c39a6ce54cc30878d960068dac
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-06 23:00:22 -0500

    Update index.php

--- paintsplatter2/index.php
+++ paintsplatter2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Paint Splatter 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'eknmpjmbahifbnfcmipjicglibmffdeb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
