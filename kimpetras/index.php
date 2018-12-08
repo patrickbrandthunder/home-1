d4029207e8d247fcc25881bf4a45156a46d00de8
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-07 21:46:42 -0500

    Update index.php

--- kimpetras/index.php
+++ kimpetras/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Kim Petras';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hemblbjaemdghnjlgplpiacmodghkbop';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
