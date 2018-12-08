0bbb9eb23332775f221c584da5c408fb9bf7f3a2
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-07 21:22:50 -0500

    Update index.php

--- yflkelvin/index.php
+++ yflkelvin/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'YFL Kelvin';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'amnkpomocgmnocgehchjgacjicoaejdi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
