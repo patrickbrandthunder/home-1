e5af4a94578ff77670c0ae00f72f6c3110c1919e
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-08 11:26:20 -0500

    Update index.php

--- janelle/index.php
+++ janelle/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Janelle';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'elepdkgdmemielgphpkcjiiiignahhgf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
