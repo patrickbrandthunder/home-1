0bb7ac3ffeaf5b6395d126f283724c08b939e940
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-07 21:22:23 -0500

    Update index.php

--- valee/index.php
+++ valee/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Valee';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'okcjmgipaehblhfidknipnjdbbdnkmhb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
