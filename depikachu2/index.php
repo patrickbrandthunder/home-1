7a464cf2ef1b4361e341907aae5be5c043b1f690
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-08 11:11:37 -0500

    Update index.php

--- depikachu2/index.php
+++ depikachu2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Det Pikachu 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'eeibhcgdboccdcheeendjkpfhcakmhio';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
