936a1856c05519b271156071322da9aa483f055c
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-09 10:13:52 -0500

    Update index.php

--- djkoze/index.php
+++ djkoze/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'DJ Koze';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'holgafbbkicgfdpbfljfjmccjnlhlocp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
