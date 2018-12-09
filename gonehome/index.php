502de74809dec3bda4688228096b3eabd617159e
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-09 16:41:56 -0500

    Update index.php

--- gonehome/index.php
+++ gonehome/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Gone Home';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mfeabknkippjalpbaemmmljajhjhmoki';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
