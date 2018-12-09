f07253d4d46d806951d774d5c58cf78818a928ee
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-09 16:41:41 -0500

    Update index.php

--- gmcdenali/index.php
+++ gmcdenali/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'GMC Denali';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'obigghjompfkbfdefpimjmbphkmoochh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
