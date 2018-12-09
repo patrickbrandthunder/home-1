922d5264f382813bce461e835f50d2b25a2b9271
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-09 09:31:53 -0500

    Update index.php

--- blueface/index.php
+++ blueface/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Blueface Rapper';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mohfebiifenfllcnjfkfiihnoepndggn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
