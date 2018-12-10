bddf816be28dea20d060a290cb25227260e85b56
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-10 14:54:19 -0500

    Update index.php

--- ashin/index.php
+++ ashin/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Ashin';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nhmeendlohanffffhdaaniakjnohbdkk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
