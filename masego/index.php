bf89ab5a671d01f528afed2e18de0278eeae3637
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-10 10:13:45 -0500

    Update index.php

--- masego/index.php
+++ masego/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Masego';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fedfhchjpfdjpkjihkdeoodfffebpdfo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
