d3111a44ddd50df3789067861d246f25e1f60e2b
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-11 15:58:22 -0500

    Update index.php

--- pokimane/index.php
+++ pokimane/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Pokimane';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'aobpeahifialjncoghkbahehnndmdiin';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
