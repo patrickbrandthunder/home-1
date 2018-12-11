7ed19745b62bee117212feabf039ba81a22d78f0
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-11 09:16:57 -0500

    Create index.php

---
+++ forniteblock/index.php
@@ -0,0 +1,8 @@
+
+<?php
+$title = 'Fortnite Block';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
