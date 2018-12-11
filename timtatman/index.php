9a7981a570ba940e5ace4de0a95c0766c781e243
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-10 21:29:00 -0500

    Update index.php

--- timtatman/index.php
+++ timtatman/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Tim The Tat Man';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pnpgidmkopmldhgdooihclmhplckhaad';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
