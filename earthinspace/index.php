0f12ec2d7b08c2e3b65bfad7f3cfff33684f2304
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-11 09:25:36 -0500

    Create index.php

---
+++ earthinspace/index.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'Earth In PLace';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
