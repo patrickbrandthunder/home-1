1674b7fe738a39c641c55fed39fc5c306fd7e850
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-11 09:53:58 -0500

    Update index.php

--- earthinspace/index.php
+++ earthinspace/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Earth In PLace';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lhmekpnjobofindclhlegnebmialboeb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
