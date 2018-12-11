91868201a5ae7c44a4e8a59b830cb234cff5c561
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-10 20:26:16 -0500

    Update index.php

--- sonichedgehog/index.php
+++ sonichedgehog/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Sonic The Hedgehog';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fplmkkojjcgdegdkdgdhapbjdagfabpb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
