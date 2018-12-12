ad5018b823f25b589ca442c51a71cbd7d5ceb108
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-12 08:34:47 -0500

    Update index.php

--- travisstrikes/index.php
+++ travisstrikes/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Travis Strikes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mnfnopafihkoopkalbcpmlagkdgadkgh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
