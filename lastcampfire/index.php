3441b062ec1cdb4a1edc50b07ab98116a8b43ca2
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-12 07:51:08 -0500

    Update index.php

--- lastcampfire/index.php
+++ lastcampfire/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Last Campfire';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lpcpopdjhfljafihjjoccfgkfihfolkl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
