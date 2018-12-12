a40501ac287ebc08966021792beb27efba227706
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-12 08:50:27 -0500

    Update index.php

--- sneakk/index.php
+++ sneakk/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Sneakk';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ikklgagmjnbkffjjphbfkiekanikgimi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
