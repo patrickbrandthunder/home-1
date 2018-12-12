a439e8101ecd0ab22d4feb10415aa235530700d2
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-12 09:37:32 -0500

    Update index.php

--- smooky/index.php
+++ smooky/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Smooky';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nhhjcgdkfhfpidbkkphgafkaeadnbcem';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
