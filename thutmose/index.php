6dbadbb6e504136b27f98ca4e451a304278592a2
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-12 07:57:46 -0500

    Update index.php

--- thutmose/index.php
+++ thutmose/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Thutmose';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kgcajeblcjcopmnmomdmfkbodbbcoihh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
