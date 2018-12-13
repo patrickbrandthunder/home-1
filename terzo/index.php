c19542edb1726e3a24e1d5ce1c1e0844406c260f
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-12 21:26:13 -0500

    Update index.php

--- terzo/index.php
+++ terzo/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Terzo';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lnobnbbhnodidmhkkpnmnlopjjjiphen';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
