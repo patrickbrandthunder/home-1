8c5996b5242fa3720e95286313a1348c3a3689ab
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-13 10:53:20 -0500

    Update index.php

--- killermike/index.php
+++ killermike/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Killer Mike';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'olaonkfianldhdohcideefpnenidlnkb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
