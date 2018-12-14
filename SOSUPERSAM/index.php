31e04371690f29fda9a8530834b27e72cb9834bc
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-14 15:39:23 -0500

    Update index.php

--- SOSUPERSAM/index.php
+++ SOSUPERSAM/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'SOSUPERSAM';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fpgdpckjmobeoilllhcfoafepmlefbfk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
