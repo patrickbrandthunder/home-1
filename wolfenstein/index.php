7b56d0c41425a5455ebcda408555139688626932
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-14 13:03:24 -0500

    Update index.php

--- wolfenstein/index.php
+++ wolfenstein/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'wolfenstein';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ehjphbjjofhjjaajmpipfiidaobolfbn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
