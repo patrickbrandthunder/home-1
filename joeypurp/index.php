e1fbbdedce0fb00ddf0293e2357eb8aad511fabe
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-14 13:53:45 -0500

    Update index.php

--- joeypurp/index.php
+++ joeypurp/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Joey Purp';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ofncilfhphhjgkebnadmmonfmjgjgfan';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
