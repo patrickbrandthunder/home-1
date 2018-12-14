1606607ad89c82321362276be3e4bcb9b25d0166
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-14 09:26:43 -0500

    Update index.php

--- division2/index.php
+++ division2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Division2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mgafhnnpckgnmnkanigkdlenekenhpeh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
