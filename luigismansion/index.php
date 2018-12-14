9ccf7d02850daebdd4255305645be8df255413cb
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-14 09:08:07 -0500

    Update index.php

--- luigismansion/index.php
+++ luigismansion/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Luigismansion';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'epnfoibmhlejlkcjopdoiagppggnehab';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
