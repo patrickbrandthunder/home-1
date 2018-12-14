107af5cbef800f563859044518d572b402e9c14f
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-14 08:21:54 -0500

    Update index.php

--- animalcrossing/index.php
+++ animalcrossing/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Animal Crossing';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'homdblefaeohgcjbnghdoojmejdnfmdf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
