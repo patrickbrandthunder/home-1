b9568dc4921d8c8aeff249b3cba672df90c31bfe
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-14 09:00:08 -0500

    Update index.php

--- gears5/index.php
+++ gears5/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Gears 5';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hggjdacdkgopmcmgcipdcfdmbkokafhi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
