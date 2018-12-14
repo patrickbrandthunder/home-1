11b47a8e428fa13df9f3b53166e96d9ada411173
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-14 08:56:43 -0500

    Update index.php

--- valleygods/index.php
+++ valleygods/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'In the Valley of Gods';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gknjanilakfijlkmcaeagjfabikhgjni';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
