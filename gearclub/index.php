53fe1195412d40dcaae248d5675dcf6c7c21c707
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-15 07:23:15 -0500

    Update index.php

--- gearclub/index.php
+++ gearclub/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Gear Club';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hohlpkfglaeflfpnnfdfiggjjpjgimca';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
