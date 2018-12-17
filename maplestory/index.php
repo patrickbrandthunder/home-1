db6635164c8525b7279287b7fdb929dd842486cc
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-17 16:14:46 -0500

    Update index.php

--- maplestory/index.php
+++ maplestory/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Maplestory';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dpoelbfbhiebgdlopmemmibdmfknhima';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
