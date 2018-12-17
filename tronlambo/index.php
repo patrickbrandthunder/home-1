de26d46a3fdf93264fa5d817bcda620bba68e145
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-17 09:33:48 -0500

    Update index.php

--- tronlambo/index.php
+++ tronlambo/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Tron Lambo';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'okfihideleopkhmnckldnfiihbgfddnh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
