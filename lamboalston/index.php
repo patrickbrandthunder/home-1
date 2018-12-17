751423724f47a6b0193b98f4c670ae997e48db42
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-17 10:51:56 -0500

    Update index.php

--- lamboalston/index.php
+++ lamboalston/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Lambo Alston';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pjfchipffmjcdlajiblnagjkehkjonjg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
