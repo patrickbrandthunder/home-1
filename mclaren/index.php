e92c1d1e70bf699fa941d2016d2e1e609f9f4931
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-17 14:37:40 -0500

    Update index.php

--- mclaren/index.php
+++ mclaren/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mclaren spider';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pfdihlmijfjkgbcmleofkkbbblgmilpn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
