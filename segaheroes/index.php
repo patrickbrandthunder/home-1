6afbc2e798ad40692a83f80cdee1808af14b0345
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-17 09:02:07 -0500

    Update index.php

--- segaheroes/index.php
+++ segaheroes/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Sega Heroes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'oacmcmcjlnpcgegcmmncplmgkhdfnolp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
