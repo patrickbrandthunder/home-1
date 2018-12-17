82a8294d68c484bff195f8cc530b55bd419e8430
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-17 09:50:00 -0500

    Update index.php

--- sierra/index.php
+++ sierra/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Sierra';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gggbmlmnboljpnmaaeepdgdelkciimlc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
