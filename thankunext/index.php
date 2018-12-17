8d346dedd5ef0cf4a353e93b87b0321707c1da2d
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-16 19:01:30 -0500

    Update index.php

--- thankunext/index.php
+++ thankunext/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'thank u next';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lebgnilncpabdmhijmeoghbadhkajphf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
