2df9b2bda7337efff8f7f397b386d9e7976811b3
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-19 15:41:08 -0500

    Update index.php

--- kidink/index.php
+++ kidink/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Kid Ink';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bagpbbpkdcedmiciliofcogongfbgppj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
