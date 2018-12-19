e98dd4895b4567f7f7360cdbe7c8e43b0cd94557
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-19 16:44:19 -0500

    Update index.php

--- enchanted/index.php
+++ enchanted/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Enchanted FOrest';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bcjibfmpanebegndnakhamfimjbdlmff';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
