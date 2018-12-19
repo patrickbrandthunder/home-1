272d66d35875b9645ff8a857227657f47fbfbab6
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-19 08:04:10 -0500

    Update index.php

--- poopsie/index.php
+++ poopsie/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Poopsie Unicorn';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pfdhfedadkafhkflnikfjoffbkgdiple';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
