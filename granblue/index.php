15b44871b11ebdb25fcddd4b6bbbec4703fa3127
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-19 16:06:58 -0500

    Update index.php

--- granblue/index.php
+++ granblue/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Granblue Fantasy';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'djbheamjfgkeejneimmehfgkfnkcifal';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
