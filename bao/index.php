1d4794090311d6dc305398223ca3251e6708df96
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-21 15:36:18 -0500

    Update index.php

--- bao/index.php
+++ bao/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'BAO';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'amaafejnmojnomhgekmkflmlgljfhjaf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
