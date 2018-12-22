a481a36dab0a0966d1e1140ae5f06787eae4fab7
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-22 16:42:03 -0500

    Update index.php

--- cutebabies/index.php
+++ cutebabies/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Cute Babies';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ppdbmfgjhodjkbombifihbmiaojcbbmf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
