83e14f5486145c928a90b1729df5546c76dd96b5
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-22 15:54:33 -0500

    Update index.php

--- bugus/index.php
+++ bugus/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bugus';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'alimghboikdlndpklnimallpkmknkajd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
