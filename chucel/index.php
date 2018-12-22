5ea96e516f9cd69d145d9937a8a584d44fb510bd
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-22 16:17:54 -0500

    Update index.php

--- chucel/index.php
+++ chucel/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Chucel';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fbjbbchppcflhkjgccinmpmelmlephpj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
