7eabec08999c93368e20415061a725e549350902
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-22 19:51:46 -0500

    Update index.php

--- dayz/index.php
+++ dayz/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'DayZ';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cegbfndeildlnmebndlfmbbpojhaoild';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
