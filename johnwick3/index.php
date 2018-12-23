0c06859139142a95a194380153582a1f1b1ce65e
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-22 20:51:59 -0500

    Update index.php

--- johnwick3/index.php
+++ johnwick3/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'John Wick 3';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dgdecgfdigcnicikmjghmlhmhlbmdhnp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
