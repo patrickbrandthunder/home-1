2a2290e2a25fb2a8c859f24b399f0ea90049fbaa
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-23 13:08:01 -0500

    Update index.php

--- russ/index.php
+++ russ/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Russ';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mlgnnnjajdpkbnhnglgikeflliiblmcb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
