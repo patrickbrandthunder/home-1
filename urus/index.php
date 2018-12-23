6ea86d1cd975939829e312ce38db6b33a3b9acdd
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-22 20:52:24 -0500

    Update index.php

--- urus/index.php
+++ urus/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Lambo Urus';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fgacjkmnaiolmmhdjkgagahkpcljifpg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
