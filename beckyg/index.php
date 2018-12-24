75a4bb38d18ea53bdd913815516b4d87af4cc0a1
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-24 14:43:30 -0500

    Update index.php

--- beckyg/index.php
+++ beckyg/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Becky G';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'oemamjnihajcjacgacfocfpfmhcgnjlc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
