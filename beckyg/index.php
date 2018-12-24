4598aa5fa4bb07c5d8fd577e3de5e07d57c4c603
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-24 14:14:56 -0500

    Create index.php

---
+++ beckyg/index.php
@@ -0,0 +1,8 @@
+
+<?php
+$title = 'Becky G';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
