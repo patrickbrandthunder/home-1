22340a70dccc35a74ad2449fbce1c592e731770b
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-24 08:42:48 -0500

    Update index.php

--- mariokart/index.php
+++ mariokart/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'mariokart';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pagndjppppanbnmiikiljbeeonegefcc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
