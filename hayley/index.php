1d82b6847442673a76da4ce106b09b680e7224d3
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-24 15:02:44 -0500

    Update index.php

--- hayley/index.php
+++ hayley/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Hayley Kiyoko';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cioocooffldjebopahibgifglfhgipch';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
