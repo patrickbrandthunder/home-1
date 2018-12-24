08683ef6fd2346559c9cfcfcd4b2a880b497db92
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-24 15:02:27 -0500

    Update index.php

--- littlebabybum/index.php
+++ littlebabybum/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Little Baby Bum';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hpdlmegojilhhpohdjpifbnilbnnpibb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
