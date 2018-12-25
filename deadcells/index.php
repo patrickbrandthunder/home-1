113888ef706a871ef31799dc63a869bdef8275f8
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-25 08:23:32 -0500

    Update index.php

--- deadcells/index.php
+++ deadcells/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dead Cells';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lfmfdfbfdfjhgjgobfcpelklneodlefm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
