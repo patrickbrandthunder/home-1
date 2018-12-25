5d94ddbf16fdd54f4f11c91e5a2fb4373e77b571
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-25 08:53:04 -0500

    Update index.php

--- princeroyce/index.php
+++ princeroyce/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Prince Royce';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pdpckofcadbdjandfghmbddidmefgcij';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
