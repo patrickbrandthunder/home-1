21ce227cf17c8ea58428c7991c07156bcf2c8a37
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-26 11:43:52 -0500

    Update index.php

--- gunsofglory/index.php
+++ gunsofglory/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Guns of Glory';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'epmpblnnnpgjoiolhnjfpljpekjhaohc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
