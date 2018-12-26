efd21dfa6d5df15df16f5923f8185f65377ae67c
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-26 10:45:28 -0500

    Update index.php

--- danshay/index.php
+++ danshay/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dan Shay';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'comhpllijlamokiclieoiiibjfiabokm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
