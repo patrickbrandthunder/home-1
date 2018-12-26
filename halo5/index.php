d1890c2142c37b1d173b2ec1f05502378455cd1e
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-26 15:30:34 -0500

    Update index.php

--- halo5/index.php
+++ halo5/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'HALO5';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dlohpnedmiopkaelmdkoalhfpppjbaae';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
