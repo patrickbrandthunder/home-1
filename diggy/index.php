5b5a036d15fdc4c6d223752a9715bb86409ea48f
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-26 11:21:54 -0500

    Update index.php

--- diggy/index.php
+++ diggy/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Diggy';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bkhealaojffdpegbcbdlnddcnhneicep';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
