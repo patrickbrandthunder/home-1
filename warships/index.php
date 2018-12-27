512a2894a9a76e89d386605d6fcb5af43c24044d
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-27 16:56:15 -0500

    Update index.php

--- warships/index.php
+++ warships/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'World of Warships Blitz';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'eekeamimnfaokeaclpkmcdlajmmhocmg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
