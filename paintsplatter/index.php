e8aacc610629f85e3fe282d1ec428283cd3f258b
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-27 13:52:38 -0500

    Update index.php

--- paintsplatter/index.php
+++ paintsplatter/index.php
@@ -2,6 +2,7 @@
 $title = 'Liquid Paint Splatter';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'kgefbnnnfdlffblfgmopnnoodbehafhm';
+$testSearch1 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
