966cd2025555f719910faf811ed73834d2d56a7b
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-27 13:52:44 -0500

    Update index.php

--- anuelaa2/index.php
+++ anuelaa2/index.php
@@ -2,6 +2,7 @@
 $title = 'Anuel AA 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'nlnjofbbaphdimnphknbemolfgegfmle';
+$testSearch1 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
