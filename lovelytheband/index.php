076ff6f0a1d7bf3e4505197d38267bbeb5542ca5
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-27 09:46:40 -0500

    Update index.php

--- lovelytheband/index.php
+++ lovelytheband/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'lovelytheband';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nkaafpcehanajjfjngloeenibpmfpjdm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
