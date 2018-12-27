8417b241992cc21a12c52a45cde9a2278ad54d6b
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-27 13:52:41 -0500

    Update index.php

--- 21savage2/index.php
+++ 21savage2/index.php
@@ -2,6 +2,7 @@
 $title = '21 savage 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'amdinjohmbippdiabadgpigalhbpcigd';
+$testSearch1 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
