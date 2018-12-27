3b242af8fb1b234ec279a7863c81901a38a49150
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-27 09:36:01 -0500

    Update index.php

--- gherbo/index.php
+++ gherbo/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'G Herbo';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'aenfpfdkphmbgelcfchehcanciineicd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
