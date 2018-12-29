b298c5a144555595bf996df749488399405b6165
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-29 11:33:16 -0500

    Update index.php

--- legomovie2/index.php
+++ legomovie2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Lego Movie 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pcpoghhomagikcpppnipelgonldgdpin';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
