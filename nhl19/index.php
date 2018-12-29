5e8bddfa2e9beacc01e0eabca087523765507c9d
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-29 11:37:42 -0500

    Update index.php

--- nhl19/index.php
+++ nhl19/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'NHL 19';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pmofinjippbfogcokcjgkaabgglaalme';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
