850b5222c384d90dff1f92aca95e187f18962e9a
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-29 11:15:21 -0500

    Update index.php

--- santamonica/index.php
+++ santamonica/index.php
@@ -1,7 +1,7 @@
 <?php
-$title = 'Name of your new Project';
+$title = 'Santa Monica';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'olfcfkpechafeglhhcllbhfmcneblnid';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
