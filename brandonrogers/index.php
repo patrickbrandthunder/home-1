8fd1d3093b23a3c3617fd1b1343274c4dfa87c24
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-29 11:18:43 -0500

    Update index.php

--- brandonrogers/index.php
+++ brandonrogers/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Brandon Rogers';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kfmogfinednnffimcidifgpiigngkefn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
