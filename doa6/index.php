c7182c317857444632e887f6d8cc5993591d8d08
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-29 11:23:17 -0500

    Update index.php

--- doa6/index.php
+++ doa6/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Dead or Alive 6';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jgphhjcfppemnblchjmfdmaoknpklpai';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
