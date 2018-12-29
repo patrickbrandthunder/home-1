c13aa8efdcf3ebc714bab76e79745bf5fe82c08b
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-29 11:35:41 -0500

    Update index.php

--- meepcity/index.php
+++ meepcity/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Meep City';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'npehipmiiddagghnagjnpngckabjdcjm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
