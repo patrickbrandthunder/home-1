b3485c3bac86c5d42e0aa9088b069229cf0498d9
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-29 11:46:56 -0500

    Update index.php

--- willhero/index.php
+++ willhero/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Will Hero';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fpecopehhaikcahcabanelgmmpphbdol';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
