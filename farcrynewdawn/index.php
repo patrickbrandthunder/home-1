3c13365b6516b123cc2ae07c23d35d87f12f46a3
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-29 11:24:50 -0500

    Update index.php

--- farcrynewdawn/index.php
+++ farcrynewdawn/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Far Cry New Dawn';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ngabeabhfjgpjbegabofhibammfkdaih';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
