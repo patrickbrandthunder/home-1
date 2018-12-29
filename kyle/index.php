3739c93e6d7beb25ada7f30dac7c2d0f3346df8d
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-29 11:32:48 -0500

    Update index.php

--- kyle/index.php
+++ kyle/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Kyle';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lmjcalcndcmmefpomjmmjlceahfiiiep';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
