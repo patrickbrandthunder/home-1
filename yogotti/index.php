80ce3eba6f4e09f54acc9be2d79866f88f129e0e
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-29 11:47:40 -0500

    Update index.php

--- yogotti/index.php
+++ yogotti/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Yo Gotti';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dppaolimdfibmagnafbdfhlmojjbddgc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
