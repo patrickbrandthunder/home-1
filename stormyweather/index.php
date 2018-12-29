f2ec2e9a4bb7b46c9686c5743f87182cf0fd2f17
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-29 11:42:02 -0500

    Update index.php

--- stormyweather/index.php
+++ stormyweather/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Stormy Weather';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dfpnaofgkjdmjlppfplcadjigjdhkjfp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
