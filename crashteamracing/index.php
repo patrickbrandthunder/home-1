4bfd1c59ab95359ca9a72cbb7df1e1f3b4b60b83
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-29 11:20:10 -0500

    Update index.php

--- crashteamracing/index.php
+++ crashteamracing/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Crash Team Racing';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'befjcdgggkjnpjaplelaagihepjjiopg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
