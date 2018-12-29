eaa69f7552198b6c9d42d27d73b4532cceab80d3
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-29 11:39:46 -0500

    Update index.php

--- runescape/index.php
+++ runescape/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Runescape';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mogljpcfekmbkbmlemigaamgkoaldbfk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
