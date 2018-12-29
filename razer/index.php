2e50aa80958eb777f360f98840ba05f171a3b123
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-29 11:14:26 -0500

    Update index.php

--- razer/index.php
+++ razer/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Razer';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'oajckjjhggohdfjooiiommffhnlpkcnd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
