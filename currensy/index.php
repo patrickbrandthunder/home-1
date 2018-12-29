34943d6c2c7f387768094c31bf99698b0e50a373
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-29 11:20:50 -0500

    Update index.php

--- currensy/index.php
+++ currensy/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Curren$y';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'acccinmfbnpmjakikidegnichfmlibaj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
