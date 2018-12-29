b224f9be4d8e402c2fa0c31415e112b101096351
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-29 11:33:46 -0500

    Update index.php

--- legooverwatch/index.php
+++ legooverwatch/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Lego Overwatch';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ahciccampppcgdbbogfllpjoafkgilol';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
