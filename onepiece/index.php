712d41d16893be25736a209867d37a0eff9b5961
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-30 08:51:40 -0500

    Update index.php

--- onepiece/index.php
+++ onepiece/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'One Piece World Seeker';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gnnbmlniloffnikiabobajmbcpbjfeme';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
