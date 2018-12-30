4d0da259ad2995519bdee7327fe0d968ce313224
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-30 08:52:18 -0500

    Update index.php

--- frenchies2/index.php
+++ frenchies2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Frenchies 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kpojfoohjbolmkfhbgmlmjlhnoamahda';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
