ac0dc7ad2d775444c760e7b406c2abb8205f873c
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-31 17:07:16 -0500

    Update index.php

--- maddenoverdrive/index.php
+++ maddenoverdrive/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Madden NFL Overdrive';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bbbfclgifmmimlngpglnpbhmhdakipib';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
