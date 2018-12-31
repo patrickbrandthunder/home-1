07d070ce31de2c5be2683369ed72effe82bd7b4d
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-31 09:39:00 -0500

    Update index.php

--- asiandoll/index.php
+++ asiandoll/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Asian Doll';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'djkcjbjdedeipoklgdioblmmbhciggpo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
