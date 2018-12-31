76de4dccd834d2c36c273455e4eb8d7d7516b12b
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-31 10:30:41 -0500

    Update index.php

--- cookingfever/index.php
+++ cookingfever/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Cooking Fever';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'malcgocpjkkmlmanpkofcdlplfkobpnl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
