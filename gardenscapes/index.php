ec108de490734d4b8401d2ab501f1b8cdc109dc4
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-31 11:10:56 -0500

    Create index.php

---
+++ gardenscapes/index.php
@@ -0,0 +1,8 @@
+
+<?php
+$title = 'Gardenscapes';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
