889a734ca366d5e6d6068d8418a7fec6765ec025
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-31 11:39:31 -0500

    Update index.php

--- gardenscapes/index.php
+++ gardenscapes/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Gardenscapes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kjlfhajoabgcmeehiiliaijdjcejpndf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
