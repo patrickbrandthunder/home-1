ccfca722adf90aa364be2cc0980518d22a000211
pjmurphy777 <pjmurphy77@gmail.com>
2018-12-31 09:52:14 -0500

    Update index.php

--- bestfiends/index.php
+++ bestfiends/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Best Fiends';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fhedniglffobmkffclicmhlogmeeohno';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
