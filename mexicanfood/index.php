ea0e6d67fed8919e2922ff25f0ef4c6673a261be
Michael Kaply <consulting@kaply.com>
2019-01-03 15:50:11 -0600

    Remove mexican food index for testing

--- mexicanfood/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$title = 'Mexican Food';
-// After extension is loading to the Chrome store, place the extension ID here.
-$extensionID = '';
-$tid = basename(__DIR__);
-$testSearch1 = true;
-include('../homepage.php');
-?>
