462269945241a3a281d8a74d68d09f5b96ec71c8
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-04 14:13:09 -0500

    Update index.php

--- birdbox2/index.php
+++ birdbox2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bird Box 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'coddmneclajjblehelbillmlldjibnlo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
