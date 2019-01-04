6efcc2f38384095057c7cf6d5f193efdcf0ca06b
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-04 11:47:22 -0500

    Update index.php

--- happyglass/index.php
+++ happyglass/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Happy Glass';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ffknbjpokjlahkcfhpmbeoeaekofoghc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
