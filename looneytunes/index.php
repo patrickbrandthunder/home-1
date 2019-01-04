9bc50287eac8a64eaab30c713cb65223b2644661
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-04 13:14:08 -0500

    Update index.php

--- looneytunes/index.php
+++ looneytunes/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Looney Tunes World';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kfkcomgiajoodcpclimcjgogfickkjbj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
