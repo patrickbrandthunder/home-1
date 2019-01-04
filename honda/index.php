a2500276a7840111c8acffe9665b5b7d5a96b5f1
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-04 12:02:47 -0500

    Update index.php

--- honda/index.php
+++ honda/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Honda';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nldfdomlfpngjpghcnndjbicmkhjapjc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
