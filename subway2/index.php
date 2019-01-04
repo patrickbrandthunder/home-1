cc43cd756fd4a9cec93ce4ce0bf150e9dffd214f
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-04 14:32:39 -0500

    Update index.php

--- subway2/index.php
+++ subway2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Subway Surfers 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ckaeffjmeckffhaeonegpekhehalllie';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
