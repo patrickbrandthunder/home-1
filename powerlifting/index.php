1998e2ced377952d91b3bf8a87ba2bca59d77097
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-07 07:41:23 -0800

    Update index.php

--- powerlifting/index.php
+++ powerlifting/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Powerlifting';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nlgpmfnckojbjhfjbglbjmfkmdjhnigj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
