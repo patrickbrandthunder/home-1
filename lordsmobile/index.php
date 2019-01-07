af387632ab4d17d39c297b4886749cca6edc332b
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-06 18:32:02 -0800

    Update index.php

--- lordsmobile/index.php
+++ lordsmobile/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Lords Mobile';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'elifkeangmifpcgjckfijdblofehbpcl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
