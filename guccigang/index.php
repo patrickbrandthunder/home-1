d9d6dde778bdd92b9dfefdd4f3f923d2f21581f1
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-06 17:45:38 -0800

    Update index.php

--- guccigang/index.php
+++ guccigang/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Gucci Gang';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hlehbpefjhegnbifmplpbpnclhoolnhi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
