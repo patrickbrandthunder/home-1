20d5cf0571ce8cf7f684a6d182c203f1c705ea08
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-07 10:23:09 -0800

    Update index.php

--- blackwidow/index.php
+++ blackwidow/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Black Widow';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'palaokbellcfpblgimjjhehongkdjekp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
