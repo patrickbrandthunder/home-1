ab95a7f60bbaf997461094fb5bacf33684d1c01c
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-07 07:29:17 -0800

    Update index.php

--- teamsonic/index.php
+++ teamsonic/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Team Sonic Racing';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ficaefhpkjoboifkjcdhnalliikdionm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
