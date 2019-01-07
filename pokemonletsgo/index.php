5e77bcf70d4c406577a33eee0c3b63bfa3eca575
Michael Kaply <consulting@kaply.com>
2019-01-07 08:42:11 -0600

    Correct title of Pokemon Go

--- pokemonletsgo/index.php
+++ pokemonletsgo/index.php
@@ -1,5 +1,5 @@
 <?php
-$title = 'Pokemon Let's Go';
+$title = 'Pokemon Let\'s Go';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'cmjjkglomefffkfhhmjgidhkflbggjmb';
 $tid = basename(__DIR__);
