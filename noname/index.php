5ea4c4a1abad3e66030bfa3205cfe387fb094292
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-08 15:30:43 -0800

    Update index.php

--- noname/index.php
+++ noname/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'NoName';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mjdodefbpmmnibhfeimlpmglgeejaopi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
