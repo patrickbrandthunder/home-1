cc7dd2794f9f1f88592690724100edf954779f8c
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-09 12:43:28 -0500

    Update index.php

--- awkwafina/index.php
+++ awkwafina/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Awkwafina';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ilomfeccmeegoijpghippiiichcigfma';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
