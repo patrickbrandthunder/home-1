68909985b738e4f532187eb4e23ee0c06f348f5c
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-09 11:04:32 -0500

    Update index.php

--- fordraptor/index.php
+++ fordraptor/index.php
@@ -4,5 +4,6 @@ $title = 'Ford Raptor';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ajaafpfphekjafmbefelcnikfhjgkjeh';
 $tid = basename(__DIR__);
+$testSearch1 = true;
 include('../homepage.php');
 ?>
