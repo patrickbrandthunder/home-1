874c67c119f67cab01b69a483fc8e6a4f048cb93
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-09 13:15:48 -0500

    Update index.php

--- celestial/index.php
+++ celestial/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Celestial Lights';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kifdidmfhmdkfepmgcdmakokmginlonc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
