d2887d70359461f36c4eb5e31732a70a98f1532d
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-09 12:12:25 -0500

    Update index.php

--- nasalogo/index.php
+++ nasalogo/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'NASA Logo';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gcomocnomlgbefikdceifncbbjoangpk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
