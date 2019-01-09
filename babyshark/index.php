00e89a9ae45d2c39539fca66e79c3313b2e2fb5d
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-09 13:30:00 -0500

    Update index.php

--- babyshark/index.php
+++ babyshark/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Baby Shark';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bfegfjjopccopfnkkhgjaghfhnlmijfd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
