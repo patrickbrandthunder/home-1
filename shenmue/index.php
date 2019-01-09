ac36c0237631f6dde76626088c1015570cb6fdde
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-09 16:32:23 -0500

    Update index.php

--- shenmue/index.php
+++ shenmue/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Shenmue 3';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gelfempdamplnmphkgpmpjgkaoagghoh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
