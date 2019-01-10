00abb0fb87ef02f26ba23a7f8996e62b2cbab55f
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-10 10:40:10 -0500

    Update index.php

--- fila/index.php
+++ fila/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Fila';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'niladhlfbogbhhicdjccnbhndgkafoon';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
