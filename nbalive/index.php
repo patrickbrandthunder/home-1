c0823969bb3aabb295bfee8caf27a4aca48bcc9b
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-10 07:40:16 -0500

    Update index.php

--- nbalive/index.php
+++ nbalive/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'NBA Live Mobile Basketball';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'aibfmlnjglfpoijakbkjlkfkhmppchga';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
