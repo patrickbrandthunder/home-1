d5222030e10e90cfcc27f3d15f485d1e11493d4e
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-09 21:29:29 -0500

    Create index.php

---
+++ nbalive/index.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'NBA Live Mobile Basketball';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
