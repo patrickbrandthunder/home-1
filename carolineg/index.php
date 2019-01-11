152020acee07b48cd2ba9d0b729c0952920bb1c3
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-11 09:00:47 -0500

    Update index.php

--- carolineg/index.php
+++ carolineg/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Caroline Gardner';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'glcnaodkancimjekajmjkpkiicmhjook';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
