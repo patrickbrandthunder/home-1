cb3372262ea4a0bf9754e5736fece6de6a1a325f
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-10 22:07:42 -0500

    Update index.php

--- mib4/index.php
+++ mib4/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'MIB 4';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fekdkankjdbbmihagnjenfkbmeikfphf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
