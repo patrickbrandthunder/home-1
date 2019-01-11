c431a7951c844928e88a78598d91a5b9610dd928
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-11 12:00:37 -0500

    Update index.php

--- psychonauts/index.php
+++ psychonauts/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Psychonauts';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lgefpidbhnphldpdooijgllkijnhejhi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
