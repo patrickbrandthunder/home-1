4ece3d50295bde06d19d3a22c00f790502a24ac3
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-11 13:40:51 -0500

    Update index.php

--- polarissling/index.php
+++ polarissling/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Polaris Slingshot';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ghodiogfmbghhjhkimjohalkkheijgdk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
