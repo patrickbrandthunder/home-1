7bfcf7b78398d3d64a7e91fbe4db12f151323c63
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-10 21:42:08 -0500

    Update index.php

--- totalwar/index.php
+++ totalwar/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Total War Three Kingdoms';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cdjclfllmbfgjmifpnmnfpimndjfnfap';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
