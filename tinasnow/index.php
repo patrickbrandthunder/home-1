976ba20adca09f13a3d8f98db5d9cc5727f74c37
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-10 22:07:19 -0500

    Update index.php

--- tinasnow/index.php
+++ tinasnow/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Tina Snow';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'neooadpplohlkcgdcjgabohnlodcbkal';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
