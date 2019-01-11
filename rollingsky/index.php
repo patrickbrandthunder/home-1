1419904717c5a41becff5a9cd97edbc0a535d828
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-10 22:08:09 -0500

    Update index.php

--- rollingsky/index.php
+++ rollingsky/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Rolling Sky';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gnfjkcophdlkpnfoljkchanijnncpehb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
