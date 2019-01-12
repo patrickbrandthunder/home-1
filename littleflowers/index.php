a2c1b9c6b5aa495a7d5c16ff817d51f5febef1a4
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-12 09:21:14 -0500

    Update index.php

--- littleflowers/index.php
+++ littleflowers/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Little Flowers';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'eoinckpbgbminabocoionlfhgkdkbben';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
