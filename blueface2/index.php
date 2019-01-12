9cdc7d0ae213853c593329569a6772d65d59f983
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-12 08:37:53 -0500

    Update index.php

--- blueface2/index.php
+++ blueface2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Blue Face';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lidlhjopdiggfjigcmkafnpkkjcpnbed';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
