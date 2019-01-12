0ba77ec2c8bba690716f26f3a28224c58ba2d7fa
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-11 20:49:49 -0500

    Update index.php

--- liltoenail/index.php
+++ liltoenail/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Lil Toenail';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ihdfaloandlengedigifaicaahikjlio';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
