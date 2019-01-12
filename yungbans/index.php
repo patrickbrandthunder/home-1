c184b5e3f9904a4e9cad45adf0a91c91cd642b68
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-11 20:23:49 -0500

    Update index.php

--- yungbans/index.php
+++ yungbans/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Yung Bans';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'iifnlandagokbglndelalfkdkfdipcan';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
