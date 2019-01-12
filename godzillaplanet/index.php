1878468af51c44b50bdba016565eb90b7c9d37c4
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-12 16:53:14 -0500

    Update index.php

--- godzillaplanet/index.php
+++ godzillaplanet/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Godzilla Planet Eater';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'clmejdfpgelekjccfkhadkdnccfpmhbi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
