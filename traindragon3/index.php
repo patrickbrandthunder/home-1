cfd5456cdb36dd6d4f8ed399d7607539896de15e
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-13 15:45:52 -0500

    Update index.php

--- traindragon3/index.php
+++ traindragon3/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'How To Train Dragon 3';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fajdbccjfkflnbfoobgbncilaeoefebk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
