bca51f17fa93cee4d53595943f721de8b25e6cfa
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-13 15:19:29 -0500

    Update index.php

--- boxingstar/index.php
+++ boxingstar/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Boxing Star';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hccakacehcedfllphffplikhckmnejbl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
