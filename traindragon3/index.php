fc93e3facd720115a6cb9abb6c0f798d7443fdd7
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-13 15:24:57 -0500

    Create index.php

---
+++ traindragon3/index.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'How To Train Dragon 3';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
