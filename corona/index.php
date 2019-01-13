b49130e4fe8f18184d5f517a452d4a9f146f30a0
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-13 15:32:15 -0500

    Update index.php

--- corona/index.php
+++ corona/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Corona Beach';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'npfjjlbeoipbofdfdojmkdmhkoflgaoj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
