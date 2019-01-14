81b6cc0f94b3edafdad7524ea59bb235f2584791
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-14 14:32:11 -0500

    Update index.php

--- goldshards/index.php
+++ goldshards/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Gold Shards';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'moncmfcmmkndjcdkehmmnojgmpdiiemb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
