93d0675eed00c79bdcc38d89caff6bbd5bca1fa0
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-15 10:21:23 -0500

    Update index.php

--- alanwalker/index.php
+++ alanwalker/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Alan Walker';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'llieaakohlpppmddpgcdnnkejglcffel';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
