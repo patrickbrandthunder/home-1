e2686bbf3099d64f18e9abc74be5a00ef436468f
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-15 08:35:56 -0500

    Update index.php

--- fordf150/index.php
+++ fordf150/index.php
@@ -3,6 +3,5 @@ $title = 'FORD F150';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'fhgnpnmokcaijdjijahgkpkdciiibila';
 $tid = basename(__DIR__);
-$testSearch1 = true;
 include('../homepage.php');
 ?>
