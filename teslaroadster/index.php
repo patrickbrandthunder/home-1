b627c481e10714cf3d1793f5e98f5408bc142b27
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-15 14:53:59 -0500

    Update index.php

--- teslaroadster/index.php
+++ teslaroadster/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Tesla Roadster';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mnagcbdappbmgmjcjkohfnfaoadjgned';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
