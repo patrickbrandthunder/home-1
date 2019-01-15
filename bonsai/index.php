90497301aeede8aa7444df0e4ca4828f979bc1b8
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-15 14:53:17 -0500

    Update index.php

--- bonsai/index.php
+++ bonsai/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bonsai';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ondalipdjkfdeflhpjjkblmdgailifgm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
