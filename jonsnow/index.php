01ea67a358f6212085e685780ee049942975a9e5
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-15 20:46:53 -0500

    Update index.php

--- jonsnow/index.php
+++ jonsnow/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Jon Snow GOT';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kdfkdgdaenbonjfpdbbfekcddcblknof';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
