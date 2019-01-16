49310a7aa6eb87f3beccbddabaec1dbb3c6cf9a3
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-15 20:59:27 -0500

    Update index.php

--- codzombies/index.php
+++ codzombies/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'COD Zombies';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'felobeimcofopkbigifkiibhieeppkjh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
