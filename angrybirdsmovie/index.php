dd1ce088f8d241950bf6d65604f8f4a3569fed5c
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-15 20:44:25 -0500

    Create index.php

---
+++ angrybirdsmovie/index.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'Angry Birds Movie 2';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
