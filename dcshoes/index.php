36dbb7a9ee72a9f09b0832c75c7f7bb3d33c1e44
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-16 16:52:40 -0500

    Update index.php

--- dcshoes/index.php
+++ dcshoes/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'DC Shoes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kdheemafnpmladedapcdpdpjhbkkolgp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
