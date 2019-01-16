9601bb79c6b1531c847e07f992ddb4e8a7de485e
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-15 22:04:41 -0500

    Update index.php

--- afrojack/index.php
+++ afrojack/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Afrojack';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hlpgnpinkjmigbcgneflefdpjmheejon';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
