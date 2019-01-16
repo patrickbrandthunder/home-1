d13441359ce2392648c5cacce833ded1e76bf697
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-16 09:58:35 -0500

    Update index.php

--- spelunky/index.php
+++ spelunky/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Spelunky 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jiongpmghpdfdnljpfglmkljnjllcbpm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
