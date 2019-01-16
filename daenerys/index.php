427d4864f1d0eeae8e0aa29fc8814bbb9092c6a6
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-15 20:35:20 -0500

    Update index.php

--- daenerys/index.php
+++ daenerys/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Daenerys';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jdkcknddfcglnlkcgbkogdkpadnpddhj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
