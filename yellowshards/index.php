a7481850b9034e0140952afc16063753f5ff11a5
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-16 09:14:15 -0500

    Update index.php

--- yellowshards/index.php
+++ yellowshards/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Yellow Shards';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jkaajfolbmjfikciaefcikcimbgdmaoa';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
