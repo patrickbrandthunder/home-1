4b26e2d52134cb3a209ccde9ee25449db5309d4e
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-16 16:22:17 -0500

    Update index.php

--- mergeplane/index.php
+++ mergeplane/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Merge Plane';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nfjehleonhfjhglbgpigcnlpkdompbkf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
