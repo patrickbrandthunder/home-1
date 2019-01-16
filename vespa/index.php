f219495737d9237d069b59639cfb14ee28c44583
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-16 09:01:08 -0500

    Create index.php

---
+++ vespa/index.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'Vespa';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
