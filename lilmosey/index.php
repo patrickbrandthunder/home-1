5884ff0e3c8770352a2155001d75a3eb09da1a8c
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-17 17:47:22 -0500

    Update index.php

--- lilmosey/index.php
+++ lilmosey/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Lil Mosey';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bdjikpfonkacpcnidcjgfgbnpcndagmh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
