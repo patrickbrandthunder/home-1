c890ff3313947287a8e624ef202ab111d28e60c6
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-17 18:37:48 -0500

    Update index.php

--- babylonfall/index.php
+++ babylonfall/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Babylon's Fall';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'amganocpfflopfegnieffdelfgphcckp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
