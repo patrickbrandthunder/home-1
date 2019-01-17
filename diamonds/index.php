a8fcdbd7f65ce052f79c3a4a53220dea4fa138d0
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-17 15:14:06 -0500

    Update index.php

--- diamonds/index.php
+++ diamonds/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Name of your new Project';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pfnecgbhmiaglgmjojjlhcdifmbgamfg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
