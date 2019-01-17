5fb7e5dc4052107f561cce22d3d49bd09357e3b5
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-17 18:38:11 -0500

    Update index.php

--- beybladeburst/index.php
+++ beybladeburst/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Beyblade Burst Evolution';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fcegkkbmojofhgflmkdipfaadecdfaio';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
