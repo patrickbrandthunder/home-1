95e1f11674d68f42369f3aaf020f8f08d8e7a153
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-17 15:02:18 -0500

    Update index.php

--- ybncordae/index.php
+++ ybncordae/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'YBN Cordae';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mmilhmppkcajpafohhaakgndgfjlldfd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
