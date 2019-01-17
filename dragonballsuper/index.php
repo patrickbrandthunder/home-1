35a54fcfd35106410bc855e7c5e8638e8fb156ac
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-17 17:06:26 -0500

    Update index.php

--- dragonballsuper/index.php
+++ dragonballsuper/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Dragon Ball Super';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dndbfhhfkhikpcmhggnbecjdoldldepk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
