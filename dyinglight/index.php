acf9976c6cec0864256f5c9a3cd5dbb0fd8d7de0
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-17 16:53:41 -0500

    Update index.php

--- dyinglight/index.php
+++ dyinglight/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Dying Light 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kcpopgefclpflnlajckfihcnoljealbn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
