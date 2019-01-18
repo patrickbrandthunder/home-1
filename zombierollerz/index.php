83d2382d416af883402e42efd1fc50f3499fb8ec
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-18 11:06:49 -0500

    Update index.php

--- zombierollerz/index.php
+++ zombierollerz/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Zombie Rollerz';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'moadpaiicjnmdclddkopiajblceelhco';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
