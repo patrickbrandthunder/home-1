e72f2d87a0458cc2fcc3a1868bfb3ce2c4601711
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-18 11:06:28 -0500

    Update index.php

--- wisen/index.php
+++ wisen/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Wisen Yandel';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dpkkcoobmdkmjebboihkjmollfpjpdjf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
