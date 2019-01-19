00a8bb7f32efb9f37283b4e7561e6f6a5dec8dca
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-19 16:14:24 -0500

    Update index.php

--- amazingfrog/index.php
+++ amazingfrog/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Amazing Frog';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bmehpmchmpjemhidofmeohcghdbimmgk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
