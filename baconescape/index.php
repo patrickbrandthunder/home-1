afd73ac64a77a510223454df134933388f63c4cf
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-19 16:14:53 -0500

    Update index.php

--- baconescape/index.php
+++ baconescape/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bacon Escape';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'poendlfpdhlbiadncjpeobncfjjgdoij';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
