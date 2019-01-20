a89dee68b8de5ef601e9f0e5ca18589a688e05fa
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-19 19:40:21 -0500

    Update index.php

--- cloudpunk/index.php
+++ cloudpunk/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Cloudpunk';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ijdphkfhbngmoonlcecmkkhghgofocei';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
