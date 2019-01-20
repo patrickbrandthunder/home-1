b3161ef95b7c7ddb79be9363b4f080df9fb81189
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-20 08:46:30 -0500

    Update index.php

--- concretegenie/index.php
+++ concretegenie/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Concrete Genie';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'olmjlehfflohlnkbmgpcijajipicdmid';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
