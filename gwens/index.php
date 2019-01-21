91fe4b917772b0dc33d04f22515cbd013ab82e85
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-21 16:57:43 -0500

    Update index.php

--- gwens/index.php
+++ gwens/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Gwen Stefani';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hgbafaknoplggnioeipjmgjgbmiamnfe';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
