214dea80fdbe1ff8bc596c8f1099f3a7dc211eea
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-21 15:05:23 -0500

    Update index.php

--- layersoffear/index.php
+++ layersoffear/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Layers of Fear 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jnbckcffnbgieelemagjpjkcgjaljboi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
