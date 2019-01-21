3f0a2d4cff8297589c7a7f01a2fb1f0ef748b51f
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-21 15:06:13 -0500

    Update index.php

--- mainecoon/index.php
+++ mainecoon/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Maine Coon Cats';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dfgmngmhehkfchehdennbblfldmieeoj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
