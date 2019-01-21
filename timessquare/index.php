0cba646534ee3449184a17071f6bd68d64645e33
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-21 10:38:14 -0500

    Update index.php

--- timessquare/index.php
+++ timessquare/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Times Square';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'baelfjnfchhaegeegfiljjickoejioek';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
