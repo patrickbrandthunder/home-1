87f652e302ef8da26878c7ffc680d57a74cd43a6
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-21 11:38:25 -0500

    Update index.php

--- angrybirds2/index.php
+++ angrybirds2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Angry Birds 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ainkegjoacdebbeiecclffipckgfalok';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
