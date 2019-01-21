ef77cd8fe1bb0b525604bd22cdc4507908799d6a
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-21 09:55:12 -0500

    Update index.php

--- ygtut/index.php
+++ ygtut/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'YGTUT';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lneebpljcompnbcjpepkfcoehnjibpfi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
