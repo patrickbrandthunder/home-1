a5946f29400d987f8d88899b74b80a2a04234da8
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-21 16:19:43 -0500

    Update index.php

--- plumage/index.php
+++ plumage/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Plumage';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fdjdmmbgnjlokbkkocbcdfbmkfaggoip';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
