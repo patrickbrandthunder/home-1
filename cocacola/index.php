1bce1d947696f35f1b8178ee2afb9448e6507a0d
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-21 14:05:29 -0500

    Update index.php

--- cocacola/index.php
+++ cocacola/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Coca Cola';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pdfkpcccimifbfcmeegcjcegcbpofmpp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
