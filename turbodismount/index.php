3a33ec078216b4210717ff3b1d279275c9f044e2
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-21 10:27:05 -0500

    Update index.php

--- turbodismount/index.php
+++ turbodismount/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Turbo Dismount';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'koalpkfkeibfnioolpifkihniloipokh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
