1c2f566b2598b31793e9bb758c1d3a6a6a445ca1
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-21 11:23:59 -0500

    Update index.php

--- sinkingcity/index.php
+++ sinkingcity/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Sinking City';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cmnhelgdkmcleelghlkejpbhmddpdeah';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
