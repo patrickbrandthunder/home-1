47a7e5f199e7d178a2b2d8dfe27819e281c65d2b
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-21 18:51:29 -0500

    Update index.php

--- hookedinc/index.php
+++ hookedinc/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Hooked Inc';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nkljgmgbnkmeddakocmpmbklldhkhcci';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
