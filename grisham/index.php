360f3122dc7eeee27712a921dc9a38f19379de2b
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-22 16:30:58 -0500

    Update index.php

--- grisham/index.php
+++ grisham/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'John Grisham Quotes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'eddcjfjgdbalhcdihpnacjkakeadonlc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
