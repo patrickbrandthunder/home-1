ae38ffc7e0603c32bf3b1ab634d1517f3fdbc2ea
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-22 14:02:08 -0500

    Update index.php

--- summoner/index.php
+++ summoner/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Summoner';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'deognekjdkljbfccjhdmkjdgaebnjlgn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
