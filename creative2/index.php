3422913abb21b292a9d6fd84769a35fe53352151
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-23 08:31:02 -0500

    Create index.php

---
+++ creative2/index.php
@@ -0,0 +1,8 @@
+
+<?php
+$title = 'Creative Destruction 2';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
