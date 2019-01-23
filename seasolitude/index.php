218deec8fbbcb902f21107d1d691d396f54d34c3
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-23 12:13:29 -0500

    Update index.php

--- seasolitude/index.php
+++ seasolitude/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Sea of Solitude';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'iicmngghgamfoafnbjodfgpnilaiakoh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
