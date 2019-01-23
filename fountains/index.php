3a573cdbdbb45514dec193a91c9f9f17aa284c9c
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-22 20:40:46 -0500

    Update index.php

--- fountains/index.php
+++ fountains/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Fountains';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lpkgpkhfnjkfmehbpbcnbeopfampmimf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
