f85d84195feff40f4894f2828e63772f58aa9e71
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-23 15:06:49 -0500

    Update index.php

--- armymen/index.php
+++ armymen/index.php
@@ -1,7 +1,7 @@
 <?php
-$title = 'Name of your new Project';
+$title = 'Armey Men';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'igpacfmlkjnmiibeokddbpjffcfglada';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
