1ac1e4b39f2ed65af5efabe90ced5415caeb8f18
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-23 12:39:42 -0500

    Update index.php

--- marshalld/index.php
+++ marshalld/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Marshall From Detroit';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lbhjpiojmodnadnaffcghglheajlhche';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
