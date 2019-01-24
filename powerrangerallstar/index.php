116383c910d610a755125f8eab756ba6cd9f76f5
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-24 10:49:29 -0500

    Update index.php

--- powerrangerallstar/index.php
+++ powerrangerallstar/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Power Rangers All Star';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ejdlihokbhkopebnkffjcijjcdggkgbg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
