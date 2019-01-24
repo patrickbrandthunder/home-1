928e857b6bb3aa5b90c3e6acc58d07fd4a4817e6
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-23 19:43:05 -0500

    Update index.php

--- shadowofdeath/index.php
+++ shadowofdeath/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Shadow Of Death';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kdijddlikdphoiajnjkiojbgggielklf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
