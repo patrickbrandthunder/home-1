1aa2f661520b8a6d2d053b2c5d2cd8536089f6e7
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-23 20:42:25 -0500

    Update index.php

--- himalyancat/index.php
+++ himalyancat/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Himalyan Cats';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lhdmphfhlmeageojiifhmkgcoleapjpd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
