6a6c38d50042dc6ea2a812cd45d60479d6255744
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-24 10:08:49 -0500

    Update index.php

--- fyre/index.php
+++ fyre/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Fyre Festival';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'chnlmjhecnmanfpeoljelbkpmfbjnpmj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
