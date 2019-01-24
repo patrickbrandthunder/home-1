6237f5553c02640eb8d19dbf64b5f1a82bb4ea30
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-23 21:01:45 -0500

    Update index.php

--- gold/index.php
+++ gold/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Gold';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ocgiahigdoohjmicamgmbpeaeamgimcn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
