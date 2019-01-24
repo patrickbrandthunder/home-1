7af06df0d896f9a648b8dd07bb5d6a720fdd7aaf
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-24 08:16:13 -0500

    Update index.php

--- aventura/index.php
+++ aventura/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Aventura';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'aealmcmigodnkfgkojeinichcjfboafl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
