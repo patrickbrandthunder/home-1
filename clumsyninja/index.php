6326fb407ef1792a5573b6d26eb0bc0d9ba3cfb0
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-24 09:17:30 -0500

    Update index.php

--- clumsyninja/index.php
+++ clumsyninja/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Clumsy Ninja';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'odilejojfibcieejjkebakfakknfhgab';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
