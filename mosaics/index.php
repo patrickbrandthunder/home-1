c021f825daf744a15f8d46c48d6e8d9bef33e1c5
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-23 19:43:58 -0500

    Update index.php

--- mosaics/index.php
+++ mosaics/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Mosaics';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'laeeppnmkekkblnbmkdpniidgfahimjh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
