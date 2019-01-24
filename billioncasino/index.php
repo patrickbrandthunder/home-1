f41490e3411eed0318d5ff6805c3b67af6e54097
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-24 08:45:00 -0500

    Update index.php

--- billioncasino/index.php
+++ billioncasino/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Billionaire Casino';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jlocomfpinempmjdjdogmdagcfleicih';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
