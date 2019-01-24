0ab3a79e0671a474ad8b93868aeffbc8cd2d9c84
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-24 08:44:37 -0500

    Update index.php

--- beefactory/index.php
+++ beefactory/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bee Factory';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'okifnlolfmijcbldjgancpacnefldpbk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
