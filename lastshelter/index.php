2fb09097dcbc9bc4ca90d5c86eb0b295562d31e0
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-23 20:18:06 -0500

    Update index.php

--- lastshelter/index.php
+++ lastshelter/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Last Shelter';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cpffdbinfmianmdoklfepfipoihihfbj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
