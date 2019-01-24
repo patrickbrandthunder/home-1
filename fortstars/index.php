490cba91fc3f49cca3718cf7fa7018302ebece50
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-24 07:52:48 -0500

    Update index.php

--- fortstars/index.php
+++ fortstars/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Fort Stars';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pgkpijphjelbbiiceocigjcebpcjnmej';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
