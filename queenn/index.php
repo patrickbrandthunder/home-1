198a5b62f3760614c0da94baa4dce2798bcba8cf
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-24 11:13:08 -0500

    Update index.php

--- queenn/index.php
+++ queenn/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Queen Naija';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'onllloggibfgkdhgfkhfpljamdapjchi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
