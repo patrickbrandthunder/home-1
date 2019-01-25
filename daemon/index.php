7340056bc7231fefe1e5b88362ba7b20a4c79014
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-24 21:49:44 -0500

    Update index.php

--- daemon/index.php
+++ daemon/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Daemon x Machina';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ckfogcjfokokggobacioglcclpaphhil';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
