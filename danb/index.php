e53c6434dffec3af64f22b3b6c5e48211a1aea20
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-24 22:09:27 -0500

    Update index.php

--- danb/index.php
+++ danb/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dan Bilzerian';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lapihmcfdbakolejpafkgmnoooilpobg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
