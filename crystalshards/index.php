66c15cf319ad03c5da71b98915f69b0b1c7a2c18
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-24 21:26:38 -0500

    Update index.php

--- crystalshards/index.php
+++ crystalshards/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Crystal Shards';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ehfdbdmckgfonpdnfapiionaejomjdff';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
