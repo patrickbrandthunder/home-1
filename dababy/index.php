18d71510629569fc44f1f6a5070c1dfd6372d8cd
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-25 15:46:49 -0500

    Update index.php

--- dababy/index.php
+++ dababy/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'DaBaby';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pllfhkkgbgjbaknlamkfbpfjcefjfdca';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
