b4543b6529a68d7ca07b4123eb49a3fc9441df7e
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-25 11:31:51 -0500

    Update index.php

--- daniellesteele/index.php
+++ daniellesteele/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Danielle Steele';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kohagaihlkmmihknhjfjloclhjamgmnk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
