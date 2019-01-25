542cc948d4ec5afc7ea1d6b2249ae6fe0fa1da30
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-24 21:21:15 -0500

    Update index.php

--- aretha/index.php
+++ aretha/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Aretha Franklin';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hcdklaleakonibcljfkddiiekkmcjfpa';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
