0cf30d629cd76e2bd9d253c05c67b5f0808285cf
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-25 17:50:37 -0500

    Update index.php

--- johndeere/index.php
+++ johndeere/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'John Deere Farming';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dmhcklmgoekigjinailofhnemcombjlo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
