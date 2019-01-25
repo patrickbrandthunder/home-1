cc109280943a29ab6b5b57fb8168daaa5ba3e945
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-24 21:26:03 -0500

    Update index.php

--- clue/index.php
+++ clue/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Clue';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'icepfodolhejmccnhjmcnagjejnnaijf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
