9eab741cecc37748aac321dbb873dbcf899ae241
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-27 13:28:15 -0500

    Update index.php

--- theroom/index.php
+++ theroom/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'The Room';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gheobgloholpcbbncjgbebbjkdionaog';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
