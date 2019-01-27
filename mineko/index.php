dd19b9c4f338d64154c80789eeff4983cb8c007d
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-27 11:35:41 -0500

    Update index.php

--- mineko/index.php
+++ mineko/index.php
@@ -1,7 +1,7 @@
 <?php
-$title = 'Mineko's Night Markey';
+$title = 'Minekos Night Market';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gpncffbkfempglimnngopkjjkpoojkoh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
