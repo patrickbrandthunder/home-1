063a7795455e6daec76ff33a39eddf3f6fb7ed5c
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-27 16:23:59 -0500

    Update index.php

--- junejourney/index.php
+++ junejourney/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'June's Journey';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kiekliglfbbjhaoepkjnnflpppofojfe';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
