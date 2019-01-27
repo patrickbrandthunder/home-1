d3895429bbcaa1d13ff627b15e4aeac64d36b3d9
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-27 10:57:24 -0500

    Update index.php

--- wutangclan/index.php
+++ wutangclan/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Wu-Tang Clan';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nklomjgeimigipibligeoaliliaengmb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
