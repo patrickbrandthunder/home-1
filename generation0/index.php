672edbf15f7a299321aa57e7abb7e42b59ac44da
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-27 15:04:22 -0500

    Update index.php

--- generation0/index.php
+++ generation0/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Generaton Zero';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cjclojlbiolgmifpbgccapnbbhmibabc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
