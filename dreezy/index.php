5ebbcecca7547d075c87d7e4d92b00acc1bb9ade
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-27 11:36:06 -0500

    Update index.php

--- dreezy/index.php
+++ dreezy/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dreezy';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ieckheopcmfnfoihlbogiodfhjeahhaj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
