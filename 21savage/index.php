87af216e35df7b5f8d9c20c0bc6f562c723c7cfc
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-28 08:52:50 -0500

    Update index.php

--- 21savage/index.php
+++ 21savage/index.php
@@ -2,7 +2,7 @@
 $title = 'Savage 21';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'fmppfellegpidlnncdpogkekhjlncdla';
-$testSearch1 = true;
+$testSearch2 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
