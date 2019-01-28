4fb5dbf62ff7e0aee8bcdd5cf9570774c6f9b909
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-28 09:33:54 -0500

    Update index.php

--- corsair/index.php
+++ corsair/index.php
@@ -3,5 +3,6 @@ $title = 'Corsair';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'jekhlbebjagfjfgephdehojdjklciekf';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
