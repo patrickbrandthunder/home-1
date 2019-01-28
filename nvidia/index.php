a0e1604f53d32c55f5280eb7b8372c061aec83f5
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-28 14:42:46 -0500

    Update index.php

--- nvidia/index.php
+++ nvidia/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'NVIDIAt';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jahabilkibhiaeijoailcbmghbidangf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
