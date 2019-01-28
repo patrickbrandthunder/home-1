d231d12d8d67a015ac8bbe96a092410af2c9d02a
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-28 08:35:49 -0500

    Update index.php

--- djkayslay/index.php
+++ djkayslay/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'DJ Kay Slay';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nanljdmbaikgnokhiopjpajckfjmmohm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
