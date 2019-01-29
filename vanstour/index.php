b938a25f4963796f2aeb2c128e43c35f4828cc4e
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-28 20:25:03 -0500

    Update index.php

--- vanstour/index.php
+++ vanstour/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Vans Warped Tour';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lpdnedjhcpalcgkbhgbafbfgpodohmjj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
