89ded573a6fad69672fa2906850ceecf5b050d9c
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-29 09:24:28 -0500

    Update index.php

--- disneyblitz/index.php
+++ disneyblitz/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Disney Emojis Blitz';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gmikakpamjljgphiakhhfhofobbohomc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
