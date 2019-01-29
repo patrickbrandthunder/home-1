0425bf74024d2b988bcd122cce8dc930b412a7d1
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-29 10:14:35 -0500

    Update index.php

--- yahtzee/index.php
+++ yahtzee/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Yahtzee';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mgfajnmkpgecglmfhmkoimpmiofonmfk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
