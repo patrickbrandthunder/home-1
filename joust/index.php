49f5133f7a1633ed0c618b8a72f84374ba904783
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-29 16:59:23 -0500

    Update index.php

--- joust/index.php
+++ joust/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Joust';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pacafijodjjjdhibaofgphocoeaaebmh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
