a4c16a1c726368835af11fcbb6f4e264b6970f09
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-29 07:26:21 -0500

    Update index.php

--- langrisser/index.php
+++ langrisser/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Langrisser';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kceflggcecgmlemmmjeekmpggokolobo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
