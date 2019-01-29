0feee65e7da44db846ef793c84ea2599ddf09158
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-29 10:29:22 -0500

    Update index.php

--- stickmanhook/index.php
+++ stickmanhook/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Stickman Hook';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nfapocingfcbjbbodjlgajkfpddgoecp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
