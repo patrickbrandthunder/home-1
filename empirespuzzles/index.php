b7292197f1f647ca76ad24d37bd4b61950221fa9
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-29 10:01:56 -0500

    Update index.php

--- empirespuzzles/index.php
+++ empirespuzzles/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Empires and Puzzles';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'idmpajkphfmleilndnhmaeaaocphjodm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
