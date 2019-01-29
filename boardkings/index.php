0442673bae7d0260f0ec3adf098d195ce59106e7
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-29 09:02:37 -0500

    Update index.php

--- boardkings/index.php
+++ boardkings/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Board Kings';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kaiomicgcnhkhkjnjnliekgfdbnmgodb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
