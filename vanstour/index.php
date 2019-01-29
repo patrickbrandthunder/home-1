c6ee8ffb0f500f8fa40dd19a0c6ca37f085b7cc7
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-28 20:04:26 -0500

    Create index.php

---
+++ vanstour/index.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'Vans Warped Tour';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
