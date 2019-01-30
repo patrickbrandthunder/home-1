ecd4e5524dad74ec886ab33b3cc064fb0b55ed4b
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-30 11:10:38 -0500

    Update index.php

--- homescapes/index.php
+++ homescapes/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Homescapes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jbejefpomgpcfcknecibnmfenjoffdmc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
