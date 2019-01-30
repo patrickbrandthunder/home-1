27e42f377e7bc67aa980994823b0ec6d29c442bc
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-30 17:01:25 -0500

    Update index.php

--- candyhearts/index.php
+++ candyhearts/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Candy Hearts';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nodibiadjbkhlaolnonbmpoalbnhomli';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
