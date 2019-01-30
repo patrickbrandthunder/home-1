4719b359840cba6e649e144354709e89ef83bb5c
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-30 10:50:07 -0500

    Update index.php

--- garena/index.php
+++ garena/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Garena';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'odahhnlngcfjfmbciedbphgbebhfdloe';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
