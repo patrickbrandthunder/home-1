34bfd911a792b52456840b15747c471df050bbdf
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-30 10:47:28 -0500

    Update index.php

--- furistas/index.php
+++ furistas/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Furistas';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ddjffbilljbpfkpmgopdimgknbnpehdl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
