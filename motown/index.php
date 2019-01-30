f9fd6e0dca5eb9098bbe20f9bac379057cdc7dec
pjmurphy777 <pjmurphy77@gmail.com>
2019-01-30 14:23:49 -0500

    Update index.php

--- motown/index.php
+++ motown/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Motown';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fchpnhoajnmhjneldilpaeclcepdemfo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
