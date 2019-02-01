d0490cdd2586bce751ddd2c782f91f747ae56000
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-01 11:58:17 -0500

    Update index.php

--- alita/index.php
+++ alita/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Alita Battle Angel';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lniilebjbkojfoigdhlcdjakccfikllc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
