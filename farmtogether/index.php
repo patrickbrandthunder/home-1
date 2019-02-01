ac20027ec2d56528097713609c769edb21a80396
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-01 12:23:03 -0500

    Update index.php

--- farmtogether/index.php
+++ farmtogether/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Farm Together';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'oeciafndcmbnlkfplcolgnpdjolfanib';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
