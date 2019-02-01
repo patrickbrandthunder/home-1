ae7cbed472caac80c78344e8fc0a4f6b7878689f
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-01 13:26:51 -0500

    Update index.php

--- mariokarttour/index.php
+++ mariokarttour/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mario Kart Tour';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ommgmglinlllbicchadehmdcmjkgdecf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
