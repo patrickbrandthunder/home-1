4c5e40c5b8ecdb06c83d3dedd9dcf420e33cb32f
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-01 20:00:10 -0500

    Update index.php

--- bidwars/index.php
+++ bidwars/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Bid Wars';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ejeecljhoidhnmkcdhbmidjilanjaenp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
