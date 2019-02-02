4f30acc23f67a9ecc6a4a9fdafd97ea93c4284d6
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-01 20:25:49 -0500

    Update index.php

--- lorengrey/index.php
+++ lorengrey/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Loren Grey';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'obdcnfemignndokhfchjfgobpdjmdkip';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
