d077a3782235ddb7635a4762a52a38ccbbab3779
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-02 10:08:17 -0500

    Update index.php

--- qdafool/index.php
+++ qdafool/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Q DA FOOL';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mdocgoaljfmaijafihpmlbabnpffgoik';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
