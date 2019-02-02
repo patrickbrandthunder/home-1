229fc319745561cbc64aae47e413ce07a1f16565
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-02 10:08:34 -0500

    Update index.php

--- tsusurf/index.php
+++ tsusurf/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'TSU Surf';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'oamfjgabfdknpdhhhpdoomgmdnmklgde';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
