a091aa11adcb958edfd200b715ca3e70f367b515
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-02 16:45:33 -0500

    Update index.php

--- drmario/index.php
+++ drmario/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dr. Mario World';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fjcgcedppbblaafngcjejnigmjkffpac';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
