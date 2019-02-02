128ef1a002f3817a0cb2291d9bc3f635cafc4e82
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-01 20:25:21 -0500

    Update index.php

--- bingoparty/index.php
+++ bingoparty/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bingo Party';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pcefppocloigfgpjjaoglgpjcehkflga';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
