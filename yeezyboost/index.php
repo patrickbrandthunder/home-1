ddcbcc5e9d6e0ecbd33542f132620bf1595bd5fd
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-03 10:14:34 -0500

    Update index.php

--- yeezyboost/index.php
+++ yeezyboost/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Yeezy Boost';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hbhgibfeemfombcjcbhjmfopkdgllkik';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
