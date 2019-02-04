f36dfca92fc9f95c7d639ae0f12eec78ef100329
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-04 10:00:11 -0500

    Update index.php

--- artconquest/index.php
+++ artconquest/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Art of Conquest';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mokebbdcfebmhbgnkfcjimjnglibfeld';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
