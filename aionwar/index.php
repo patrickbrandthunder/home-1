bbad911ee46476e81b8a26be248ead44b80b8a3b
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-04 09:07:09 -0500

    Update index.php

--- aionwar/index.php
+++ aionwar/index.php
@@ -1,5 +1,5 @@
 <?php
-$title = 'Aion Legends of War';
+$title = 'Aion Legions of War';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = '';
 $tid = basename(__DIR__);
