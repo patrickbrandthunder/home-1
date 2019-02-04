b7c431516ac261bcf8f04fb05d10641eb431865f
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-04 18:52:50 -0500

    Update index.php

--- wonderpark/index.php
+++ wonderpark/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Wonder Park';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gbhjefnocmgkfpdgknfcckffgbaemkin';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
