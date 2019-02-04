613e413c2bcb805dce5500cf11bdff4d27a76481
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-04 17:36:15 -0500

    Update index.php

--- apexlegends/index.php
+++ apexlegends/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Apex Legends';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pmnncbopkljhlkofieepamgdboofjaje';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
