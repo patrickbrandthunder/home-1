49aa61bdd5180fc7f0e0fd96279718c915cc4b00
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-04 13:07:10 -0500

    Update index.php

--- casinotycoon/index.php
+++ casinotycoon/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Casino Tycoon';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bcjlgdhdnmjpcmmccaoakdlheinjneme';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
