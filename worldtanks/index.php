b507819354b8e46b9fffe6d3a3029ca1444a5cca
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-04 18:53:16 -0500

    Update index.php

--- worldtanks/index.php
+++ worldtanks/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'World of Tanks';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dbapfjkpoodecafkejonfijdchfmodhc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
