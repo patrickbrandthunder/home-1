2083d63d3a2c4804c0f7c83da3df256cd5dc9659
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-04 09:16:48 -0500

    Create index.php

---
+++ anothereden/index.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'Another Eden';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
