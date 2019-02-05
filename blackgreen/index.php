07365cbea986d5f5bf2ad849a2a1e3d7a3719b8c
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-05 09:01:37 -0500

    Update index.php

--- blackgreen/index.php
+++ blackgreen/index.php
@@ -3,6 +3,6 @@ $title = 'Black Green Shards';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'gohenckfoabhdnnclfoibodhdjddfjek';
 $tid = basename(__DIR__);
-$testSearch3 = true;
+$testSearch2 = true;
 include('../homepage.php');
 ?>
