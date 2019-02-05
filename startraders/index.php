35fcd7d06fb46fc5268db16748fde67ea6e84351
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-05 16:15:47 -0500

    Update index.php

--- startraders/index.php
+++ startraders/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Star Traders';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mofcknofkmdgegnhhlnhlfdjmafbhbid';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
