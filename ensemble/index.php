fa3f66149e1782e22a2894816cf37a6f6d951ab9
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-05 08:55:35 -0500

    Update index.php

--- ensemble/index.php
+++ ensemble/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Ensemble Stars';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ogllffdmlimfjekfgemcnhckebpgkajd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
