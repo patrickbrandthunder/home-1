fe0b447f94e9654cd9a6b99ea64f34cd4620f289
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-05 16:51:33 -0500

    Update index.php

--- octodad/index.php
+++ octodad/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Octodad';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gnjimjlncehcabmlcjibmgdiaioaolna';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
