5a2d28aad0ccb0bd359e20485ae9eb256d676490
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-05 08:33:47 -0500

    Update index.php

--- donutcounty/index.php
+++ donutcounty/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Donut County';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mfgclpjnnaajeanogonmjaomnimapidd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
