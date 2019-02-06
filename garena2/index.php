b3b4e1158ca6f9d4a9ffc55ff97133f5b8633b24
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-06 09:11:31 -0500

    Update index.php

--- garena2/index.php
+++ garena2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Garena Free Fire 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jadohijpdfoadohfoajibfkggfeojjcf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
