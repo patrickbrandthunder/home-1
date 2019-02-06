5ab210a510d9c43f7f2f491f249cd2749253816a
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-06 09:35:44 -0500

    Update index.php

--- apexlegends2/index.php
+++ apexlegends2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Apex Legends 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dmdmagddjpdgpihbjpaefjhoeanecnbi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
