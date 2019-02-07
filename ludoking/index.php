73553c29cfb1e95fb15456b698cdf058a8a2fe05
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-07 15:07:07 -0500

    Update index.php

--- ludoking/index.php
+++ ludoking/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Ludo King';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bohbkmndlaioogdaahkhbihgebdilkem';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
