1b748c8a585f77e7f768a206d0c39e0c8c98f671
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-07 10:08:23 -0500

    Update index.php

--- euchre/index.php
+++ euchre/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Euchre';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kolhjpgfheandkmfpjeelmponchpimmo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
