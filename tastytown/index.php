7f835db3ad835b600cf1971792c70f66c7e2c99f
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-07 15:08:02 -0500

    Update index.php

--- tastytown/index.php
+++ tastytown/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Tasty Town';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'oaojpphgmnahdackjgmmibbjgabaelgj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
