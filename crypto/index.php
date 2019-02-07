2a17be168fc1680c29711861000e1f23a4173ba1
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-07 09:46:29 -0500

    Update index.php

--- crypto/index.php
+++ crypto/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Crypto Trillionaire';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lmiocgfplkbkbnijjddbhoflpkbjplbi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
