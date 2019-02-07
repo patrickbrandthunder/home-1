0fd10bb61243c9201a075288f2ec9e922dada858
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-07 15:26:39 -0500

    Update index.php

--- passengershaming/index.php
+++ passengershaming/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Passenger Shaming';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jdnnifokoapgobjmebfdencpnbidekmn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
