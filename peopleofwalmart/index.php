74cf6cc5e203fa9e6d4397e638b3f05126bcb88b
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-08 12:28:22 -0500

    Update index.php

--- peopleofwalmart/index.php
+++ peopleofwalmart/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'People Of Walmart';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'moeekldlkmkaeaabecfaijokenjjkmod';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
