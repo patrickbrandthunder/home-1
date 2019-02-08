0018c96683434c6e7ce421472ad94e3477fe689b
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-08 14:34:15 -0500

    Update index.php

--- idlecapitalist/index.php
+++ idlecapitalist/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Idle Capitalist';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ohoobhpghpecbjehlefncdlnachcfcgp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
