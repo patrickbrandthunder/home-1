b07e8567ece659ba7276dc8ac9bcec4f11719f8a
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-08 09:31:22 -0500

    Update index.php

--- shadowgun/index.php
+++ shadowgun/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Shadowgun Legends';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fkgohdjjejghdgblfcckllmbamaeldpk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
