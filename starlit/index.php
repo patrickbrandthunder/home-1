ed41c26a026a36f78a421b4ec7dd95703356cb45
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-07 21:28:40 -0500

    Update index.php

--- starlit/index.php
+++ starlit/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Starlit on Wheels';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ojgocebalhfpkhdckdnndaebnjbcmmnl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
