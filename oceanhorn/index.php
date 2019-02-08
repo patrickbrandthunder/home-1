317f40c91a8565c300292234b35edcc0ba58ddbc
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-07 21:29:17 -0500

    Update index.php

--- oceanhorn/index.php
+++ oceanhorn/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Oceanhorn 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mmgdnhbebagijdpmpmafgbpkfplfkaji';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
