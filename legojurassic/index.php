ee4e1ec761848ee3b1178ca9c9e824da1f2e3b42
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-08 11:03:14 -0500

    Update index.php

--- legojurassic/index.php
+++ legojurassic/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Lego Jurassic World';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'benbknecaobobioalilgkdeelppnkomo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
