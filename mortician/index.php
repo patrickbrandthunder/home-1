1bd5bbef0bd7ac019015ec004c26526d130efd86
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-08 13:13:32 -0500

    Update index.php

--- mortician/index.php
+++ mortician/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mortician Tale';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fhgmkikmcmdbnhnlelcfjfpkamlhbhef';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
