af8c1584992894cf0afd5c6f414728523b4ede63
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-08 09:49:50 -0500

    Update index.php

--- ramageddon/index.php
+++ ramageddon/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Ramageddon';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kpbgafcmkgpmnilebbfhhkgjcoedjppj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
