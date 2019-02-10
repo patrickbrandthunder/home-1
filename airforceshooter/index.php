138b57c4b4d36aaf707d4a15096f621e69e9d82a
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-10 09:42:34 -0500

    Create index.php

---
+++ airforceshooter/index.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'Air Force Shooter 3D';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
