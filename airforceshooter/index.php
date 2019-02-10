0f394394379eebaad2106f445c75ed5787c48672
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-10 10:06:42 -0500

    Update index.php

--- airforceshooter/index.php
+++ airforceshooter/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Air Force Shooter 3D';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cegclgihkemghedjmanhagooeddaghmj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
