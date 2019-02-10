48d3016fb9ec615798ebf6968b1abf002ddb03cf
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-10 16:03:43 -0500

    Update index.php

--- choices/index.php
+++ choices/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Choices Game';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ekbpnbjeebfnhiljphclocggdpncnckb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
