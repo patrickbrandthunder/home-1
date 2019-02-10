6a74d8dfa02395759bb29c87f3bc395308476e4e
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-10 11:20:53 -0500

    Update index.php

--- childsplay/index.php
+++ childsplay/index.php
@@ -1,5 +1,5 @@
 <?php
-$title = 'Child's Play 2019';
+$title = 'Childs Play 2019';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'bhablhogacelgfppcoceooaegnjmlnke';
 $tid = basename(__DIR__);
