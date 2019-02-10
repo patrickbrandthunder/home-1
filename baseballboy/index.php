ce6793fada5232ce5bcf9a9595e2d91ea7b752c6
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-10 10:56:42 -0500

    Update index.php

--- baseballboy/index.php
+++ baseballboy/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Baseball Boy';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nogiabbiinpjdiemehhblolgkcmlomap';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
