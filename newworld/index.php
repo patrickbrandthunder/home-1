8855c86c314cc7ab6bb8637e2387ae7236864948
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-10 10:08:50 -0500

    Update index.php

--- newworld/index.php
+++ newworld/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'New World';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ldombnimgenlbklgpmcobkifgafmccec';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
