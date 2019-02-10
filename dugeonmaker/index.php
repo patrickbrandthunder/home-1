49a78d1ff49c0c0cd738583d3bea6840778421ef
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-10 18:11:20 -0500

    Update index.php

--- dugeonmaker/index.php
+++ dugeonmaker/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dugeon Maker';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'emiihelnbekflcocoddgnkpfmebhanld';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
