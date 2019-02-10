c209b6685d2a38747a1298a3266ef309999b8176
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-10 09:56:12 -0500

    Update index.php

--- 60seconds/index.php
+++ 60seconds/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = '60 Seconds Atomic';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ampekfgflfdnihabdhjikedleijebncp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
