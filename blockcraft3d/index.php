f8689dc83acf364dc00c34d586e3d1bfd47cdefd
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-10 11:06:56 -0500

    Update index.php

--- blockcraft3d/index.php
+++ blockcraft3d/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Block Craft 3D';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dpogedphebdkicbbhomkkbdbnnbkjikj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
