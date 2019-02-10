61df57ed627ffaf2a14bc891cbfc4a4649582a61
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-10 17:52:19 -0500

    Create index.php

---
+++ explodingkittens/index.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'Exploding Kittens';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
