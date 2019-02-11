eddac75f79cde8cbf1ad3c07fc13c20b3e46fde7
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-11 16:22:03 -0500

    Update index.php

--- pou/index.php
+++ pou/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Pou';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'honklagmloibjjhpcdajppicbmldjhkk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
