574290fbe709804fdc4c99ce39401ee29dd1392e
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-11 14:49:20 -0500

    Update index.php

--- farmsim/index.php
+++ farmsim/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Farming Simulator';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'baemhjhmlampbepefjldhniniejcmkdn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
