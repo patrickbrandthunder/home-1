87353822b1d45a31150d4e7a91dd1b75ac889437
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-12 15:43:46 -0500

    Update index.php

--- vineyards/index.php
+++ vineyards/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Vineyards';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fdjbdojmmhdkcblmhblhmmfhiopppaao';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
