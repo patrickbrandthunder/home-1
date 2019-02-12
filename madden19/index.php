efd4f7ea26e4aa4a077dedc60e0376c2ab2199f6
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-12 08:30:53 -0500

    Update index.php

--- madden19/index.php
+++ madden19/index.php
@@ -3,5 +3,6 @@ $title = 'Madden NFL 19';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'fghjpbhhgofdcogbjpkijhnbkboiojfe';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
