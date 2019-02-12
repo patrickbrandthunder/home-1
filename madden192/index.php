30dfb09a055f22228c10c377276d8e7ae343187e
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-12 08:30:47 -0500

    Update index.php

--- madden192/index.php
+++ madden192/index.php
@@ -3,5 +3,6 @@ $title = 'Madden192';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'diafoidgkpjnlmikaoennijfdhaefkif';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
