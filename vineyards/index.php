d55239ea7b8ca52a9d1090e2066ea6f64575f641
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-12 15:25:29 -0500

    Update index.php

--- vineyards/index.php
+++ vineyards/index.php
@@ -1,5 +1,5 @@
 <?php
-$title = 'Name of your new Project';
+$title = 'Vineyards';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = '';
 $tid = basename(__DIR__);
