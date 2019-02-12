88721b88a2f2a31bfea27f0212e73bf4fc31ab94
Michael Kaply <consulting@kaply.com>
2019-02-12 14:42:13 -0600

    Facebook to parachute

--- homepage.php
+++ homepage.php
@@ -395,7 +395,7 @@ if (isset($tiles)) {
       outputTile($tile);
     }
 	echo '<a href="http://redirect.viglink.com?key=8860b76d9d55e5e067640b5beb7354ca&u=http%3A%2F%2Fwww.walmart.com "><img class="tile" height="50" width="50" alt="Walmart" title="Walmart" src="https://home.newtabgallery.com/global/images/walmart.png"></a>';
-	echo '<a href="http://redirect.viglink.com?key=8860b76d9d55e5e067640b5beb7354ca&u=http%3A%2F%2Fwww.facebook.com"><img class="tile" height="50" width="50" alt="Facebook" title="Facebook" src="https://home.newtabgallery.com/global/images/facebook.png"></a>';
+	echo '<a href="http://redirect.viglink.com?key=8860b76d9d55e5e067640b5beb7354ca&u=http%3A%2F%2Fwww.parachutehome.com"><img class="tile" height="50" width="50" alt="Parachute Home" title="Parachute Home" src="https://home.newtabgallery.com/global/images/parachute.png"></a>';
     $count = min(sizeof($tiles), 8);
     $rand_keys = array_rand($tiles, $count);
     for ($i = 0; $i < $count; $i++) {
