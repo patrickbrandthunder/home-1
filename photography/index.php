a8c054b43ba8b7ad1c51e241d1d946a8ba8bb5ee
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-13 13:00:27 -0500

    Update index.php

--- photography/index.php
+++ photography/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Photography';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'maolcjacdfgdckgnaalgcnoadeheamnh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
