cb9844657595dec0a44fc6abf5d578f03afb2535
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-13 13:00:52 -0500

    Update index.php

--- mafiacity/index.php
+++ mafiacity/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Mafia City';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'laeeekliehkhbdhcchdomkbgemlcipga';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
