16e30c09689b7c2e0c9b334be3acab41932c33bc
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-13 15:21:22 -0500

    Update index.php

--- gemini/index.php
+++ gemini/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Gemini';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gkdkgeodnbghlclibnlnedpalfgehmfa';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
