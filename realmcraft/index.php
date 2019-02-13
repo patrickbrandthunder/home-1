fb5208d73b06ffe45f5a4a077f93e8171afe2e3f
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-12 19:02:58 -0500

    Update index.php

--- realmcraft/index.php
+++ realmcraft/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'RealmCraft';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hocbmgbpcjbfgkmckmcjjmhomhoeenkm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
