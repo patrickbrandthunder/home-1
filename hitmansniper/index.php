1d7627b67398932d1176b40f044bf89f56f89b81
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-13 16:20:57 -0500

    Update index.php

--- hitmansniper/index.php
+++ hitmansniper/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Hitman Sniper';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nooajjodneecbediakgigaamehhhepde';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
