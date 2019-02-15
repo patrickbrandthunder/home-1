6ffaa5837a95809eefe44abb7ff4bf33cbb7eed6
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-14 20:29:16 -0500

    Update index.php

--- reigns/index.php
+++ reigns/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Reigns';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cminpcoojeccpkadejgjeipofpenfphi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
