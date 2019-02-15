75b71c6109f42d5fe34251fafafc1c3375d6d33f
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-14 20:08:43 -0500

    Update index.php

--- polog/index.php
+++ polog/index.php
@@ -1,7 +1,7 @@
 <?php
-$title = 'Name of your new Project';
+$title = 'POLO G';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'moboflnfanaccajjdjjokbfgfmfaggam';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
