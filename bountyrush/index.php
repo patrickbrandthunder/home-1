b1a3356a58dd77a0a1802e3ed5d22970ef4e0d59
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-14 20:03:42 -0500

    Update index.php

--- bountyrush/index.php
+++ bountyrush/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'ONE PIECE Bounty Rush';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hdefojldblebbeajpgpncfjdlmdibndk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
