143c4274fb0071be50014074a32b0e6d250566e3
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-16 10:02:19 -0500

    Update index.php

--- fortnitenerf/index.php
+++ fortnitenerf/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Fortnite Nerf Guns';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ajbeflngjcnmhchikeliajedbjoopkdo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
