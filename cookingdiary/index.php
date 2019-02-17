bcaf69640b2b6059a575522ed480f5087b90c3bd
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-17 18:06:28 -0500

    Update index.php

--- cookingdiary/index.php
+++ cookingdiary/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Cooking Diary';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'maefcjhhmbaapehngmimeplgfdfcecmf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
