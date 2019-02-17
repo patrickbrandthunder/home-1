c31e7ff4658762de03acc8cdf2fa5d2909671b98
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-17 18:20:27 -0500

    Update index.php

--- ben10/index.php
+++ ben10/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Ben 10 Up To Speed';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dbkdnekombplgakjbppjgafehoiomkbb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
