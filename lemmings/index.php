835ee7b153308fd056d5053534b87172f0f6a4d0
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-17 16:23:54 -0500

    Update index.php

--- lemmings/index.php
+++ lemmings/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Lemmings';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'obnhpicikhgmgndjggcjhjnomnljfmll';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
