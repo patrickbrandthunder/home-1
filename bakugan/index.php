8ba05e4efc013b6d1e03d9dbe9379943eae1367d
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-17 16:13:22 -0500

    Update index.php

--- bakugan/index.php
+++ bakugan/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bakugan Battle Brawlers';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cfgiagpjmheiogeddcdjpgopfoedehcl;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
