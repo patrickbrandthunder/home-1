c155622abcd54190d013c97835c0b66476cd2cab
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-17 15:56:35 -0500

    Update index.php

--- linnpath/index.php
+++ linnpath/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Linn Path Of Orchards';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ocgckfbhaaejbbmgnehljcmoaeiimkdn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
