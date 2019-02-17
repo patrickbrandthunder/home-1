40388e7b3541a707602be58a2219ccbaf9579035
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-17 18:12:08 -0500

    Update index.php

--- kygo/index.php
+++ kygo/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Kygo';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nfjgblnkjkhjkjijamkelcnpkmpcckmg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
