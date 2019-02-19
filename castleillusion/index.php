a014f825a5a2faabf3a0a7be2a0f25a848ad8e9d
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-19 08:58:29 -0500

    Update index.php

--- castleillusion/index.php
+++ castleillusion/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Castle Of Illusion';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nlfmmmbbimoiikeijlidmikjpgjhcpfo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
