b78cf559ad47d2291397ae4f4e1d055be0b172a5
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-19 21:50:21 -0500

    Update index.php

--- grandheist/index.php
+++ grandheist/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Operation Grand Heist';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lflgkcidcififhbononoecbonabmbpfi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
