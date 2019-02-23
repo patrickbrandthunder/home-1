9f51930f645cab06642b9c92b109c81ea736f338
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-22 19:15:58 -0500

    Update index.php

--- doompatrol/index.php
+++ doompatrol/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Doom Patrol';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kinipnkckoepheiidjkmpjbcaodkmhno';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
