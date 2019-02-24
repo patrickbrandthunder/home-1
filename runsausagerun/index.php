475b30c2531991864bfe8aa35a071ac12474b292
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-24 15:33:56 -0500

    Update index.php

--- runsausagerun/index.php
+++ runsausagerun/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Run Sausage Run';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mehplgiagjjblbjdjinjhknhgglnamam';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
