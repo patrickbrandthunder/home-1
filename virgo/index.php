5ae6b06eb7a4f30c9c223bc5b2e2f748f63377e3
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-24 14:12:00 -0500

    Update index.php

--- virgo/index.php
+++ virgo/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Virgo';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'eidlaenbgegiincjfbefjbbcplaogpfp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
