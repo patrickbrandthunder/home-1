d6923b0da8a4f84e7c4815760d96cda6aa17bd00
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-24 17:14:10 -0500

    Update index.php

--- nbajam/index.php
+++ nbajam/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'NBA JAM EA SPorts';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dbaonjgmgkjelmnbgbndelfpehiglmgm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
