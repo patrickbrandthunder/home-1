41f3832b5b8f52f8a4f5a74ed33005885ef3201c
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-24 15:05:40 -0500

    Update index.php

--- scorpio/index.php
+++ scorpio/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Scorpio';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cekcmidegagknmmcnaeelpickabglmnl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
