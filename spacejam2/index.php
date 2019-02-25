892e1c55852d0a5ef066a299b81b0cc001078a96
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-25 15:28:08 -0500

    Create index.php

---
+++ spacejam2/index.php
@@ -0,0 +1,8 @@
+
+<?php
+$title = 'Space Jam 2';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
