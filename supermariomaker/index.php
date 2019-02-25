335c312b5e791ed17b57b7db0b1b0a50d1303dab
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-25 13:36:50 -0500

    Update index.php

--- supermariomaker/index.php
+++ supermariomaker/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Super Mario Maker 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lghgiaggdbombgfeeliaeajmmjpcijeo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
