b990b66e4a57a9c47469f4b904d2e02a2dde9546
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-25 16:21:18 -0500

    Update index.php

--- pokemonduel/index.php
+++ pokemonduel/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Pokemon Duel';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'oikgkjmbncinejgnkdcbhnadpkanfeoo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
