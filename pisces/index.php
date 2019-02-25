f40f0bd95408a1497f7066d6c816583687243410
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-25 11:05:59 -0500

    Update index.php

--- pisces/index.php
+++ pisces/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Pisces';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'edpgdancmofgdbcdmmfdjndebpjemihe';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
