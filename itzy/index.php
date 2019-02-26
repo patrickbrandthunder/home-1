4ebc096380da12f0d885592db8f03f5d3cb4a066
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-25 20:50:10 -0500

    Update index.php

--- itzy/index.php
+++ itzy/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'ITZY';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pljddgldpmjpppdomnfomanaepepafpm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
