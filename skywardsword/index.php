8007ad690e5e29758411b8f4bb4999122b557f6a
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-26 09:45:01 -0500

    Update index.php

--- skywardsword/index.php
+++ skywardsword/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Skyward Sword';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ipnnepebebclllhmcbdnkfhmjhpdfpii';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
