7ff3a15388a2797807c4c4d65d7ee087dec58d16
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-25 20:51:21 -0500

    Update index.php

--- criminalcase/index.php
+++ criminalcase/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Criminal Case';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kaklaoaalahjedmhgkcigjaicobnlhkg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
