57b842c965a7062d81179b9e861aab5a13863bfc
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-26 10:00:43 -0500

    Update index.php

--- markronson/index.php
+++ markronson/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mark Ronson';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pdkfjhdkgplgblmigakepegicaojkdmn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
