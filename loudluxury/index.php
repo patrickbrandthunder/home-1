41d688df3c7e27513f37e7ebf622f1299f977d0d
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-26 08:02:08 -0500

    Update index.php

--- loudluxury/index.php
+++ loudluxury/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Loud Luxury';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hfoeddkbpolldhhjdekkponnamoigaab';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
