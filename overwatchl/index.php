f181a2be93446e5cd5b292f89659018aa7a426ce
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-25 21:09:10 -0500

    Update index.php

--- overwatchl/index.php
+++ overwatchl/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Overwatch League';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mpjinjmajfelkpchgbgdhmdefdfinmfi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
