261fe3f441b4fabdcc46358b44efc97c62a07353
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-27 16:15:53 -0500

    Update index.php

--- brawlstars/index.php
+++ brawlstars/index.php
@@ -4,6 +4,5 @@ $title = 'Brawl Stars';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'nnpkebhokhdijnifmpimhcgkfilgbmje';
 $tid = basename(__DIR__);
-$testSearch2  = true; 
 include('../homepage.php');
 ?>
