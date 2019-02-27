12ab7d794057b5a3e5d7ea671bc1ff43d435f148
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-27 16:16:24 -0500

    Update index.php

--- pokemonsword/index.php
+++ pokemonsword/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Pokemon Sword';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jhhmkahegdbdoiibmpdnolldeniamlil';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
