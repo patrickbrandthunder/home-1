058dca2aacb3f98290a326a2bd6ef4c1f307621a
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-27 15:54:19 -0500

    Update index.php

--- pokemonshield/index.php
+++ pokemonshield/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Pokemon Shield';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jgpkjfjjkfeonmjchimmkcnfhdphjdki';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
