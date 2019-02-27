ac33aa61b605c72e5c960f56b84bc23162376e20
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-27 16:14:50 -0500

    Update index.php

--- eraoflegends/index.php
+++ eraoflegends/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Era Of Legends';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ciikigolfmbfnpdlmcpoimepioghaafg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
