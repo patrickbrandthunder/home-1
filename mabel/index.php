85e9e3b57229109ebdfe69dbf69ea9c1b7778da7
pjmurphy777 <pjmurphy77@gmail.com>
2019-02-27 19:45:01 -0500

    Update index.php

--- mabel/index.php
+++ mabel/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mabel';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mekadedideophpmodgobghigeegangam';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
