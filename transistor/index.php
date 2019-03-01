0c287a6982f7fcf97483c06201178f6f6d2ab88a
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-01 10:47:20 -0500

    Update index.php

--- transistor/index.php
+++ transistor/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Transistor';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kgkaibkeahkaheiepbejgjbipmfeidkf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
