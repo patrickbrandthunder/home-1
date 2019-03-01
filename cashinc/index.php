d01d255e5bf1437af57c038a0380a2a42918ef59
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-01 15:25:20 -0500

    Update index.php

--- cashinc/index.php
+++ cashinc/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Cash Inc';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gefbelabgbhggkloiihcnelckpfhpghd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
