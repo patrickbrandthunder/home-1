b6db8a03ee0f756307111276f4e8dd394a5c5a27
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-02 20:31:54 -0500

    Update index.php

--- rbibaseball/index.php
+++ rbibaseball/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'RBI Baseball 18';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fagocmmnfbkhflpipgboggbaijpcnbhb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
