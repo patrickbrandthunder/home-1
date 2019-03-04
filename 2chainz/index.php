d24b24c980f8734c5e2c0ac68b66346eae43fc09
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-04 12:28:03 -0500

    Update index.php

--- 2chainz/index.php
+++ 2chainz/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = '2 Chainz';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jkmbnpdnmgaohnemefglopjfahlkdmic';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
