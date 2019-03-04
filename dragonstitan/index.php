921e7d8cfcb33a53504f15a3a76dde014b483ccd
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-04 13:34:18 -0500

    Update index.php

--- dragonstitan/index.php
+++ dragonstitan/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dragons Titan Uprising';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ejfmbjniladikchfihgjllihofdceapl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
