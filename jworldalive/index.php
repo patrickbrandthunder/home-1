b5aa2f843fb3f59a2dad962813b250e2229df655
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-04 12:28:35 -0500

    Update index.php

--- jworldalive/index.php
+++ jworldalive/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Jurassic World Alive';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'anaaojldhiojmgbpmjndpggjhohfccbl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
