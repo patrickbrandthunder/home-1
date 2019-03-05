03426695559dd29829ca38ea45331ca194809555
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-04 21:35:05 -0500

    Update index.php

--- allybrooke/index.php
+++ allybrooke/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Ally Brooke';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hnbihjbnlekfddkmnmggdejephdmlkcm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
