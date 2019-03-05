bbbd4e3e417604e83530373a68af99691de91247
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-05 14:58:29 -0500

    Update index.php

--- hotelta/index.php
+++ hotelta/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Hotel Transylvania Adventures';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kjhoeboihiledoidiipbhkogglcfeflb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
