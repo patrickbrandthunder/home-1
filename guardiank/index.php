7dc75cb65de2746e42451584795dd46d32bd1875
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-05 10:57:00 -0500

    Update index.php

--- guardiank/index.php
+++ guardiank/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Guardian Kingdoms';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hbafnhpldanhnanklfiokcjapnnllkmg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
