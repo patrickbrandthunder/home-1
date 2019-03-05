a205e37606dd0fe1f8cfbaf483455c414f9c254a
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-05 10:18:00 -0500

    Update index.php

--- fever333/index.php
+++ fever333/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Fever333';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cjcenmlllclcendbeiigaiiodfmheacd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
