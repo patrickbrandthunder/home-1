156533feb8dbf12b17d62850c412455e6c57fb25
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-07 18:53:20 -0500

    Update index.php

--- drums/index.php
+++ drums/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Drums';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'eodnaljdbcnjjdjlhdgcpfcabfjmldlh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
