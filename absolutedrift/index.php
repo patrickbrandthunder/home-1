d51c4315ff900ba4e51bd800acaeff2d5a97d315
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-07 18:53:47 -0500

    Update index.php

--- absolutedrift/index.php
+++ absolutedrift/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Absolute Drift';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hmibphhenmdpealfjofllcobakfinena';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
