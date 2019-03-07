5d15d02ea00d3f800d2e8bec3bd65d014291ced4
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-07 16:09:29 -0500

    Update index.php

--- hoodieallen/index.php
+++ hoodieallen/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Hoodie Allen';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nalgmlbpmpijoakjmkjcpfkdlgakmhme';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
