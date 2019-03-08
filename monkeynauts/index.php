d430d25a2bab42c5143f19cfe5826cb6c03d8adc
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-08 15:50:37 -0500

    Update index.php

--- monkeynauts/index.php
+++ monkeynauts/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Monkeynauts';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'akdjlhhmibfgjkenlafbdehjkdbbpgll';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
