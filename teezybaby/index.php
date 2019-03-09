2b6a58f18e94b536d75920b00e5c1e26e4758292
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-09 08:05:51 -0500

    Update index.php

--- teezybaby/index.php
+++ teezybaby/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Teezy Baby';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bjdneecfccnijfgfepbagbneahhggife';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
