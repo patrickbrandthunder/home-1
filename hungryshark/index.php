9e145d86afabd320bd105e532897cbc3b996c5d4
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-11 15:13:54 -0400

    Update index.php

--- hungryshark/index.php
+++ hungryshark/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Hungry Shark Evolution';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nnnaacdapobpphcjbencnhkceglafeli';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
