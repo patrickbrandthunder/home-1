bb81f87fa544342f52ed26ec6b0b78210c7c377b
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-11 15:13:18 -0400

    Update index.php

--- candycrushsoda/index.php
+++ candycrushsoda/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Candy Crush Soda Saga';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cikefkbbhegocncjlkicnmfdnikdoffb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
