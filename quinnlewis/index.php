28a0ea25ee816acbfa60835cd9b0269ef4e685fe
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-11 16:21:27 -0400

    Update index.php

--- quinnlewis/index.php
+++ quinnlewis/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Quinn Lewis';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'incoelggeeilmpgogmpdjkdfihhmdolo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
