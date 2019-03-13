455ffd482c303f23d6851003e51ff0ce6f3be66d
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-13 10:41:23 -0400

    Update index.php

--- hoodieszn/index.php
+++ hoodieszn/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Hoodie SZN';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'icbdnijbljlchldbicjiaigldpkeebcl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
