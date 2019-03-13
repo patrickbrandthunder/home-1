8c7f9a127d812e0ab961d447ed1ea2dfc6fe0dcb
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-13 14:30:09 -0400

    Update index.php

--- worsethandeath/index.php
+++ worsethandeath/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Worse Than Death';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'clhnkdgbgeocdfdpaljehfnafmcgkbmh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
