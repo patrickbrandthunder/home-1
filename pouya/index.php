b76c2097d1551076cf09b9f5ab8d8dc17d165811
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-14 08:07:38 -0400

    Update index.php

--- pouya/index.php
+++ pouya/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Pouya';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'abmkgkgifcapcmaoolcbnjkcpaofdlca';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
