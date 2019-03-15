d0aba3bb868d9b456c902eeb4396648b5691aae7
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-14 20:23:24 -0400

    Create index.php

---
+++ afrob/index.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'Afro B';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
