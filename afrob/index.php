65753a9b44b25646e7f07cb68f21fe3364165f0c
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-14 21:02:53 -0400

    Update index.php

--- afrob/index.php
+++ afrob/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Afro B';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'epljlkadikfkahigochodbjgabjldlkd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
