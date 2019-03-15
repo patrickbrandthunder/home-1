c228f5f4c7287f6902e6aaec2d6415873f0f1cbe
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-15 17:04:54 -0400

    Update index.php

--- pinkfong/index.php
+++ pinkfong/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Pinkfong';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pjgfajainimkgobagagljkaaelkkmgfh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
