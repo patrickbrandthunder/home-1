e205b397149baa28395b8695279e6cc435c6d209
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-16 16:26:41 -0400

    Create index.php

---
+++ thewitness/index.php
@@ -0,0 +1,8 @@
+
+<?php
+$title = 'The Witness';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
