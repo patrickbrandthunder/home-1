49f18ea09987d5451fa933348bddfbf735b7e8c8
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-16 07:47:09 -0400

    Update index.php

--- paintpop3d/index.php
+++ paintpop3d/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Paint Pop 3D';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lmjmejpmlbncplbiebljbpieocmpnaaa';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
