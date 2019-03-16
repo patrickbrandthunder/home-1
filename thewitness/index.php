f914de85ef0161713e77cd6c8f3eee50788e8bb1
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-16 17:43:15 -0400

    Update index.php

--- thewitness/index.php
+++ thewitness/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'The Witness';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cmpdadaifndojhlnbpnoaoomofoimkfl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
