9b87b97e6fe2cb698a89f80567d78cf4d1a842d0
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-17 10:05:32 -0400

    Update index.php

--- skylanders/index.php
+++ skylanders/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Skylanders Ring of Heroes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jpjmjoldhkokcnifgnlippmmbigljojn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
