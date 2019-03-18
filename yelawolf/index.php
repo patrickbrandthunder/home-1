54d8c45135436495f5e3a3a5857d075549fd67bb
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-18 10:56:42 -0400

    Update index.php

--- yelawolf/index.php
+++ yelawolf/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Yelawolf';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'omnaniidhloogllaifjpjanladeodnjk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
