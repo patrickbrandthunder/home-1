071358983f0ca375a5d44d19191a4375bd310ed0
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-19 08:13:36 -0400

    Update index.php

--- raidshadow/index.php
+++ raidshadow/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Raid Shadow Legends';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lnpfpnpmgjimdmonlbboogbignklhnem';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
