1e5d0cfdd74b760b38ee8f138c48eb824dec9f7c
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-20 07:12:00 -0400

    Update index.php

--- blacksummer/index.php
+++ blacksummer/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Black Summer';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mciaidoccbkjomaoakiekblcnpjjnopp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
