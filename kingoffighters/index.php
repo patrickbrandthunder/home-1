cfdad50219d0565e53090644d8d3ee8dca777ddf
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-20 14:54:16 -0400

    Update index.php

--- kingoffighters/index.php
+++ kingoffighters/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'King Of Fighters';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fodlgpmkmkopmmddpjfchbeifaieknjl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
