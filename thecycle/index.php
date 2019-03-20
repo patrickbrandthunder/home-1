22f9f2fb523625e5cd737bfc2caba663f7f25628
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-20 17:00:07 -0400

    Update index.php

--- thecycle/index.php
+++ thecycle/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'The Cycle';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dcjfpgncpkpfibplffkcdfmnhlilmckn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
