f17d907776efc6f870af885b249afb18875d0f60
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-20 17:35:04 -0400

    Update index.php

--- gymsuperstar/index.php
+++ gymsuperstar/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Gymnastics Superstar';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mpdciejfhcnekfcpmmhbbhmmhcnhgbgh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
