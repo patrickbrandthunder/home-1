a2b2496673035fdaa1a3f8682befa6daeadda639
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-20 09:01:16 -0400

    Update index.php

--- austinh/index.php
+++ austinh/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Austin Mahone';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cfhhabnocdbaamhjconmimmpdedpglen';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
