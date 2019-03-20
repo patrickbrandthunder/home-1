80a78ccbdfc361ee7fb1468ae13febf5a1c76a37
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-20 15:25:31 -0400

    Update index.php

--- dragbattle/index.php
+++ dragbattle/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Drag Battle';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mbjblkfpepeadmnkapakbonidlkhlibf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
