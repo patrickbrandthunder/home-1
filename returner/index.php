b0047082c956ae4e958c41082fcfb8f9801d8ffc
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-20 17:28:15 -0400

    Update index.php

--- returner/index.php
+++ returner/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Returner';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lleinjdnaldkcgbelcdceplifhdmkkal';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
