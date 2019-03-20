b3b6624732b7956b6ab1c3ac01bcb7f197b4bafd
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-20 17:28:52 -0400

    Update index.php

--- supermarket/index.php
+++ supermarket/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Supermarket Tycoon';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'akpkecagmemcmgcnhaccnldffflpbojg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
