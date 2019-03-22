024863546bb908e47eded5195787ecd700673a89
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-22 15:28:36 -0400

    Update index.php

--- lilnasx/index.php
+++ lilnasx/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Lil Nas X';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lkgnieeafhinkjgodbpobfpahjcheceo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
