128e05b56b6b2671e081ed8d5c1378b08cf0bdd0
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-22 21:07:40 -0400

    Update index.php

--- rollercoasters/index.php
+++ rollercoasters/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Rollercoasters';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pampafnbmpciidllljafcjaampfckaam';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
