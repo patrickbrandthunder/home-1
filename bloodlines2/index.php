d989c00fe9822f2e1afae7204e0430218f5578c6
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-25 12:36:59 -0400

    Update index.php

--- bloodlines2/index.php
+++ bloodlines2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bloodlines 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'chppgmaofmanjdjehekiddidimdijcfb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
