d1a0850c85c923ebb6e9951c72339e24bfd99540
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-25 16:53:44 -0400

    Update index.php

--- petrescue/index.php
+++ petrescue/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Pet Rescue Puzzle';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'icmcbliihljhkldjkalpapdjlchcdbed';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
