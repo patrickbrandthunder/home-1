8bf8cc73e859905f8cc21c79a7bc21c46dc57ac5
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-25 10:41:13 -0400

    Update index.php

--- jetpackjoyride/index.php
+++ jetpackjoyride/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Jetpack Joyride';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dkacklbejfbmnkokjefijfmplbhkgkgb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
