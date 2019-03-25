f2aa9785dc6fe45dc03ffba50e292d2064307bdf
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-25 15:32:53 -0400

    Update index.php

--- thepathless/index.php
+++ thepathless/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'The Pathless';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'obefcliheplichnhjblgekifpkphmiag';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
