dbce91ffc4c872b0ce9d98cd804c5541c7a205a3
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-25 09:29:30 -0400

    Update index.php

--- powerrangers/index.php
+++ powerrangers/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Power Rangers';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gofdamaldpolmblleekfomlbkjcglohi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
