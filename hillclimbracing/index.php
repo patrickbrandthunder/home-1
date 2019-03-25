ba403384a4ca6dac75ee84b6ca4cb7c670fd4530
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-25 16:28:58 -0400

    Update index.php

--- hillclimbracing/index.php
+++ hillclimbracing/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Hill Climb Racing';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dnnolodpefkacjemfgffiokhffljlddk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
