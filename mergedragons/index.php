044ddf54df8bffe3e36e39414e732ea4ad5f5cd1
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-25 14:52:00 -0400

    Update index.php

--- mergedragons/index.php
+++ mergedragons/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Merge Dragons';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bggmjhgphcogdbnolchncooegifibemh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
