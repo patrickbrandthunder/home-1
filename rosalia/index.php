1729d38f959ba6a4d26f1348bd93aa41d2a0a4d6
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-26 11:46:34 -0400

    Update index.php

--- rosalia/index.php
+++ rosalia/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Rosalia';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gpjdmclkkolmncanchjhfmojdkcdlidg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
