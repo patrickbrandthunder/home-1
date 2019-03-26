d19fbbb03188f7d0a8a08e539023099b9898bbd3
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-26 11:44:57 -0400

    Update index.php

--- shieldwall/index.php
+++ shieldwall/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Shieldwall Chronicles';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fpecebbnhlccmgfihkficdhjkmnjnobn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
