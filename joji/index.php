1a764c842dde0e01e2fc05b3f8644ca1a4a15333
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-27 09:24:39 -0400

    Update index.php

--- joji/index.php
+++ joji/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Joji';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ejpdfcdlphedgdlpklmnlepdfgjkcoih';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
