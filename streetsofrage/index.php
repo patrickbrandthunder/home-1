1857fde10082e0cb32f848178ed4ffe652435b14
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-27 07:57:22 -0400

    Update index.php

--- streetsofrage/index.php
+++ streetsofrage/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Streets of Rage';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dfelamoccfgnjdholndfncfefkpohnob';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
