59773a3c7171a5892ad49689c01b4351c43a93f6
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-28 15:26:05 -0400

    Update index.php

--- tempestpirate/index.php
+++ tempestpirate/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Tempest Pirate Action';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ecnhhnobjghjemefijbjfmimelaipail';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
