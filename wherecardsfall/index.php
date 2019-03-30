63462283f42743ccb0652f5532f9a432092d229b
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-30 09:52:36 -0400

    Update index.php

--- wherecardsfall/index.php
+++ wherecardsfall/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Where Cards Fall';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ldpbokinlhofbmloilnmgomldddhjodd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
