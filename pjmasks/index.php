d8d0fb19461dbb5a7bac5e4392989f0fd4491302
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-30 09:56:43 -0400

    Update index.php

--- pjmasks/index.php
+++ pjmasks/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'PJ Masks';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hfpnclbfncdfgdceapcockijcmpodkfa';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
