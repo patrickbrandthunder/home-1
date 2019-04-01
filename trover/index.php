6c392c8ad67b81868af7bcca837c643954bf5670
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-01 15:42:41 -0400

    Update index.php

--- trover/index.php
+++ trover/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Trover Saves The Universe';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ndhfgfmpnhokjjkpplbljmaomnmgpifk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
