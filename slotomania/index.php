c9fc7b8a39ff8ec257cc361951136e0e504f616d
pjmurphy777 <pjmurphy77@gmail.com>
2019-03-31 20:35:22 -0400

    Update index.php

--- slotomania/index.php
+++ slotomania/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Slotomania';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ggbmojdkfpfccnlfjmmfoalgionedion';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
