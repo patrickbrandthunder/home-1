4bba2af410e83a78bf3f5ed11dcdd1cedd24ef79
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-01 14:25:00 -0400

    Update index.php

--- friends/index.php
+++ friends/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Friends';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bgdkmknpbokpopcdchmipmnkopapldpc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
