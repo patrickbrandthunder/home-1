d889397244878d42bef0459114adb17212f71c0d
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-02 11:26:17 -0400

    Update index.php

--- kingcombs/index.php
+++ kingcombs/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'King Combs';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kcifkkkcdlpjjbedibeccngapfgplgoi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
