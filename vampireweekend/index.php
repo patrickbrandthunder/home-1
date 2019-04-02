3af0a9514a58b6390e72ec81c6de11744f37d96f
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-01 20:15:23 -0400

    Update index.php

--- vampireweekend/index.php
+++ vampireweekend/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Vampire Weekend';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gibjkdjclanllefagcimdpimoefnnnpp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
