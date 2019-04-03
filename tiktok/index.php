91d095200d8adad37cba9d972ae0e3062d5dab62
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-03 08:11:36 -0400

    Update index.php

--- tiktok/index.php
+++ tiktok/index.php
@@ -3,5 +3,6 @@ $title = 'TikTok';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'cbcpiaiejflndcdpepolnnaigamjbhal';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
