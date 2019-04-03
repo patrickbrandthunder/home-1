6ead8e4cbbe02841eec5f7f620015ffd69334afa
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-03 08:15:23 -0400

    Update index.php

--- pewdiepie/index.php
+++ pewdiepie/index.php
@@ -3,5 +3,6 @@ $title = 'Pewdiepie';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'lgejehnodcaoadhacmckgheibmbjnlod';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
