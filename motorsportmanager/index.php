3dd02ee56ecd9c40b6cbb278e8e3e8bd6c64f64a
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-05 14:30:05 -0400

    Update index.php

--- motorsportmanager/index.php
+++ motorsportmanager/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Motorsport Manager Mobile 3';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dedoijhacokeejflbaigamjdnmmiggbj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
