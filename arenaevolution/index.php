71350828a35a0b64db2b57faef5448222793adf2
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-07 16:44:10 -0400

    Update index.php

--- arenaevolution/index.php
+++ arenaevolution/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Arena Evolution CHess Heroes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hkllpchejhhfdecfalbkloafadkaepbc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
