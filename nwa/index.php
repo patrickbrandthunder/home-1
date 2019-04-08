857059864002ca32682e3d20e50b2b99fa4f9879
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-08 16:08:42 -0400

    Update index.php

--- nwa/index.php
+++ nwa/index.php
@@ -3,5 +3,6 @@ $title = 'NWA';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ofigdjifbalkjppdjgdmdcpilbcpopic';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
