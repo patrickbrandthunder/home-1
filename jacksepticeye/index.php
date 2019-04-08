b1db80976bbc3b9bb7645dec7e41e8a055166c30
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-08 16:09:38 -0400

    Update index.php

--- jacksepticeye/index.php
+++ jacksepticeye/index.php
@@ -3,5 +3,6 @@ $title = 'Jacksepticeye';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'lcidjfcffoomfpgdgddcdekglabcilol';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
