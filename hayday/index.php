072a3034392eaf27d98ce97982f0532e26a00f50
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-08 15:27:36 -0400

    Update index.php

--- hayday/index.php
+++ hayday/index.php
@@ -3,5 +3,6 @@ $title = 'Hay Day';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'hmiamfknefhemlippkhhiahdpldhgmlf';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
