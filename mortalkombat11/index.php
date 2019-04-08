763737a47828caa512c3ffcdb2e4c0d6725044ed
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-08 14:58:05 -0400

    Update index.php

--- mortalkombat11/index.php
+++ mortalkombat11/index.php
@@ -3,5 +3,6 @@ $title = 'MOertal Kombat 11';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ednikibcpfojemijkiiaagggdiaenhgm';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
