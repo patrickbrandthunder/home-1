9c3d35a0d58e41a9f9eb7c65c46ed849785168db
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-08 15:21:59 -0400

    Update index.php

--- citygirls/index.php
+++ citygirls/index.php
@@ -3,5 +3,6 @@ $title = 'City Girls';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'nmcfomgcicinblbbncocmkcejdiflggp';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
