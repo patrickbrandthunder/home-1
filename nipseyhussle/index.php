27ff1b6c0a7e338535c19ecef07c994639ce7a5a
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-08 15:28:59 -0400

    Update index.php

--- nipseyhussle/index.php
+++ nipseyhussle/index.php
@@ -3,5 +3,6 @@ $title = 'Nipsey Hussle';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'nhbcmpihnddffflopodbplkggnpgdobn';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
