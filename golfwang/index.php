2aa4c32e73820db0b38e264f35a6937bde23b915
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-08 16:18:44 -0400

    Update index.php

--- golfwang/index.php
+++ golfwang/index.php
@@ -3,5 +3,6 @@ $title = 'Golf Wang';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'djjadompmmlmafonfomappaijlaedeoj';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
