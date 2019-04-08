c51bc13a71aad5c84dd67af5ee350ca945cf8202
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-08 15:19:27 -0400

    Update index.php

--- energy2/index.php
+++ energy2/index.php
@@ -2,6 +2,7 @@
 $title = 'Monster Energy 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'oipnmdkckondjhhmeggogackpkcmjkoa';
+$testSearch2  = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
