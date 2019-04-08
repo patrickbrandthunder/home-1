5fb75ad777868d4a07dfe8bacdb615cddc4034b0
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-08 16:10:41 -0400

    Update index.php

--- jacquees/index.php
+++ jacquees/index.php
@@ -4,5 +4,6 @@ $title = 'Jacquees';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'jominbmbgpdmpbmhpcklogccdflfonek';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
