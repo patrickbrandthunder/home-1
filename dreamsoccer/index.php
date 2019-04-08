4411e4e78be8da5751261a59ec6b7c648f495522
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-08 15:23:08 -0400

    Update index.php

--- dreamsoccer/index.php
+++ dreamsoccer/index.php
@@ -4,5 +4,6 @@ $title = 'Dream League Soccer';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'aljlcemcnhemlghagpgagndmaemmhdhm';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
