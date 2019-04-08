541fef66f5efa0abcd08f99a46501b6cada55005
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-08 15:25:14 -0400

    Update index.php

--- ellamai2/index.php
+++ ellamai2/index.php
@@ -3,5 +3,6 @@ $title = 'Ella Mai 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'bnhcibkdephhfhjgabhdnfglmheajodm';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
