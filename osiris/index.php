a9065e7358206f7f26cdbb98d6859e689419d3ed
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-08 15:19:54 -0400

    Update index.php

--- osiris/index.php
+++ osiris/index.php
@@ -3,5 +3,6 @@ $title = 'YK Osiris';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'fgfmkbfhhfkmfiobpkciopheigbjnefe';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
