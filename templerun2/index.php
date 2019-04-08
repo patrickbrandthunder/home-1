f27aac66d0c0ae7d63692a396ab27805e016b70c
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-08 16:12:52 -0400

    Update index.php

--- templerun2/index.php
+++ templerun2/index.php
@@ -3,5 +3,6 @@ $title = 'Temple Run 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'okkmplnhoeeiidoajmooamokogcdphgo';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
