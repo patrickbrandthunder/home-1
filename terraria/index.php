487ce88554ef19f9d7e1d0dcab52433d302801dc
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-08 16:21:13 -0400

    Update index.php

--- terraria/index.php
+++ terraria/index.php
@@ -4,5 +4,6 @@ $title = 'Terraria';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'lepmnlpfnmkiohmnhhobldhclfhfgkhb';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
