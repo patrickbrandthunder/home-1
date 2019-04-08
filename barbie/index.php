fb9e7a02c10f52989397f47d53308ab582c5c0f4
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-08 15:24:48 -0400

    Update index.php

--- barbie/index.php
+++ barbie/index.php
@@ -3,5 +3,6 @@ $title = 'Barbiet';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'igflobgpdpbjcfggdlipjpbccgnkpahc';
 $tid = basename(__DIR__);
+$testSearch2  = true;
 include('../homepage.php');
 ?>
