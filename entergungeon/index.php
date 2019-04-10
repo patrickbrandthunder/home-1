e5529e6ffffac22bdb311f20c54e36cff71c6142
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-10 10:07:55 -0400

    Update index.php

--- entergungeon/index.php
+++ entergungeon/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Enter the Gungeon';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fjfdkkclnfepnakkdmmobnodmmegldab';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
