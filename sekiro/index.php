548786da40a999bb8e19dda7bdb1aeb3271025e7
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-11 09:04:56 -0400

    Update index.php

--- sekiro/index.php
+++ sekiro/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Sekiro: Shadows';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ncdhninkdfamdikbabfgobigpdpnogol';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
