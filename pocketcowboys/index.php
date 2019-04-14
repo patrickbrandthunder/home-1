e1c32d912fe5dc3a9dbc4ee05a9eb790703a0b8b
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-14 09:57:22 -0400

    Update index.php

--- pocketcowboys/index.php
+++ pocketcowboys/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Pocket Cowboys';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'docbafaknfgcekbkepigfooajlpdbalb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
