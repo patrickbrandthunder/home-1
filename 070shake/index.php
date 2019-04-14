b1b06eebaaec1a44d9d1980ebaedea60000b913c
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-14 09:29:22 -0400

    Update index.php

--- 070shake/index.php
+++ 070shake/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = '070 Shake';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gecjabebjccomfmogebmefgndjmaoglm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
