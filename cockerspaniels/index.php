cfeaca4e075b9be04d88e15db4828a08b0dd1841
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-15 10:31:36 -0400

    Update index.php

--- cockerspaniels/index.php
+++ cockerspaniels/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Cocker Spaniels';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pplojiekahffcaekaaacfbcnopdbephj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
