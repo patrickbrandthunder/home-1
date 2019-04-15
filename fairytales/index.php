7aef170f319dc1fbe41d74f3a49de981fd1c0766
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-15 11:14:43 -0400

    Update index.php

--- fairytales/index.php
+++ fairytales/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Fairy Tales';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'onkejkfkdgijodjeecndbnodhbmgmkkm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
