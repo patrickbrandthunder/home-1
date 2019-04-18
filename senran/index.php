91263f59720e6bdff24ab4bb4877e2051f91f64c
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-18 08:02:40 -0400

    Create index.php

---
+++ senran/index.php
@@ -0,0 +1,8 @@
+
+<?php
+$title = 'Senran Kagura Peach Ball';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
