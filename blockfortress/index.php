53a1087d8ef6c047db779bbce5273ee2ce29015e
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-18 07:58:08 -0400

    Update index.php

--- blockfortress/index.php
+++ blockfortress/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Block Fortress: Empires';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dgngfededgppihmbneolhljfdlcpgkfk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
