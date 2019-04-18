8c34ed2ab2348db3bc472980d4db7deba1a9007e
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-18 08:39:17 -0400

    Update index.php

--- southernliving/index.php
+++ southernliving/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Southern Living';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dlnfjlpoojfflpjojnigkjipkcaaaeoe';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
