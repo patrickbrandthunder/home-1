8dc5637b4e34fa32478ca28baea6aab66c10beb4
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-18 08:25:27 -0400

    Update index.php

--- skiingfred/index.php
+++ skiingfred/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Skiing Fred';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bnmhbiphnigedlohfchkjkilcageghbn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
