af11e5e941d116f27e21e083877b512863a6c1fc
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-18 19:50:54 -0400

    Update index.php

--- ticketearth/index.php
+++ ticketearth/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Ticket To Earth';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gonilgjeiddjnkgjafkbfmllkdhpnbpb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
