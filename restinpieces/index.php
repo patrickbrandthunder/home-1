3203980ce0fce73ca8c8d78c26cdf3b341b25100
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-18 07:58:25 -0400

    Update index.php

--- restinpieces/index.php
+++ restinpieces/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Rest In Pieces';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fpjnhfmaplfdmnbnckpakdncnbgdedpp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
