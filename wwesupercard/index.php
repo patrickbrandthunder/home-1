945d8725ce7fb2a65b56be5a5fd8da1b38354705
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-18 09:34:40 -0400

    Update index.php

--- wwesupercard/index.php
+++ wwesupercard/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'WWE SuperCard';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'komkkgapaimipnccdaokbamlgidedojc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
