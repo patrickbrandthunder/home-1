f4f33b18d79cbb61a2a16d91d8ccca1f9bbd16c5
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-18 14:38:17 -0400

    Update index.php

--- superspell/index.php
+++ superspell/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Super Spell Heroes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cjciamdbpnlhbbgindnaiigimcjjcdcc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
