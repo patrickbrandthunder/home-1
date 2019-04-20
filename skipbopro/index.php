678dfc29f548c9542791f614e342445bc60730fa
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-20 19:40:10 -0400

    Update index.php

--- skipbopro/index.php
+++ skipbopro/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Skip Bo Pro';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'oiajkddmgkefplbioenpmccbphlnjipj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
