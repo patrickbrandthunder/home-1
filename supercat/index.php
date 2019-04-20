f6cc2a16065598842199a37a963fda0014587ea5
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-20 18:41:21 -0400

    Update index.php

--- supercat/index.php
+++ supercat/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Super Cat Tales 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'aokfojlenaaiebniaoamgicbjahabapf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
