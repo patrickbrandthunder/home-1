51f289ca1525f0398c3d7084a5bef2b0a47e73e9
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-20 18:33:52 -0400

    Update index.php

--- partymasters/index.php
+++ partymasters/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Partymasters Fun Idle Game';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fkmhakecinjndameancahclpfccellpc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
