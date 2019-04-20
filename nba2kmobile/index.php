566a508de22830cdc22ec3430a59cf5c95ffc032
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-20 17:58:35 -0400

    Create index.php

---
+++ nba2kmobile/index.php
@@ -0,0 +1,8 @@
+
+<?php
+$title = 'NBA 2K MOBILE';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
