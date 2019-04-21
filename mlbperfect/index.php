1ef0183be34c627ea61ee9c4185b49e66dbf8dce
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-21 17:26:55 -0400

    Update index.php

--- mlbperfect/index.php
+++ mlbperfect/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'MLB Perfect Inning';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dodgkceaellkgdplfjanfgoedmpfcdhl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
