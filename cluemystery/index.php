d1ed5138eb30c9362465abf40a5a2718a68778ae
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-21 08:49:37 -0400

    Update index.php

--- cluemystery/index.php
+++ cluemystery/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Clue Mystery Game';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kpfcgieckhehpkjefkogconnaoghldka';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
