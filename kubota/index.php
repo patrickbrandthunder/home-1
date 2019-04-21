2345a5a8639834f36c2a130bd402d985c10c35a3
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-21 08:26:23 -0400

    Update index.php

--- kubota/index.php
+++ kubota/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Kubota';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nidmofndljiebnkepffjabbgkobcnemn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
