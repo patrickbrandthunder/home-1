9fcdd255191897110289b4ff029e6fa33157f3d5
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-23 10:27:11 -0400

    Update index.php

--- bejeweled/index.php
+++ bejeweled/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bejeweled';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dgidiefngpfbfakbogpgaikgljddppch';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
