a1bcb24c24eb879e6392b026dceb08ce556148cf
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-23 09:47:06 -0400

    Update index.php

--- cannons/index.php
+++ cannons/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Cannons';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nmjfjgmmdjmhbbdmciaemimnnbigbnoo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
