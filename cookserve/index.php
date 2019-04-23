8e0fc58fce54d46b7949bbde106bcab48312dbc6
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-23 10:50:11 -0400

    Update index.php

--- cookserve/index.php
+++ cookserve/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Cook Serve Delicious';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'phjllkiikbnlfkkhnohijggbedogchfo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
