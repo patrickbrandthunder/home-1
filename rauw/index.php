8191ba1417c76e181a354fef04930fbfcf965a40
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-24 19:57:10 -0400

    Create index.php

---
+++ rauw/index.php
@@ -0,0 +1,8 @@
+
+<?php
+$title = 'Rauw Alejandro';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
