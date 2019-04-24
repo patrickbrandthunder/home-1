ae9021d2d09d14c18b174dad74a16b6d582f800b
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-24 14:52:41 -0400

    Update index.php

--- villagepillage/index.php
+++ villagepillage/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Minecraft Village Pillage';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nikiaclcdpjpjcphdcfkhcmnbdegjpnc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
