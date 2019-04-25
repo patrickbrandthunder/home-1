5506297a8098ab5b1822aad0a2b775bb37fef603
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-24 20:17:16 -0400

    Update index.php

--- swtherise/index.php
+++ swtherise/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Star Wars: The Rise of Skywalker';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'abcbmigpcbjlicpeobjdkpohljpoiohi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
