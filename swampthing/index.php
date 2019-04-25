491a1392e0190db97af06c855565dc4267ce108a
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-25 14:49:47 -0400

    Update index.php

--- swampthing/index.php
+++ swampthing/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'DC Swamp Thing';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'boafpdoecncnfhekbfnkhelbkbfedofj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
