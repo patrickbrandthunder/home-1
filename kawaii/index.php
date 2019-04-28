6d33383088553d7c2674807aa6486be905a6e468
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-28 16:22:20 -0400

    Update index.php

--- kawaii/index.php
+++ kawaii/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Kawaii Kitchen';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ejniefaccjefncgkadnaklajlkjanome';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
