30706053955268535d8ef9bc99e8a04c37e29ae0
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-28 16:04:07 -0400

    Update index.php

--- darkiel/index.php
+++ darkiel/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Darkiel';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'anepdhgabeghgpccehbdelhjfjofpjpp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
