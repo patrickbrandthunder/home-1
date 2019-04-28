76af53d690a65ae2ebe5d424c33a446a46cb6c72
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-28 16:14:45 -0400

    Update index.php

--- tool/index.php
+++ tool/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Tool';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lmodgnfbndodcioibpdbkoljccabjjeg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
