3ece81c27ae06d6438983c39c9b55c690a38c8c8
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-29 13:22:50 -0400

    Update index.php

--- tamarin/index.php
+++ tamarin/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Tamarin';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lijajcjcdomhmkhmmmoeogkobmnkjold';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
