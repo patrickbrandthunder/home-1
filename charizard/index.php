8944d39d7cff57e8860bcb85817792ff54602bdb
pjmurphy777 <pjmurphy77@gmail.com>
2019-04-29 08:37:40 -0400

    Update index.php

--- charizard/index.php
+++ charizard/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Charizard';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dnbijhhbdiclllgmgelomabmebgjnfja';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
