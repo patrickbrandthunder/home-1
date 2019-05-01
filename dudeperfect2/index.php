1588f47dce06c31e4327825c18f6009ef5f41d6f
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-01 15:17:53 -0400

    Update index.php

--- dudeperfect2/index.php
+++ dudeperfect2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dude Perfect 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'abjjnebjhgkpgifiaggoggbpodgfkdkb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
