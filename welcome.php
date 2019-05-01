e9b59610a51dfd5f57f3db84b0d9da7e47e2914a
Michael Kaply <consulting@kaply.com>
2019-05-01 08:07:22 -0500

    Add image

--- welcome.php
+++ welcome.php
@@ -2,8 +2,12 @@
 if (!isset($_GET['tid'])) {
     die();
 }
-chdir($_GET['tid']);
+$tid = $_GET['tid'];
+chdir($tid);
 $welcomePage = true;
 include('index.php');
 ?>
+<img src="<?=$tid?>/icon256.png">
+<h3>
 Want more of <?=$title?>? Click <a href="https://chrome.google.com/webstore/detail/<?=$searchExtensionID?>">here</a> to get the search extension!
+</h3>
