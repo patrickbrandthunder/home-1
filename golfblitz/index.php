a13eee28d8df11cd3c0cf98c57983ecf76ed9389
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-01 16:04:20 -0400

    Update index.php

--- golfblitz/index.php
+++ golfblitz/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Golf Blitz';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ombpkkldfaomabgepfbkfakjeihnemee';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
