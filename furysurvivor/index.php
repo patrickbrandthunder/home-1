d64d45b9a0712214abc29c6700ac60a778ead486
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-02 11:13:38 -0400

    Update index.php

--- furysurvivor/index.php
+++ furysurvivor/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Fury Survivor';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dakcigfleblijdekladfkfohbfgjoiek';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
