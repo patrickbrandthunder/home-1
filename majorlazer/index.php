63d10c373d14bad92a4e84eb9ab4333ee87ee179
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-02 15:42:22 -0400

    Update index.php

--- majorlazer/index.php
+++ majorlazer/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Major Lazer';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gfbhedbnhifkmaakblkikdmaamlkbdmn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
