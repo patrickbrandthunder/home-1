982d0f81d192270cdbc41ee71bedaef809507391
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-02 16:11:37 -0400

    Update index.php

--- monbingo/index.php
+++ monbingo/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Monopoly Bingo';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gadjakpniiafpeenfpgjgcihmdckliol';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
