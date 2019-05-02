090278f68f0580343d894860ce2857fe98cb3785
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-02 11:03:23 -0400

    Update index.php

--- dictator/index.php
+++ dictator/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dictator Emergence';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gmnibkfpahblmhjbgjoafaoemgenpjol';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
