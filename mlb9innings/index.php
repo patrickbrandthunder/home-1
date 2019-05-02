7be04152ebcd12e4c2af53915c7879633427ccb0
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-02 16:08:34 -0400

    Update index.php

--- mlb9innings/index.php
+++ mlb9innings/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'MLB 9 Innings';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jfnmgegcnknjbpgehmegmbbdijmclaio';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
