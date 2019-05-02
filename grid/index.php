c6d59d72f9dc80bf052cd851f0c7a6ed4c729a73
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-02 12:22:19 -0400

    Update index.php

--- grid/index.php
+++ grid/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'GRID Autosport';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nopjpcchdoinhnaleeoelandbcpaikkc';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
