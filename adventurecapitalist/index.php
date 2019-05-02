e121e2d467091fbcfe6ac28d1e1bce8b6a6a3ff8
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-02 11:00:05 -0400

    Update index.php

--- adventurecapitalist/index.php
+++ adventurecapitalist/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Adventure Capitalist';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hgljoonmfgjnfoholcejhbfbdhodmhhg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
