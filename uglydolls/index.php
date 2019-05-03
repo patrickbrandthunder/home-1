ae9312473e01b9300ec25606ddb12008919cf670
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-03 10:51:43 -0400

    Update index.php

--- uglydolls/index.php
+++ uglydolls/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Ugly Dolls';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kpdakpiojekhgndkipghkcfedecnlcne';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
