21ebc98a518ee86e0cbe261c81202380d2881da1
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-03 10:21:46 -0400

    Update index.php

--- oldtime/index.php
+++ oldtime/index.php
@@ -2,6 +2,7 @@
 $title = 'Old Town Road Remix';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ainjfhplaffjhfmbmjbojnlmincgkbpm';
+$searchExtensionID = 'fddfllipmbhffmdjieikopchofkdamle';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
