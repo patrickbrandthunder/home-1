e1e77f7ab25311747efa67cfbdda7ae5f6d4f917
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-03 11:29:08 -0400

    Update index.php

--- rottweiler/index.php
+++ rottweiler/index.php
@@ -2,6 +2,7 @@
 $title = 'Rottweilers';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'hilmnlipeanjlmoppfgkdcghhghfglko';
+$searchExtensionID = 'mbfcgcppgncijdjolmocgdbpinolbdho';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
