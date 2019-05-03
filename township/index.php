0e677a32fc4d0410821a24bb2b6ccb9bb3d8033c
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-03 11:03:53 -0400

    Update index.php

--- township/index.php
+++ township/index.php
@@ -2,6 +2,7 @@
 $title = 'Township';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ljehdgdbdgfdljjbblacddnkhjigjhic';
+$searchExtensionID = 'hlohnddgcidghcghbgaonoilgppdkmoa';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
