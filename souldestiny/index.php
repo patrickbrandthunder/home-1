edc107ff6380ad8b3ea8ba047753218ea6d609b7
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-09 17:42:12 -0400

    Update index.php

--- souldestiny/index.php
+++ souldestiny/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Soul Destiny';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pcdgjphopobgafcabfecfhppkdnoonio';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
