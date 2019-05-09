1bb6e1aa44c9b5223e27229cc7267b153cf657d8
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-09 15:14:44 -0400

    Update index.php

--- asusrog/index.php
+++ asusrog/index.php
@@ -3,6 +3,6 @@ $title = 'ASUS ROG';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'jloneboapdconggolhbngkfknbicddoa';
 $tid = basename(__DIR__);
-$testSearch2  = true;
+$searchExtensionID = 'kmbchefellfppmbookapdjlcdenfijnd';
 include('../homepage.php');
 ?>
