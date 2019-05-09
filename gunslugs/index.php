2bb029fd167e298f1b75b55880103f5bd86d1d76
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-09 17:58:09 -0400

    Update index.php

--- gunslugs/index.php
+++ gunslugs/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Gunslugs';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cmfbmpegckkkichanlbgcilhgkfhhbie';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
