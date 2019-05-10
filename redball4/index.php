c31aa538cdaee8d41444aa4943e617f4d2ef0a1c
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-10 10:27:50 -0400

    Update index.php

--- redball4/index.php
+++ redball4/index.php
@@ -2,6 +2,7 @@
 $title = 'Red Ball 4';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'nbgfilfkdpbkikmchlnfjmobmoapoigh';
+$searchExtensionID = 'gokckeeapjjkcfaenjcpndgcoieimpmp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
