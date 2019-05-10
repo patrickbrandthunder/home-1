d6f4c308e358ec457f1b84e39aa15ced8b1957cb
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-10 15:19:19 -0400

    Update index.php

--- nikeair/index.php
+++ nikeair/index.php
@@ -2,6 +2,7 @@
 $title = 'NIKE AIR MAX 720';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ohihgdmobkljcccphlghnknjdhdhngbm';
+$searchExtensionID = 'cpcdfpcemgkigbdjlflmhhnicigchgnd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
