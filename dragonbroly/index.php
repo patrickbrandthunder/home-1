980325b11143c5aefd10298280477a4daff2050b
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-10 10:00:59 -0400

    Update index.php

--- dragonbroly/index.php
+++ dragonbroly/index.php
@@ -3,6 +3,6 @@ $title = 'Super Dragon Broly';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'gdjpneilpaakfdkbofbdcoggimplfjek';
 $tid = basename(__DIR__);
-$testSearch2  = true;
+$searchExtensionID = 'fbjnlcgjfliikpoadnpaeaekgkkdkckb';
 include('../homepage.php');
 ?>
