ee1abac8dcb7a381b642bf38d1834c01e66f59ea
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-10 15:20:05 -0400

    Update index.php

--- thegrinch2/index.php
+++ thegrinch2/index.php
@@ -3,6 +3,7 @@
 $title = 'The Grinch 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'lcdfpjghkcegankjbngpbjhfcgflmehj';
+$searchExtensionID = 'pghpemipedlookahoafpajgnikjieiac';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
