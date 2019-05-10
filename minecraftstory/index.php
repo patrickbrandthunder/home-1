4dc595ab5050f98c5014ecfab27be428308eb46b
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-10 10:07:21 -0400

    Update index.php

--- minecraftstory/index.php
+++ minecraftstory/index.php
@@ -2,6 +2,7 @@
 $title = 'Minecraft Story Mode';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'jcpkhkbclgccajdklalphhbnkpbokand';
+$searchExtensionID = 'eaddenfilhieopifbklkgpkomcmamfdn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
