4e1f719849f3a420eab6516e559ee9acac17fb18
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-11 09:01:37 -0400

    Update index.php

--- bhadbarbie/index.php
+++ bhadbarbie/index.php
@@ -2,6 +2,7 @@
 $title = 'Bhad Barbie';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'mjieiemindaefkbhhkgbfgioodcolldb';
+$searchExtensionID = 'okngdbgkpnhjamkocogbljkdomehmfcp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
