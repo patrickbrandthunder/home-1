c5d47e3fed28b9b570bb76d6a2d2340eb0e7b884
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-13 11:27:56 -0400

    Update index.php

--- flyfishing/index.php
+++ flyfishing/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Fly Fishing';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gkphcjfhacnifhdbpjdmajhgfoainepn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
