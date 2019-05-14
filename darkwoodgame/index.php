ceded4fa1a780591cac771507466edbdc40bf8bc
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-14 15:42:43 -0400

    Update index.php

--- darkwoodgame/index.php
+++ darkwoodgame/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Darkwood Game';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ajhdemhbgamambegcgicbigejdpbhbko';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
