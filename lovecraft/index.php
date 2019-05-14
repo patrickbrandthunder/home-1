befd30fdb61660704bf0f0f0a8421a1901ef99da
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-14 15:13:00 -0400

    Update index.php

--- lovecraft/index.php
+++ lovecraft/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Tesla VS Lovecraft';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ehbnfgaehccjlllkfapfckgingdcckdl';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
