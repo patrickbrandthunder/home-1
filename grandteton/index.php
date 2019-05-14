2ce6590b39a138f0d97ebe78605da48dd79896e8
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-14 14:41:17 -0400

    Update index.php

--- grandteton/index.php
+++ grandteton/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Grand Teton';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'emibnpbeajcklaailcmgcdnoeinbpbnd';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
