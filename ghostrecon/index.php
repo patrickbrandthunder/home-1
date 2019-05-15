d3d43c4b348ba5f7f137f2c069dc78281ddcb613
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-15 08:53:52 -0400

    Update index.php

--- ghostrecon/index.php
+++ ghostrecon/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Ghost Recon Breakpoint';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gmplbelcfjnfglbpopeeaegglldpmhfl';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
