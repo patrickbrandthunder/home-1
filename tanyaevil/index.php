266930db9a8a7e6422974de9ce68e020cf7b1241
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-15 12:36:44 -0400

    Update index.php

--- tanyaevil/index.php
+++ tanyaevil/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Saga of Tanya the Evil';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dlfpkhkhlmgjaijaflbbdeolcgkecaop';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
