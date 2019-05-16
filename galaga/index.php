e18ed5776e8ffb8c492b41e28adfd1b794bb9636
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-16 16:31:33 -0400

    Update index.php

--- galaga/index.php
+++ galaga/index.php
@@ -2,6 +2,7 @@
 $title = 'Galaga';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'gjbgbmfjncohpenjoojhgnenjnimcdbm';
+$searchExtensionID = 'pjmbaonaoibfadbomkemglldkdhpilei';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
