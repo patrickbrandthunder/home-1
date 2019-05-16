a9d505f1c1f5e84c5481d25d3ab4a7621fda7852
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-16 10:43:52 -0400

    Update index.php

--- itchapter2/index.php
+++ itchapter2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'IT Chapter 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ikkpbhdpdmkoihojejcimplclapgnehf';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
