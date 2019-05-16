95c5bcee8286d62683b43275924ee1aa34dbd9f4
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-16 09:09:47 -0400

    Update index.php

--- birdwatching/index.php
+++ birdwatching/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Bird Watching';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bdpckbgoccbnfionlhcngjojfldnobak';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
