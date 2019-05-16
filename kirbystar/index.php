517c0885c25f5bd85f0d8a0b8c6f79aba07fbbc4
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-16 11:02:36 -0400

    Update index.php

--- kirbystar/index.php
+++ kirbystar/index.php
@@ -2,6 +2,7 @@
 $title = 'Kirby Star';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ffopknikijceocdkgoccnebcaeceimen';
+$searchExtensionID = 'apidfoogpilkdkclchdmfneckmdkokng';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
