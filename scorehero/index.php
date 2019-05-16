faab4c05848da87829a71854ebc5f322c8c8f7e4
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-16 16:37:32 -0400

    Update index.php

--- scorehero/index.php
+++ scorehero/index.php
@@ -3,6 +3,7 @@
 $title = 'Score Hero';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'lanpamdhfhncaoljnipbllnebmnlchjo';
+$searchExtensionID = 'nhnaphmbmebklnpoeofmnipackgpmiid';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
