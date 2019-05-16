8558c17e5889a48ebc561ec623ebfb727fd8eb58
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-16 16:23:41 -0400

    Update index.php

--- subnautica/index.php
+++ subnautica/index.php
@@ -2,6 +2,7 @@
 $title = 'Subnautica';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'mnjmpophgmnfngfljikmefemledlagnk';
+$searchExtensionID = 'chnimaldmaoahchcpkicandplphghhhi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
