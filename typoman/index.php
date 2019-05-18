122864f6cbab914dd1f6fa61fb4c31fce4e94281
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-18 08:31:52 -0400

    Update index.php

--- typoman/index.php
+++ typoman/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Typoman Mobile';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hmbadomkcijmehfdhmpehajnkfbhcoff';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
