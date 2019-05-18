13c4e2605e24a4fe0aba1010114220fe1a5ab3af
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-18 10:05:57 -0400

    Update index.php

--- jeffreestar/index.php
+++ jeffreestar/index.php
@@ -2,6 +2,7 @@
 $title = 'Jeffree Star';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'laeecfjeidelmjlofciefheanmdbhjoa';
+$searchExtensionID = 'ckefpmojghlaofdenelmbepihcmgckgb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
