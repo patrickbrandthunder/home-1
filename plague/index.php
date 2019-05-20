5c52da0c9387e27df06e279f1112ea4fd517a379
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-20 13:25:23 -0400

    Update index.php

--- plague/index.php
+++ plague/index.php
@@ -2,6 +2,7 @@
 $title = 'Plague Inc';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'lgglnjfaglblnglkdmmdhmjcpplmjdfj';
+$searchExtensionID = 'leehidjdplikoeocbgcckcnnjnblejkd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
