7ea3703dc20fd41fbb3f9465652eb7d9eb491a04
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-20 15:48:57 -0400

    Create index.php

---
+++ abirdstrans/index.php
@@ -0,0 +1,9 @@
+
+<?php
+$title = 'Angry Birds Transformers';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
