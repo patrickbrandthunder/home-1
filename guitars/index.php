050a8dd7e96af6d8272824f4d942479af5dd8f37
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-20 10:42:04 -0400

    Update index.php

--- guitars/index.php
+++ guitars/index.php
@@ -2,6 +2,7 @@
 $title = 'Guitars';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'jgglchakgfcknbhklibddgnppefmbhpf';
+$searchExtensionID = 'onedcpeihonlpbadnnieidkaakfcbjhc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
