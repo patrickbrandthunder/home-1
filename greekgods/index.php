5b5bc88513fda79b5ba022748f59ebeacd541d65
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-20 09:13:31 -0400

    Update index.php

--- greekgods/index.php
+++ greekgods/index.php
@@ -3,6 +3,7 @@
 $title = 'Greek Gods';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ojghidnehnahhifhpfnjaghplaadjhmm';
+$searchExtensionID = 'mbblfngoegocoemagcalhpnnjhcfahej';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
