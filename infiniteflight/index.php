0724d47076ada2eae44f9bb13c31274170a291bf
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-20 12:57:00 -0400

    Update index.php

--- infiniteflight/index.php
+++ infiniteflight/index.php
@@ -3,6 +3,7 @@
 $title = 'Infinite Flight';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'oacggaajnmhcpecpmfpndcdeligdanao';
+$searchExtensionID = 'eapjdfhiankimmkconjoacbbmmlkpkcf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
