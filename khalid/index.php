3c47263eea8149062d89d8494bd82cfecc4ca64c
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-20 09:05:08 -0400

    Update index.php

--- khalid/index.php
+++ khalid/index.php
@@ -2,6 +2,7 @@
 $title = 'Khalid';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'pgkechagagiajlncgmcnljhnahbkpiah';
+$searchExtensionID = 'nkekflaandcpiigcnpkdgdeajdfobdmh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
