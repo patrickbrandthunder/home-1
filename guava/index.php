969bf1d8c61b1c9b4908970a3ff599908f875522
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-20 10:03:37 -0400

    Update index.php

--- guava/index.php
+++ guava/index.php
@@ -2,6 +2,7 @@
 $title = 'Guava Juice Tub Tapper';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'flldhkaccdccecmnkgcmadenipdoldmj';
+$searchExtensionID = 'joccmfejjnejhlhhdckhhofiiomhbeib';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
