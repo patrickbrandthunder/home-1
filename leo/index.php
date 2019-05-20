39211ca0ef49cd5f458c9f03bbf5614aca5e0ecd
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-20 13:35:36 -0400

    Update index.php

--- leo/index.php
+++ leo/index.php
@@ -2,6 +2,7 @@
 $title = 'Leo';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'kekfkfgghlfdoeledkkhdlcbabebmcjd';
+$searchExtensionID = 'bokakckjmbeooecpagalojbeibnjjoke';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
