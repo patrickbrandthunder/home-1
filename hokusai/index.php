c86655f660c243291e9aeebedeab9d14fad6b699
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-21 13:28:38 -0400

    Update index.php

--- hokusai/index.php
+++ hokusai/index.php
@@ -2,6 +2,7 @@
 $title = 'Hokusai Wave';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ffjlcbgjpoplimefoblpjiicleomknhm';
+$searchExtensionID = 'gfgokjlddadmlljdcahablifcankkhbk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
