ce68e6c0ce4ad9735db35d07d134caf39449d32f
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-21 11:43:59 -0400

    Update index.php

--- everest/index.php
+++ everest/index.php
@@ -2,6 +2,7 @@
 $title = 'Mount Everest';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'cpejoehjijgncmopfklamffinoapadoa';
+$searchExtensionID = 'bidfldpgdianneicadbhenkcdlhnfcpg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
