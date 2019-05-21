6993b57d4b854cecc4b79c6c34a1d54b50bf9e9b
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-21 12:11:57 -0400

    Update index.php

--- commedesgarcons/index.php
+++ commedesgarcons/index.php
@@ -2,6 +2,7 @@
 $title = 'Coome Des Garcons';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'jlmokpclbfolodekaanekngeogfmjjlg';
+$searchExtensionID = 'dohdoogmhenlojbkgakcihchhejlglid';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
