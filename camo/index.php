b5a0d1ff09ad3f8906a9b932c0a3b01987469645
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-22 10:12:17 -0400

    Update index.php

--- camo/index.php
+++ camo/index.php
@@ -2,6 +2,7 @@
 $title = 'Camo';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'kkagabffhemaeaaheboojinebcbfdjea';
+$searchExtensionID = 'cggokcnafdnmjeomjlkcokbbdfopiald';
 $tid = basename(__DIR__);
 $testSearch2 = true;
 include('../homepage.php');
