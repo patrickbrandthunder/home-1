882d1a4f1e974109a4f812dc198cbe367d38ea96
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-22 10:56:31 -0400

    Update index.php

--- sailboatsunset/index.php
+++ sailboatsunset/index.php
@@ -2,6 +2,7 @@
 $title = 'Sailbnoat at Sunset';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'hehnmobhdjnejpkckiaclpedoeoekcha';
+$searchExtensionID = 'mjbigiaojpgmnbodgpnfhopfahealjlo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
