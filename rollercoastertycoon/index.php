dc142d1c231604dbe824ba8b9141c9482e74d8cd
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-22 13:25:01 -0400

    Update index.php

--- rollercoastertycoon/index.php
+++ rollercoastertycoon/index.php
@@ -1,7 +1,8 @@
 <?php
 $title = 'Rollercoaster Tycoon';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'baafnlgkkjgnghgkfiobablhigdlinfi';
+$searchExtensionID = 'lmcakhjmjhggembfjojgbpaphilbgljh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
