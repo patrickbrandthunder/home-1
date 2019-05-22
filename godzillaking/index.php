c2777369bd62cdb6d217eddf56289a3a928602d6
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-22 15:06:03 -0400

    Update index.php

--- godzillaking/index.php
+++ godzillaking/index.php
@@ -2,6 +2,7 @@
 $title = 'Godzilla King';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ekfflehpnfjghlcjolngelaandfebmmd';
+$searchExtensionID = 'jpibdhiebachfefdkhhjgmkphfcoblhn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
