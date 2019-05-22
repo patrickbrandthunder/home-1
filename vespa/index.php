99245053892fbd02e00e7b3931a90a9d64540a5b
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-22 14:40:41 -0400

    Update index.php

--- vespa/index.php
+++ vespa/index.php
@@ -2,6 +2,7 @@
 $title = 'Vespa';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'cplkdiflpkehpfpkjldahdandedcelbl';
+$searchExtensionID = 'oigjchkechgglkdpnkihbpikbejdogkn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
