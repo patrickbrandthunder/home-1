d64c44516fdae3b9f5de0dd688643b3d9705064d
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-22 10:37:45 -0400

    Update index.php

--- keyglock/index.php
+++ keyglock/index.php
@@ -2,6 +2,7 @@
 $title = 'Key Glock';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'igaamblchoebcpbegbhlkjgaejacfofo';
+$searchExtensionID = 'oafldnofapaffdefajbkdkddeaaegajf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
