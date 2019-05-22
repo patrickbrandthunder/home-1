30821e1d554c9a9b0f3e9d3841cb081100eca4d2
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-22 10:19:22 -0400

    Update index.php

--- honkai/index.php
+++ honkai/index.php
@@ -2,6 +2,7 @@
 $title = 'Honkai';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'jlmcinohejhdopcahhbdmefkfgmimejj';
+$searchExtensionID = 'dejjcnmddibpkooniickagobbkilnlbi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
