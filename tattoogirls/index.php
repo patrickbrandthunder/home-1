d3cdc2e47c49981564f843ccc2d726618de453d9
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-22 09:46:39 -0400

    Update index.php

--- tattoogirls/index.php
+++ tattoogirls/index.php
@@ -2,6 +2,7 @@
 $title = 'Tattoo Girls';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'hfnligobajhglnpcgcknaendjljcdcnm';
+$searchExtensionID = 'ejaphckcockpjdeolhlfankhioknnhll';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
