0ce3600bd33752ae79fc9ce7577e548723b6a267
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-23 15:53:25 -0400

    Update index.php

--- footballmanager/index.php
+++ footballmanager/index.php
@@ -2,6 +2,7 @@
 $title = 'Football Manager FM 2019';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'imllabkhimkaamjbknmogajdpmcllbnd';
+$searchExtensionID = 'cjmcgennlamilkmafednfhkgcfeblopn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
