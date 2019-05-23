ed05aca84eab720a7a902674eab56a3c7937f06b
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-23 15:24:29 -0400

    Update index.php

--- screamgohero/index.php
+++ screamgohero/index.php
@@ -3,6 +3,7 @@
 $title = 'Scream Go Hero';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'depldcaaaoedhjhdkfmjcdeddljgppbm';
+$searchExtensionID = 'keckjobakjjobfnnfkjaadhaodeepgjh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
