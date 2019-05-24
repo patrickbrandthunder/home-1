06b85eb2b7eb3b5886faf196c4823bbbd73260b3
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-24 10:03:43 -0400

    Update index.php

--- fistnorth/index.php
+++ fistnorth/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Fist Of The North';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cbpneahmmmggpkdoddklmcififlmicfg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
