6f2cbfae7d6e59e5ac4ca4a0b8d99701b0921457
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-27 08:28:19 -0400

    Update index.php

--- indian/index.php
+++ indian/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Indian Motorcycles';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'omdeclpbjcjmnfbfeacioadoacgiofce';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
