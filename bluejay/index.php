48fe2143deac33120120bc31d9175dfd1309e88f
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-27 08:08:41 -0400

    Update index.php

--- bluejay/index.php
+++ bluejay/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bluejay Bird';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fcgagmebengjomigjlgfnfogblljcmdh';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
