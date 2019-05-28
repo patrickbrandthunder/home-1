7974f1e7beeb6847430b16528dacc10db1315cb9
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-28 15:35:02 -0400

    Create index.php

---
+++ axethrowing/index.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'Axe Throwing';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
