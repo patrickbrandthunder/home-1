3ba9bed9fd3025ecb84127e7f51da91e368787bf
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-28 11:29:20 -0400

    Update index.php

--- morningdew/index.php
+++ morningdew/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Morning Dew';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'imbfcjfhddaofahnonmbbcabcjakhmdf';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
