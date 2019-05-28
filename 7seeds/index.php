ca51f370795a0ff557fa657f3a45a1b57a0ab0ab
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-28 09:48:57 -0400

    Update index.php

--- 7seeds/index.php
+++ 7seeds/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = '7SEEDS';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jinjcefgagljmmfickidjaedakhdalcl';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
