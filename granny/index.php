b382c084fc56e0ee59242617b0568afe8420ae5b
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-28 09:30:29 -0400

    Update index.php

--- granny/index.php
+++ granny/index.php
@@ -3,6 +3,7 @@ $title = 'Granny';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'mecbngdjmjmalachlibbdehhpojgenhe';
 $searchExtensionID = 'eaajnjineiclfocnpmedeilmipbnjkpk';
+$testSearch2  = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
