ade7cfbffa1948137b1f0b0f97f98d40d943bb6e
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-29 14:55:31 -0400

    Update index.php

--- klondike/index.php
+++ klondike/index.php
@@ -2,6 +2,7 @@
 $title = 'Klondike Adventures';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'bfhpgibbkdpgaghinlkanhcldkppoogi';
+$searchExtensionID = 'nilpmkccjgcjgigodkefncmjblknncfp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
