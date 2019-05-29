786cb7f301fef6dbc17a72ee18f02213075202da
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-29 09:52:48 -0400

    Update index.php

--- idleheroes/index.php
+++ idleheroes/index.php
@@ -2,6 +2,7 @@
 $title = 'Idle Heroes';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'cafifojpdlnmhahjndgblallnmmcdmdo';
+$searchExtensionID = 'jdhaplbaepdoacpikaanjnmldfhfcacn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
