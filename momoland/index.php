52b3e623d10019711029ed62cd3b5941e72b181e
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-29 15:29:06 -0400

    Update index.php

--- momoland/index.php
+++ momoland/index.php
@@ -3,6 +3,7 @@
 $title = 'Momoland';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'pkkeeiknmlccfngdimhfdcjcbmgadghk';
+$searchExtensionID = 'nbhbfchfjibppkbilfpnepljadiipdci';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
