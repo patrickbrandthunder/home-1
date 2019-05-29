4d6d33f013ead17c0253c6694d0874243d60f58c
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-29 14:47:08 -0400

    Update index.php

--- crashofcars/index.php
+++ crashofcars/index.php
@@ -2,6 +2,7 @@
 $title = 'Crash Of Cars';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'oejdmhdelgjiiodeaelnhdeedmchfkca';
+$searchExtensionID = 'difdpocaingainjhobkdndgjmdhonkeg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
