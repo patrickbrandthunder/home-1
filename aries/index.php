1e35093b3f083c8c458a24967dbaa62e50180fd6
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-29 10:24:08 -0400

    Update index.php

--- aries/index.php
+++ aries/index.php
@@ -2,6 +2,7 @@
 $title = 'Aries';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ckpiojbppkbnojgilhblmkfenogcpmmn';
+$searchExtensionID = 'giaakamebeidoomppgijndabedoliamd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
