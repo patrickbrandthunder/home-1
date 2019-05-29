9fdda7595e08d0b4e37077d368e1e8db34332c08
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-29 15:11:05 -0400

    Update index.php

--- yinyang/index.php
+++ yinyang/index.php
@@ -2,6 +2,7 @@
 $title = 'Yin and Yang';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'feildbecioellehlgghokdfccpdkaekf';
+$searchExtensionID = 'mejmaobmagpafbahnbnmikaipihopnim';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
