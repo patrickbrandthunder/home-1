d016c355a721afb4f22a270f72a1c672ede22e3e
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-29 10:53:44 -0400

    Update index.php

--- catscrash/index.php
+++ catscrash/index.php
@@ -2,6 +2,7 @@
 $title = 'CATS Crash';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'dmpffapjomeajmognbhfjgaclicfbkjg';
+$searchExtensionID = 'gfolamggpflmdmdgglgicmlihodphclj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
