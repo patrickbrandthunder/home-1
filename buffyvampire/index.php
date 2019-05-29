989e95c3cc306b6e7eb8677892611d9882d854c7
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-29 15:19:29 -0400

    Update index.php

--- buffyvampire/index.php
+++ buffyvampire/index.php
@@ -1,7 +1,8 @@
 <?php
-$title = 'BUffy Vampire Slayert';
+$title = 'Buffy Vampire Slayer';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'fiicaeodhlolebiimgmlfkebmfnfnoil';
+$searchExtensionID = 'gmapomibjemeefbblmmhdbpjckkleffm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
