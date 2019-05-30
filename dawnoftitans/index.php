18cdab53fcb2c6e19e62969429d57acea75e93ad
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-30 16:08:55 -0400

    Update index.php

--- dawnoftitans/index.php
+++ dawnoftitans/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dawn Of Titans';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'igljebglbpchaffnehjlphobjpomlknm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
