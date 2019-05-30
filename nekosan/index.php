a4d05c6c643b1455ef6001160b2eece4a333415e
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-30 16:21:59 -0400

    Update index.php

--- nekosan/index.php
+++ nekosan/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Nekosan';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fpepaiaipgkhjjnnkcdeebbkpmildgoi';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
