61b7f8356ca897a25b8f9080dd5ad425dedb847d
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-30 16:15:38 -0400

    Update index.php

--- buddha/index.php
+++ buddha/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Buddha';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nffjfhjdeelgojcbpkkinnfjakenglcd';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
