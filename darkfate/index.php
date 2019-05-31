945729917a521c24c61c8ed999c8830839eca0fe
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-31 09:58:18 -0400

    Update index.php

--- darkfate/index.php
+++ darkfate/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Terminator Dark Fate';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ikjibmldpplocpgpfeembhgedeigebnf';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
