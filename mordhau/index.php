9fb7121114ad80f45476d181ce34c14344cfb4be
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-31 09:49:48 -0400

    Update index.php

--- mordhau/index.php
+++ mordhau/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Mordhau';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'allhagigidadmpcfebjiclhnnhkolkoe';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
