7edc835d827ddf0609cb7878f4ee9d00419850ff
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-31 10:04:14 -0400

    Update index.php

--- oddworld/index.php
+++ oddworld/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Oddworld Soul Storm';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cokemlfoomglefmjidolpdinnabbgbih';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
