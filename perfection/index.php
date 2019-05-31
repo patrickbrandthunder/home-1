7555c7c436c369ba2ceb453ccc41ec442b1eee61
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-31 10:54:07 -0400

    Update index.php

--- perfection/index.php
+++ perfection/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'The Perfection';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ndboecbjfkfmcecjgbdllcaaojhdipfh';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
