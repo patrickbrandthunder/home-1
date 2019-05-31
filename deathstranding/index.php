ab0252e74294b2cf91920d82530d0352398aff01
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-31 10:42:27 -0400

    Update index.php

--- deathstranding/index.php
+++ deathstranding/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Death Stranding';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bgjgejgliekkjaloiimgbjgbjdfmnngo';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
