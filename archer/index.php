b02578591851a33f8043fee0b48d11ff0a292b36
pjmurphy777 <pjmurphy77@gmail.com>
2019-05-31 10:26:21 -0400

    Update index.php

--- archer/index.php
+++ archer/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Archer 1999';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'honknohgaihoknjdofknkcfmfbhidkkb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
