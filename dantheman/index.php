1fb30dfe57920acc96c43bdf701fa330c60ed2c4
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-04 16:45:22 -0400

    Update index.php

--- dantheman/index.php
+++ dantheman/index.php
@@ -2,6 +2,7 @@
 $title = 'Dan The Man';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'khghjminipchkjgdoflloebegkpaiobn';
+$searchExtensionID = 'lcmmpobbknommjaioapdjadlaapmkdbo';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
