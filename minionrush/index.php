0b02540bad1f2f644278bc46088657cbb0ba5527
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-04 15:39:44 -0400

    Update index.php

--- minionrush/index.php
+++ minionrush/index.php
@@ -2,6 +2,7 @@
 $title = 'Minion Rush';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'olmihmofmhgbfeakofemcmohfmlbgkag';
+$searchExtensionID = 'mbffcaakojgkakpoaodkofloahhinbcl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
