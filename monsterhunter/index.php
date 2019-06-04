f1d7afb38512f61b4434c2081e9c32660ba01202
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-04 12:30:56 -0400

    Update index.php

--- monsterhunter/index.php
+++ monsterhunter/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Monster Hunter World: Iceborne';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fjiifbanengimmfcemhojodplplnpboo';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
