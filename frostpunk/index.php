909e0935174144da33717d647ada61ef34197127
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-05 09:09:31 -0400

    Update index.php

--- frostpunk/index.php
+++ frostpunk/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Frostpunk';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'igmlenbnfmdjoalekbmhcbfnhlmnjeao';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
