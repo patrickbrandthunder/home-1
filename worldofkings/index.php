804e7c4e37c0ab35a533498faa2da69f4b67f5e3
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-05 10:12:57 -0400

    Update index.php

--- worldofkings/index.php
+++ worldofkings/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'World Of Kings';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cmkmdhgdncnaecojljdlfcendjfepkhl';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
