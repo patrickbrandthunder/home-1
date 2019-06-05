31e289f731ad0ca8589166155eb0ed708162a0c3
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-05 08:53:20 -0400

    Update index.php

--- amongthesleep/index.php
+++ amongthesleep/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Among The Sleep';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'camgbmamghhkoadhiklknglflnnbdabl';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
