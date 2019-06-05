c1b13cf1e32107e6b991ba6996c3f4bcfe002ed1
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-05 09:01:53 -0400

    Update index.php

--- billyjoel/index.php
+++ billyjoel/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Billy Joel';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'adjpmokclbabiaogjbiokmgimeogclkj';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
