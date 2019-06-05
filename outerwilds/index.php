3a413edcfd2fc6a44492979852daf517ec36c793
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-05 09:25:42 -0400

    Update index.php

--- outerwilds/index.php
+++ outerwilds/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Outer Wilds';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cgakajiphpgcpkfndidljcckciamailn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
