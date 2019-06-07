434a39764611bf7470ec0eb29a6db23c6e004679
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-07 10:55:12 -0400

    Update index.php

--- incredibox/index.php
+++ incredibox/index.php
@@ -2,6 +2,7 @@
 $title = 'Incredibox';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'calpbohgndeojkllnhppcgcnaopnjnoj';
+$searchExtensionID = 'aidddlbonmkjlfnpelgcggijgacjbkfh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
