ab5cbf256b5ff1114faeca54bf7a55b15a3ed0d8
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-07 12:00:31 -0400

    Update index.php

--- gettingoverit/index.php
+++ gettingoverit/index.php
@@ -2,6 +2,7 @@
 $title = 'Getting Over It';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'folhjohjifoojfgpbdlohmnljomacknj';
+$searchExtensionID = 'cjnodhknpeohlmoljmfdjbjnkomekhdf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
