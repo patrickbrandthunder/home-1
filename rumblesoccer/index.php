9cdba332e02dde2eb79ecb0a9a1d4e69ec02fd02
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-07 11:02:11 -0400

    Update index.php

--- rumblesoccer/index.php
+++ rumblesoccer/index.php
@@ -2,6 +2,7 @@
 $title = 'Rumble Stars Soccer';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'pochheacnmgggdfgdbcalcccecoamibi';
+$searchExtensionID = 'mcmhafhjpdjomnknmhbndnigkahpcach';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
