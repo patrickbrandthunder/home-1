4b64f0ee70dc3b6d1906f569b12b692524291c3a
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-07 14:24:29 -0400

    Update index.php

--- springflowers/index.php
+++ springflowers/index.php
@@ -2,6 +2,7 @@
 $title = 'Spring Flowers';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'nlobgdgdagbehicejnkjklmjfibhbfbb';
+$searchExtensionID = 'jogbpnigcoojoejgoinmahbonclgoknb';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
