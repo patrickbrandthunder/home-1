4be31d0e66cd13fc396e82a62d1130e3eedfae44
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-07 11:54:18 -0400

    Update index.php

--- vainglory/index.php
+++ vainglory/index.php
@@ -2,6 +2,7 @@
 $title = 'Vain Glory';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'booeibfbdghbmdddjkehmbaegbmmeicb';
+$searchExtensionID = 'igomlkngbcifijlmpkopfadjalmbbkfk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
