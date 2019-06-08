c4bb25ecf234851d58d1bf1ccc509ed1b3fa04f7
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-08 08:34:41 -0400

    Update index.php

--- troyesivan/index.php
+++ troyesivan/index.php
@@ -1,7 +1,8 @@
 <?php
-$title = 'Troye SIvan';
+$title = 'Troye Sivan';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ffmcmpkaloknejnchibickpcedhlomhh';
+$searchExtensionID = 'nglilmdbgmodeialbialmngodnaikjnc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
