117befdd8351c503da8a100dd9a71a591f20eb54
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-08 09:52:27 -0400

    Update index.php

--- abominable/index.php
+++ abominable/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Abominable';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fbegihbngmpgbjlfalplfolacgfjonep';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
