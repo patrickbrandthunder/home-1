d287a09b7f9ca46a9c3bd35bed8eeebc4156a281
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-08 08:54:13 -0400

    Update index.php

--- escapists/index.php
+++ escapists/index.php
@@ -2,6 +2,7 @@
 $title = 'Escapists 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'bgenmnhkdcmlelfaipponejbbljlmjpf';
+$searchExtensionID = 'amfbompndnlhcngbelmiafolakgdcojn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
