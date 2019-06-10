caa2029226e62f446129c43f71e3c74bcb9c14b1
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-10 16:43:36 -0400

    Update index.php

--- baldur/index.php
+++ baldur/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Baldur's Gate 3';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pkojfemmblekelpbeppalmdcmlijnbfc';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
