426c611751cbc595d4dbc7cd5ad12d91a87b8a1c
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-11 09:15:59 -0400

    Update index.php

--- destroyhumans/index.php
+++ destroyhumans/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Destroy All Humans';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nmpnjpjgllefgignjpghfblnnjhbbeec';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
