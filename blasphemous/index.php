3878a3180b858001acdc155faf7315fecb9bc036
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-12 11:09:04 -0400

    Update index.php

--- blasphemous/index.php
+++ blasphemous/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Blasphemous';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'afmcfkeengfgoelekhiiehccedpddaem';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
