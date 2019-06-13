95e6336adf3eb74b2c9ae24d13b457ad0be2cb9b
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-13 10:20:49 -0400

    Update index.php

--- chayanne/index.php
+++ chayanne/index.php
@@ -2,6 +2,7 @@
 $title = 'Chayanne';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'lgnghfdoeaniiiojjkamoilgjjbjigie';
+$searchExtensionID = 'ecgmpimafkpgkjbjfkcpigenbmgdlbdd';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
