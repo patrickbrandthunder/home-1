5170c423733434585c7aa63831dfeacc2fe6c52e
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-13 14:02:40 -0400

    Update index.php

--- doometernal/index.php
+++ doometernal/index.php
@@ -2,6 +2,7 @@
 $title = 'Doom Eternal';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ikgccljjdplfmcpafcklhjibbcfackdb';
+$searchExtensionID = 'dgbnhdbkfcojneebjdigkgmjldmmbmdg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
