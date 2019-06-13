4ba8fc6f4e91ca501c29c7fc071b8958ef2f96d3
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-13 12:22:24 -0400

    Update index.php

--- darkwood/index.php
+++ darkwood/index.php
@@ -2,6 +2,7 @@
 $title = 'Dark Wood Texture';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'mgolcljohhgjgmkhecigfikkjanopneb';
+$searchExtensionID = 'lggggiafifjohfbpmpdhacfmglilipnc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
