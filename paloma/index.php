dd05ede0da4b52281147b32b75239d5fa13f095f
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-13 15:14:04 -0400

    Update index.php

--- paloma/index.php
+++ paloma/index.php
@@ -2,7 +2,7 @@
 $title = 'Paloma Mami';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'igkaccibambnmnagknhodljnabncpkmb';
-$searchExtensionID = '';
+$searchExtensionID = 'cnlmlpfepncbbiahbkilagbmnjmgahik';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
