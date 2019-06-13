0e79a0daf61750dd55c329dcbf599b714400da2d
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-13 15:30:10 -0400

    Update index.php

--- toonblast/index.php
+++ toonblast/index.php
@@ -2,6 +2,7 @@
 $title = 'Toon Blast';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ecnmpjjcneamappgnkljnbmneloapphe';
+$searchExtensionID = 'okpgkfpnpepfelckojboopcfkpgfaglk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
