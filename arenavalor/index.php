623654157234c0dcca8b3262f1c5064c00a6a801
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-13 09:35:21 -0400

    Update index.php

--- arenavalor/index.php
+++ arenavalor/index.php
@@ -2,6 +2,7 @@
 $title = 'Arena of Valor';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'epfjepaikailajdikdmdcibobbmpiphp';
+$searchExtensionID = 'nepcfbkoafocpnblgaoodinogcpgiffh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
