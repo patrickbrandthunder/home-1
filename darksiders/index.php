6c2fe9a2d0bc43fca48b658529c441621edd9c15
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-14 08:27:34 -0400

    Update index.php

--- darksiders/index.php
+++ darksiders/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Darksiders Genesis';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kedmjaiadjecbegjifcnfnbbhoihgigl';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
