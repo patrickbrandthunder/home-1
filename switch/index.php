09c9cfe321963105676d0e46ca7ee59488b88563
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-14 10:08:49 -0400

    Update index.php

--- switch/index.php
+++ switch/index.php
@@ -3,6 +3,7 @@ $title = 'Nintendo Switch';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'kpalangohmfcmlaajlpkobcmdccfdadf';
 $searchExtensionID = 'obpnnmicdpchogpllimbnokhocldkdbo';
+$testSearch2  = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
