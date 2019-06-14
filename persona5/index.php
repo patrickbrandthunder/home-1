20e64cc1fe42caa65974c852330ea560fe6ab8d6
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-14 07:32:38 -0400

    Update index.php

--- persona5/index.php
+++ persona5/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Persona 5 Royal';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kofgpbemohkhegmkpakdokjpkmkjdlej';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
