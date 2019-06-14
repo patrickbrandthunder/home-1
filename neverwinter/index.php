fa4294a498119aa938f3b08d45c50b31c7a1f936
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-14 07:00:12 -0400

    Update index.php

--- neverwinter/index.php
+++ neverwinter/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Neverwinter Nights';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mmolheomaolhcjhoaliokaljcodgdkdg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
