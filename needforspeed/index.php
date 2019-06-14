a6c2c6fea292c01c3882a33dbf7cb8286e0cb7b2
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-14 10:31:02 -0400

    Update index.php

--- needforspeed/index.php
+++ needforspeed/index.php
@@ -2,6 +2,7 @@
 $title = 'Need For Speed No Limits';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'dfjogjepkobhlcfnpaflakcdjonoachh';
+$searchExtensionID = 'oidmknnphmpkomaldfpajcghdgbmnaao';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
