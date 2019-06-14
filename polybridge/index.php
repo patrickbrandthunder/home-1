72a170b90cf50f1cb124bcdcdfeb6ad59b176d6c
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-14 11:23:04 -0400

    Update index.php

--- polybridge/index.php
+++ polybridge/index.php
@@ -2,6 +2,7 @@
 $title = 'Poly Bridge';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'hneknoakmihcjpfehdhobgnoochdngfl';
+$searchExtensionID = 'nljipekhlddmfhbckkfabhfalpkekncj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
