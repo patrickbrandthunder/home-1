4d3331b2d88bf58a06a6e447014f867a41e25abd
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-18 19:58:12 -0400

    Update index.php

--- eldenring/index.php
+++ eldenring/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Elden Ring';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'codckamlkafohldgpgcaihjfknnlempa';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
