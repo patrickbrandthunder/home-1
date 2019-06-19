f9277a3566e82e4b7b755b0e0a32c54dbdb8f9b4
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-19 10:04:36 -0400

    Update index.php

--- harvestmoon/index.php
+++ harvestmoon/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Harvest Moon Mad Dash';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fogaghadbkohfihplmjpibdfnmplkhlh';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
