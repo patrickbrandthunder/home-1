d828045d0b1067c2865fa4a8ddb8a460aa0c79ee
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-19 09:39:21 -0400

    Update index.php

--- godzillad/index.php
+++ godzillad/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Godzilla Defense Force';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mnglgoeioicoibmpdmfddjnipipomclo';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
