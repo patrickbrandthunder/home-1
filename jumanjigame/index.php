b637d45385bc4779bd9a192f2cde69fde771e30d
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-19 10:15:26 -0400

    Update index.php

--- jumanjigame/index.php
+++ jumanjigame/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Jumanji Video Game';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lbbabmegmcebanijnhjoplmhneaijeip';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
