34839992d48603f125633b247197bdbe5cbb358f
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-20 16:16:15 -0400

    Update index.php

--- avamax/index.php
+++ avamax/index.php
@@ -2,6 +2,7 @@
 $title = 'Ava Max';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'klmmbhdfdiflnnnfkhbhmjiiakliflog';
+$searchExtensionID = 'okbpbgeakbobpbeaedhchicjildkbodl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
