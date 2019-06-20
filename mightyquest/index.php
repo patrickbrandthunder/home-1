266a4da89c0c982bbed82bcf10f55e70b53af538
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-20 12:58:10 -0400

    Update index.php

--- mightyquest/index.php
+++ mightyquest/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mighty Quest for Epic Loot';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'meefjmdknhgaihncfiffahdcbifoabnb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
