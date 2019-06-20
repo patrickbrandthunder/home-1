53c19442a4d995d25a54abaeede5115608c862cf
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-20 16:37:00 -0400

    Update index.php

--- explodingkittens/index.php
+++ explodingkittens/index.php
@@ -2,6 +2,7 @@
 $title = 'Exploding Kittens';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'icopphiklneamopkjplleeejmmgijgil';
+$searchExtensionID = 'bdpphlepcoajahkdjjbjmfjagohcmmoi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
