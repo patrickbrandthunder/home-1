e724dcc5e5ed1a3cc9dc3a9109a79c3136533097
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-20 16:21:28 -0400

    Update index.php

--- blueberries/index.php
+++ blueberries/index.php
@@ -2,6 +2,7 @@
 $title = 'Blueberries';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'eekmngmichckpjfcnpfafjpdnbpfnlci';
+$searchExtensionID = 'fbklfbgahoilhknenppjdpbcnadgnfoh';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
