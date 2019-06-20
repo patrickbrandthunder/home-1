99db794f0f2c238cf29699ede621c528a9c4fb12
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-20 10:06:38 -0400

    Update index.php

--- panzer/index.php
+++ panzer/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Panzer Dragoon';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'npkfmpkkolgjmndinanckgmejhklgcpm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
