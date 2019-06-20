92d4f7c3cec2f349c8a55d0ecd51dbc835c5ec60
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-20 09:57:26 -0400

    Update index.php

--- planetzoo/index.php
+++ planetzoo/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Planet Zoo';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fhgmnhmpfonfmebkjmigfgbjaagefmod';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
