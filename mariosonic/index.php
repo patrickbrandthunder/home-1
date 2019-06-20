4910fbc146ea5637e712c34510236e0c24da4128
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-20 13:05:40 -0400

    Update index.php

--- mariosonic/index.php
+++ mariosonic/index.php
@@ -1,6 +1,6 @@
 $title = 'Mario Sonic Olympic Games 2020';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'clopiemfefgpipahkacbfmciklkmojpl';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
