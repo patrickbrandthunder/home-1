281854a48e5b9158efbcf93c4287f35f46d38bbc
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-20 09:38:43 -0400

    Update index.php

--- titanquest/index.php
+++ titanquest/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Titan Quest';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'eaaieijneldofmlmdmaakmabfjoboacp';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
