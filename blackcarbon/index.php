9bc8c5d820c7857247bbf7592073a2b194ef916b
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-21 08:49:19 -0400

    Update index.php

--- blackcarbon/index.php
+++ blackcarbon/index.php
@@ -2,6 +2,7 @@
 $title = 'Black Carbon';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'efenboghaikiiakmoagofjnhdlgknmcb';
+$searchExtensionID = 'fdmjgkddefbpimccfkbgokbocjlochea';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
