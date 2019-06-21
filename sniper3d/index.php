ad03010db41b8e020ac352f5cfa9021e1110d6ac
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-21 09:13:18 -0400

    Update index.php

--- sniper3d/index.php
+++ sniper3d/index.php
@@ -2,6 +2,7 @@
 $title = 'Sniper 3D Assassin';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'lbgmfpkcfcjjgbfpilgdmmooligfopim';
+$searchExtensionID = 'nocbamplnfilbandgbpajaalcppdjmbp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
