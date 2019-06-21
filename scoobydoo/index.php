fbcd9804bf939ebf910d06a87445ce85acd54851
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-21 10:31:22 -0400

    Update index.php

--- scoobydoo/index.php
+++ scoobydoo/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Scooby Doo and Guess Who?';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jkccllgcjffpnedpmogmfkldjhheaeln';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
