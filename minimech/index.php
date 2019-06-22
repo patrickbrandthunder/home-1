e85fb326d7fe82ed845345dc50dd87c06e67c5a8
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-22 09:45:24 -0400

    Update index.php

--- minimech/index.php
+++ minimech/index.php
@@ -1,7 +1,7 @@
 <?php
-$title = 'MIni Mech Mayhem';
+$title = 'Mini Mech Mayhem';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ickbfgdogdgfogpcagchbpjbphjgannn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
