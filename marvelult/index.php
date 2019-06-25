c666175c456621b16c05511f98971c596b07833b
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-25 11:40:58 -0400

    Update index.php

--- marvelult/index.php
+++ marvelult/index.php
@@ -1,8 +1,8 @@
 
 <?php
-$title = 'Marvel's Ultimate Alliance 3';
+$title = 'Marvels Ultimate Alliance 3';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'damiobkmpeilhmbhnpkmnmbojjpffioc';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
