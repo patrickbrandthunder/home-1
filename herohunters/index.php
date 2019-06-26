7683372c8671e7b60b32a61f9e438ff630812e11
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-26 09:04:32 -0400

    Update index.php

--- herohunters/index.php
+++ herohunters/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Hero Hunters';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dddkoodkibhdcbmopbndpocondilffcf';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
