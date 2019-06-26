912d20ff9080fa0e97d56db9858e0e9eef364e0f
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-26 08:29:25 -0400

    Update index.php

--- pixelstarships/index.php
+++ pixelstarships/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Pixel Starships';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'memkaabkojapkaacpeaiedomiclmpipn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
