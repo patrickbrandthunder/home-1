0792dae3a5ef3e76d366baa42ec07035abb7628f
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-26 08:52:05 -0400

    Update index.php

--- warpshift/index.php
+++ warpshift/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Warp Shift';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lagbpbdhcjpleiapieplbjdhdgleoeoe';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
