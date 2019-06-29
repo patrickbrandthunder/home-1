33a3c06dc9874d96b6bcf38e180d086725969d55
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-29 09:41:37 -0400

    Update index.php

--- mariokarthot/index.php
+++ mariokarthot/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mario Kart Hot Wheels';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bgahajeapcbhjagigmfflcginnihdeba';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
