a48242de186f6c67257ef0fbbba1d08c062669d2
pjmurphy777 <pjmurphy77@gmail.com>
2019-06-29 09:34:18 -0400

    Update index.php

--- 8ballhero/index.php
+++ 8ballhero/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = '8 BALL HERO';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ahpfimpejkabpbidmknamcofhahoinme';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
