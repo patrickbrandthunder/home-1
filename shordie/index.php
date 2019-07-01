77192e3e998ffd17af728ad7a2b8d656a6d242bb
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-01 09:39:32 -0400

    Update index.php

--- shordie/index.php
+++ shordie/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Shordie Shordie';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fkkfpmkgdneiegegajdjlnepicgfainj';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
