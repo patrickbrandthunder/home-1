9fd12b3bed3d9fac9fc1397f79e244488c750dd9
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-01 15:28:23 -0400

    Update index.php

--- buble/index.php
+++ buble/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Michael Buble';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'chelgafhkejmlpceoaolomjmohjikokn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
