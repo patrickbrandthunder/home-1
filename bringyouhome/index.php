772aee808191e1ba138ba99498d40bafd624c62e
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-01 07:35:01 -0400

    Update index.php

--- bringyouhome/index.php
+++ bringyouhome/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bring You Home';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'egobepdkebadgbaagpncnbmipljpaiea';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
