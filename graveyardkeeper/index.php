61a45bc4e1aac45d66b8127c21a48777611cd874
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-02 09:37:35 -0400

    Update index.php

--- graveyardkeeper/index.php
+++ graveyardkeeper/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Graveyaerd Keeper';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fmciognbakhiliiinjdfjifepfghnfhd';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
