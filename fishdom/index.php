6055b2c6ef2832ed2cfeb4b1dcba388497e91eab
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-02 08:16:43 -0400

    Update index.php

--- fishdom/index.php
+++ fishdom/index.php
@@ -2,6 +2,7 @@
 $title = 'Fishdom';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ngppbnplgeppdceolphnmlamdkalfjgc';
+$searchExtensionID = 'ocabnelhopfjhijlgigpgkjbmmacnjge';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
