4e8bc2f49484de1450d0991a6c4b7c24ae3653da
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-02 09:16:53 -0400

    Update index.php

--- nba2k20/index.php
+++ nba2k20/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'NBA2K20';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jfpjankondholgfbifpplcdggbhpmphf';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
