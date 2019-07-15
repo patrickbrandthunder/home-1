e8b1afe4fad8f3f690af04c3b3beadb48a57c45b
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-15 14:12:37 -0400

    Update index.php

--- evillands2/index.php
+++ evillands2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Evil Lands 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lfnkkokdapnacmgefkpofhmnjdbhgnak';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
