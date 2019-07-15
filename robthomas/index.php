8a860892f0f974fae33f2a14508e00e19cdec87c
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-15 09:08:23 -0400

    Update index.php

--- robthomas/index.php
+++ robthomas/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Rob Thomas';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cknddiigllcifallgopjkomiokcpmboe';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
