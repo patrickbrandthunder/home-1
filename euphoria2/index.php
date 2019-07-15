224420aaddb7bfce3b3a8de0075659221a9a50a6
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-15 09:16:30 -0400

    Update index.php

--- euphoria2/index.php
+++ euphoria2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Euphoria 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'aebiakamckdgjifjmhpknokbjehbohbb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
