c0408c500d19c62b2e2d2a4432302091c2a73165
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-15 10:11:01 -0400

    Update index.php

--- koepka/index.php
+++ koepka/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Brooks Koepka';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gifkcfonokhdipcgpflmmedaphjdgaii';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
