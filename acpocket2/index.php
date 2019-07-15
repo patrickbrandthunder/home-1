788359f0ba767533b5667bbce7f0a6404eb3aeab
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-15 09:00:27 -0400

    Update index.php

--- acpocket2/index.php
+++ acpocket2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Animal Crossing Pocket Camp';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ghajhfhpbfdfgoclfofhnabhdcbjmbhi';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
