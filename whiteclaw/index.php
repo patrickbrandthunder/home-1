9e3954d685c30a15f4abb1004ccc5ab4d4e75d0d
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-15 10:02:33 -0400

    Update index.php

--- whiteclaw/index.php
+++ whiteclaw/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'White Claw';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'adagdbjaflphchmknikbecpllinbapcg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
