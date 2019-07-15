4564520721fcc0aee82b34c74d5bb20aabb52902
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-15 09:33:24 -0400

    Update index.php

--- chrisd/index.php
+++ chrisd/index.php
@@ -1,7 +1,7 @@
 <?php
-$title = 'Chirs Delia';
+$title = 'Chris Delia';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'flpbifmhncjaiipmnogfkmnkcefagdpp';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
