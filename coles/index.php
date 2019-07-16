27aa0fe8dbcd8082cf968b13557fbc7d69d36b23
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-16 08:54:23 -0400

    Update index.php

--- coles/index.php
+++ coles/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Cole Swindell';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fgipekhbhkpapafnhodmhgjdacchoidf';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
