379524f9d0990a2074f775b58c701df5fd7c6656
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-16 08:38:37 -0400

    Update index.php

--- thirdeye/index.php
+++ thirdeye/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Third Eye Blind';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'meppokkgffjmjcekkkkeoomgmnbomcha';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
