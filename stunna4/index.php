94bfa0b4d43e39acafe7de1bb22713eada753a29
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-16 09:15:48 -0400

    Update index.php

--- stunna4/index.php
+++ stunna4/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Stunna 4 Vegas';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'llnpelgemmdmamjgophegmdoadkpkclo';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
