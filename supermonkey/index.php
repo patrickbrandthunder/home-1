93c675f84efda13cb586fb80a246232e874f28d3
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-16 08:29:59 -0400

    Update index.php

--- supermonkey/index.php
+++ supermonkey/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Super Monkey Ball';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fnefbgciojbcnhfgmhbfncicepgclaag';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
