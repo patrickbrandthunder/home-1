84f413ddd12b35e8ea508a65ccd7797cbf841a4d
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-16 16:57:04 -0400

    Update index.php

--- returner77/index.php
+++ returner77/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Returner 77';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dpmfgelodbgbhkfaamiingccmlmaihfh';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
