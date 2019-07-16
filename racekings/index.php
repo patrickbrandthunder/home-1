2fbb29dafc971d2f9cddfc56e78f7d6002aa82a0
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-16 09:41:36 -0400

    Update index.php

--- racekings/index.php
+++ racekings/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Race Kings';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kggiiaghmpllkpgncbcoegljbefgpjcd';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
