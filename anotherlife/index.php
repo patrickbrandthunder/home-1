ab452d0c4af81b62f95d64054de68dc796f3cc44
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-17 10:15:03 -0400

    Update index.php

--- anotherlife/index.php
+++ anotherlife/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Another Life';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'doncfpcaialjlfknlmidmmcaecigklao';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
