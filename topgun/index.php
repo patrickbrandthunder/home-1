1f044abf204e74a1b4853e788b14f4d757e1c271
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-22 19:22:28 -0400

    Create index.php

---
+++ topgun/index.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'Top Gun';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
