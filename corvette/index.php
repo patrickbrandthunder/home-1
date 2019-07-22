b1d9679236d59b7fe9a43d06b4fd81988de496b6
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-22 19:10:12 -0400

    Update index.php

--- corvette/index.php
+++ corvette/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = '2020 Corvette C8';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'oadghcohkmefdacmjocmicckpdlhafoc';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
