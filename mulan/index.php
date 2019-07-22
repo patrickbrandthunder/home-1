0f121808495191e16dcc3b9aaebf0aaf60e01920
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-22 15:07:53 -0400

    Update index.php

--- mulan/index.php
+++ mulan/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mulan';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kkbbbfpbdggpmjcpchiilkkbkmaagddl';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
