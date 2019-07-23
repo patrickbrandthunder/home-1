03b1c81decef74a1cae0b86e00de6d0a4f90e7cf
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-23 09:56:59 -0400

    Update index.php

--- skrillex/index.php
+++ skrillex/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Skrillex';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gmekeeeobgagnehkffgkhmlocjmnpiok';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
