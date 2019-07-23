71bc88e8b99961424f5aad9a0dc37a7c17e2ae7a
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-23 12:43:19 -0400

    Update index.php

--- bombastic/index.php
+++ bombastic/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bombastic Brothers';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mfkdbaoakcjjjbgokoadgjjdejpolmgb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
