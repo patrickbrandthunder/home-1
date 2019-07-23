d8a603c97cf0fa8207fbe8947a75224c72855067
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-23 11:40:18 -0400

    Update index.php

--- sinsraid/index.php
+++ sinsraid/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Sins Raid';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ahmmebdlbpllhaacccahejmhapenpcci';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
