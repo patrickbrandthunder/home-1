d949997abca1979699f3b82f482f5d8dd2df2e61
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-23 10:21:52 -0400

    Update index.php

--- kingprincess/index.php
+++ kingprincess/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'King Princess';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lpbnieoodikeljjkkdefkjcpeloehhbi';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
