cfe85637805c46602cacd5421aef8a858ce4ee8b
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-23 09:43:53 -0400

    Update index.php

--- streetkart2/index.php
+++ streetkart2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Street Kart Racing';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dijjdncdaildfndkofjfmaioclnicgpk';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
