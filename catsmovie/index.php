0f1a3319f9066512e3224f5c75d81479377b2334
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-23 09:26:14 -0400

    Update index.php

--- catsmovie/index.php
+++ catsmovie/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'CATS Movie';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'midapihnkaclbdgkgiohcbnnccfpelkg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
