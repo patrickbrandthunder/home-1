c9965a31d8513bfea5b3a840a7faf3b3f1a56f51
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-23 17:51:20 -0400

    Update index.php

--- dungeonbreak2/index.php
+++ dungeonbreak2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dungeon Break 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kllmdcokdcldleinbkeadkpooklmhbhg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
