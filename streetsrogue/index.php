cee3b700d5167d42510057b3c87fc5d7d4d61b6f
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-24 15:18:29 -0400

    Update index.php

--- streetsrogue/index.php
+++ streetsrogue/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Streets of Rogue';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'emkoagikpdcofnbhkmochkjjjpfkfhlp';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
