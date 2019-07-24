ce5c728252f57fbe81c3f425ef42ff31abebb279
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-24 15:38:23 -0400

    Create index.php

---
+++ beyondwall/index.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'GOT Beyond The Wall';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
