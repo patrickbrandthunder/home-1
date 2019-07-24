cf6846726626a260c78304a83a149a388974a83b
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-24 12:08:22 -0400

    Update index.php

--- animalcove/index.php
+++ animalcove/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Animal Cove';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pmcgffdigigdaenmicaeobapelgkbaja';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
