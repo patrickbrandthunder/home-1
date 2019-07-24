ed0afba9de089baec2bcae337d5b97c546dd3f0d
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-23 21:51:57 -0400

    Update index.php

--- letsbe/index.php
+++ letsbe/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Lets Be Heroes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jalpjgnhmbjkoakldccoklaabeecggod';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
