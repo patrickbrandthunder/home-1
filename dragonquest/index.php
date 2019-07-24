b10defca5a5c25c3e064baf0844989ffd2f614db
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-24 16:48:54 -0400

    Create index.php

---
+++ dragonquest/index.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'Dragon Quest Builders 2 ';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
