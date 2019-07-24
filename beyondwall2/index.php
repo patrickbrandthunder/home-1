1a65a20f6d961c149fc8e372bfdbad6876b22bb7
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-24 15:48:15 -0400

    Update index.php

--- beyondwall2/index.php
+++ beyondwall2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'GOT BEYOND THE WALL';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bfnkacgbdjmbfnkjkpgjkpggbkpopdcn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
