b7b73c81c0657b75acd9af5c45b208d7b87a6902
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-23 21:41:21 -0400

    Update index.php

--- chipmunks/index.php
+++ chipmunks/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Chipmunks';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pojgdjiompdlflckijgamegpmhdhgnad';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
