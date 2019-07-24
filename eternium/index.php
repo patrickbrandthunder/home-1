3908f72eb9415c20c22f8ef40e2e02e98bdd1285
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-24 13:33:08 -0400

    Update index.php

--- eternium/index.php
+++ eternium/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Eternium';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mhojoaocgdodeejajfhdblkoaahcgidm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
