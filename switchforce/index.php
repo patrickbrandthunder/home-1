f6455821a885452ab061053e2fe0ea9d1f34b3d6
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-24 13:44:50 -0400

    Update index.php

--- switchforce/index.php
+++ switchforce/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mighty Switch Force';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fbbbjoeapemapmlhbjbkcefhnihgoook';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
