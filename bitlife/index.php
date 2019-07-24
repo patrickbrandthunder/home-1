a725e0765261b985f475cb59b24b4ce724510e4e
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-24 13:00:43 -0400

    Update index.php

--- bitlife/index.php
+++ bitlife/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'BitLife';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ghkenngahhmhiankhinhppeonoaifoli';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
