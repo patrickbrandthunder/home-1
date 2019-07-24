8989f7814eaacc34878bca7cd1e41017ebacafe1
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-24 16:53:37 -0400

    Update index.php

--- dragonquest/index.php
+++ dragonquest/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dragon Quest Builders 2 ';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'idafjpjdneammfmmgcpgboejiaehdaem';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
