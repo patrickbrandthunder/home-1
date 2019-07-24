e8ae9027826fff9f5b172aea3246ad0f331582b7
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-24 14:43:27 -0400

    Update index.php

--- nba2kplayground/index.php
+++ nba2kplayground/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'NBA 2K PLAYGROUNDS';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jehjbbkhkadepappkidndmjgnehkhjkk';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
