6328a8d4252e49f7a9714b2e74ddb0683709b1a4
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-24 15:32:23 -0400

    Update index.php

--- spies/index.php
+++ spies/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Spies in Disguise';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kpdcbchepomiljkpjnbgcdhiafcpalcl';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
