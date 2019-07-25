f603e81c664c5a67c17016023766307a4a078fe7
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-25 09:20:27 -0400

    Update index.php

--- sayonara/index.php
+++ sayonara/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Sayonara Wild Hearts';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jeglhmpmmbminllfjedoooahkopaeilk';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
