80ce5f97d4819458a904ef7226ebe92e65276fc4
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-30 12:22:19 -0400

    Update index.php

--- darkcrystal/index.php
+++ darkcrystal/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dark Crystal: Age of Resistance';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ompbnfmdbpmndgipbgjpnjigdgcmemaj';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
