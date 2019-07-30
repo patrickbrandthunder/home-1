5762cf800656335f4be7854104a65297cc641867
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-30 10:30:07 -0400

    Update index.php

--- theboys/index.php
+++ theboys/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'The Boys';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'akbphndgecnhekejcjjgpenoopnlfppn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
