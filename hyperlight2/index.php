69e1cc1a14a631a837a9d436832c93b2c0f69d0d
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-30 15:06:44 -0400

    Update index.php

--- hyperlight2/index.php
+++ hyperlight2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Hyper Light Drifter';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'iibeglfeihikpipmkndmbnhidgddnhjg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
