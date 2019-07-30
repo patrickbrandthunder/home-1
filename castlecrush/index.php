7d653ba359dba045f1407542ae068180ee64abcf
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-30 12:31:43 -0400

    Update index.php

--- castlecrush/index.php
+++ castlecrush/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Castle Crush';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kgmlimadamaddjgolcognmpnmnocfick';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
