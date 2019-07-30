216071ce4ae04684e4416d528f8e721dd14727bd
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-30 13:17:50 -0400

    Update index.php

--- projectzero/index.php
+++ projectzero/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Project Zero Deaths';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bihbaellbgicegkcohkdklgjijacpleg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
