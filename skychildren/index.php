3db905768bf0616b1e4da0b719def5845785415e
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-30 13:37:42 -0400

    Update index.php

--- skychildren/index.php
+++ skychildren/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Sky: Children of the Light';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gkbmpjhnfnjgdipmjencejikcaekdmnn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
