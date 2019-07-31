ff1b3a36862d0b2010b2b3608e85d22c71a1c482
pjmurphy777 <pjmurphy77@gmail.com>
2019-07-31 10:55:59 -0400

    Update index.php

--- bugha/index.php
+++ bugha/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bugha Fortnite';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bieojaoopgikpdbohakkjejkichmhabm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
