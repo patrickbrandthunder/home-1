1f6d432e7f35418473fc8459ff33166779c36f74
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-05 08:58:33 -0400

    Update index.php

--- astrobears2/index.php
+++ astrobears2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Astro Bears';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ggoggagdiffbkopaodmmbbkiblcjgjof';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
