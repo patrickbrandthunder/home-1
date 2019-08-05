bb1e7058ddaf2533d1394d2e48ececa07b20a6e9
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-05 11:35:15 -0400

    Update index.php

--- sweetescapes2/index.php
+++ sweetescapes2/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Sweet Escapes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mfnbgjnddacbloejdcjpkfbpcjfjbpcb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
