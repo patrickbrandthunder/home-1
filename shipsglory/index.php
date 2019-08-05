4fc62966af1dfe1fb70104dfa36d144bee405437
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-05 15:30:05 -0400

    Update index.php

--- shipsglory/index.php
+++ shipsglory/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Ships of Glory';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'iabohddncfaihinepielkkhgnbmcibnb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
