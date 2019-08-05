a91d08e64352bfb62d096cc5d245eb63dd9b2534
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-05 10:45:13 -0400

    Update index.php

--- fgmobile/index.php
+++ fgmobile/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Family Guy Freakin Mobile';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'llijlbholddcnaemaelggninokkfpela';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
