ccb96d89113814d21d26117a4d99dee583a88bc8
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-05 16:30:01 -0400

    Update index.php

--- idletheme/index.php
+++ idletheme/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Idle Theme Park';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lmdmhpjmmcdaobmnichphooihdibioke';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
