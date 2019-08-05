a5927c3005989510c2a62f8a3cb3058fce187360
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-05 09:47:05 -0400

    Update index.php

--- escapelogan/index.php
+++ escapelogan/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Escape Logan Estate';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'apjkbahekgeijpdblifhdbabghfnbhcg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
