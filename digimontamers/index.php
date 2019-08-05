b9828ab13b16764013f98604169a70c9456dfffa
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-05 09:07:17 -0400

    Update index.php

--- digimontamers/index.php
+++ digimontamers/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Digimon Tamers';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'oflahaphlhjkaajdoobjllgkegkgbfap';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
