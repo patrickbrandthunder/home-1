d56438858a32c2e83a6448df29851c56ca65ebb3
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-05 11:15:10 -0400

    Update index.php

--- forgeempires/index.php
+++ forgeempires/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Forge of Empires';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ikgckgmffmbgpabeldjdeidppkebljko';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
