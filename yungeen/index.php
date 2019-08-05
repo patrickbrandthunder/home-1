72911d0e9a991763eafac4f5a76d7c82d423fa3b
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-05 13:26:32 -0400

    Update index.php

--- yungeen/index.php
+++ yungeen/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Yungeen Ace';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = 'iakafcahihdkhipmpeodjigmahpcibgl';
+$extensionID = 'pppdkpmainjgddmbplagdjlhchcgkjcn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
