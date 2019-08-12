91c2d5c50c11d8c93b77d7500df11be109ec9db2
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-12 10:52:49 -0400

    Update index.php

--- 100deek/index.php
+++ 100deek/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = '100Deek';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jcejlmlkgpfmmcicgcggjfebkemphkjf';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
