52bd794a24d169f02e32ca61a5459c6b063014e1
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-12 11:05:50 -0400

    Update index.php

--- brawlhalla2/index.php
+++ brawlhalla2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Brawlhalla';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ffnflimhcjadmfkhghjmihjeemdehpoi';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
