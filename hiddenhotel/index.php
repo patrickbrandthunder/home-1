949edc00fefe5bae9cff25e14fb56554beea490f
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-12 13:39:13 -0400

    Update index.php

--- hiddenhotel/index.php
+++ hiddenhotel/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Hidden Hotel';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'conmcbepccjddfchebphlgbafbgnpgff';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
