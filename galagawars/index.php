fc7c9b1489d68d63efdc1e04a1ebc4cd958f84f4
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-12 12:47:49 -0400

    Update index.php

--- galagawars/index.php
+++ galagawars/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Galaga Wars';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'oocgjbocjajjlffpbohpgcikgfpkkhob';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
