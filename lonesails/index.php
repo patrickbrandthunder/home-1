f8e39dbd8fac14b16b9e1386e0bfd3c610fd564c
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-12 12:41:43 -0400

    Update index.php

--- lonesails/index.php
+++ lonesails/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'FAR: Lone Sails';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'efhhnoelemdimbnmlcganbjfedooclng';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
