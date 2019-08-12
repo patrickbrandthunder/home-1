69997d139f0ee098d254e95a17af270b87acb55e
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-12 15:08:58 -0400

    Update index.php

--- gotc2/index.php
+++ gotc2/index.php
@@ -1,7 +1,7 @@
 <?php
-$title = 'Game Of Thromes Conquest';
+$title = 'Game Of Thrones Conquest';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'enmnffajgfcgfmfjbomepcakpiphicjk';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
