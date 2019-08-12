584f4519aa98d1b31a44c5226373f207d21f80a2
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-12 11:26:33 -0400

    Update index.php

--- mariotennis2/index.php
+++ mariotennis2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mario Tennis Aces';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jhmbeaphhndepkieijenikddjaadgbel';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
