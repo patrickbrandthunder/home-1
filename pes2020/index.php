2242a52cfbaaeee20ee7494500b523dfc4b9aa42
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-13 09:59:42 -0400

    Update index.php

--- pes2020/index.php
+++ pes2020/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'PES 2020';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gfankcfaonocekaopjpphihohelglafd';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
