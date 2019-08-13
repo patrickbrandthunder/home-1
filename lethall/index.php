cc1a337358b5e177a19ae68ec94e173335b5c1a8
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-13 16:16:08 -0400

    Update index.php

--- lethall/index.php
+++ lethall/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Lethal League Blaze';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cjodpeedojgoknedaopbkedgaflojign';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
