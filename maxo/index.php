79479d5578a4ff6494a0c5bb0c50b20161170980
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-13 14:25:46 -0400

    Update index.php

--- maxo/index.php
+++ maxo/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Maxo Kream';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bnjkldmihmajomjfogfehhimkhpjhfdk';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
