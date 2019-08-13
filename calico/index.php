e38ae5b04c34bbb42444128b4a5c46fd52b583fd
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-12 20:31:17 -0400

    Update index.php

--- calico/index.php
+++ calico/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Calico Cats';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'apdifkhpkbjnmelieimenbapeilmajhf';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
