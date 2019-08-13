44f2fb89e6b0cc48b0b8a9e28dc25013cf43c749
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-13 10:19:38 -0400

    Update index.php

--- riftbreaker/index.php
+++ riftbreaker/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Riftbreaker';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mebeceaemgppkgecgkojniahdaolpgon';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
