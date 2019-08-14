29201de5cae8d396f0f3fb8a0d279e6eee9a85d7
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-14 16:19:46 -0400

    Update index.php

--- jackpotparty/index.php
+++ jackpotparty/index.php
@@ -2,7 +2,7 @@
 $title = 'Jackpot Party Casino';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'bodehjiohndicooanbhamjllgligcpbp';
-$searchExtensionID = 'ocodephalioefikfhiecloebjjlbmlep';
+$searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
