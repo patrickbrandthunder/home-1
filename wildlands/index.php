42da38fbba53640d68b273cc23feb30165ab5495
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-14 16:34:19 -0400

    Update index.php

--- wildlands/index.php
+++ wildlands/index.php
@@ -2,7 +2,7 @@
 $title = 'Durango Wild Lands';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'eobogphelalkideeimnnadjfngjllemg';
-$searchExtensionID = 'lffjnheldpnbejekcncbcncknoegabdp';
+$searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
