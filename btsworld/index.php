7be508856fd39b9c32e38199a9bb6c36582973e0
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-14 15:59:12 -0400

    Update index.php

--- btsworld/index.php
+++ btsworld/index.php
@@ -2,7 +2,7 @@
 $title = 'BTS World';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'pfagaoijmjimchkjponhpopcppmjepkn';
-$searchExtensionID = 'bakidgjfgimimhghnpknonfleeddanlo';
+$searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
