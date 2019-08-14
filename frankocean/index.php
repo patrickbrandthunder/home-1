a45bb8326f509f0229f3b99d66bc8124cebd37fc
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-14 16:20:19 -0400

    Update index.php

--- frankocean/index.php
+++ frankocean/index.php
@@ -2,7 +2,7 @@
 $title = 'Frank Ocean';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'kiclfllpgchnnlebhphfbejhljeoagki';
-$searchExtensionID = 'aedhjgclffcmcddfjagpagnhfobdkabn';
+$searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
