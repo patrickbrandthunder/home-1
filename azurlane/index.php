ce027899901d215f451a71dd9d6a82be27cb5b8b
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-14 16:16:09 -0400

    Update index.php

--- azurlane/index.php
+++ azurlane/index.php
@@ -2,7 +2,7 @@
 $title = 'Azur Lane';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'kghnnellonncggaieiojdnmmoiapmlpd';
-$searchExtensionID = '';
+$searchExtensionID = 'fpahbdcpokfnjefbijappppeplnajble';
 $testSearch2  = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
