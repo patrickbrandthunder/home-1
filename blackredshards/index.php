484194d54fd2aa58a5eaf7e6fcdadb0151299703
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-14 13:38:21 -0400

    Update index.php

--- blackredshards/index.php
+++ blackredshards/index.php
@@ -2,7 +2,7 @@
 $title = 'Black Red Shards';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ccjkffjimbodkehklomfmdmihfncfeig';
-$searchExtensionID = 'iffgkpocinhelicngpmjgdchghacpfln';
+$searchExtensionID = 'gnknfmabohlenbkaoddbmbgojenempbb';
 $testSearch2  = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
