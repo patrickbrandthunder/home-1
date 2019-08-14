8af613a6287d029e4800da04b2b6fc1d60f5d227
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-14 16:01:01 -0400

    Update index.php

--- talkingtom/index.php
+++ talkingtom/index.php
@@ -2,7 +2,7 @@
 $title = 'My Talking Tom 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ekoggdebjopmneknkdmkmonooogabdig';
-$searchExtensionID = 'lhcaggpeamlcbdoebnnanmogkjndhpbc';
+$searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
