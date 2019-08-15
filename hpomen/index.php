c912bd3b87d03ce158c0212b5741fef427d2f93c
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-15 09:22:14 -0400

    Update index.php

--- hpomen/index.php
+++ hpomen/index.php
@@ -2,7 +2,7 @@
 $title = 'HP Omen';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'mcnodldlgohncdcgfgohdncmekdppaba';
-$searchExtensionID = 'hplljdmfflicocmmfinokobgpbilnelc';
+$searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
