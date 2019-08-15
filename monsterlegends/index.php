fdf07a30ad610d894d8c38b544c8e318898e9e4b
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-15 09:21:18 -0400

    Update index.php

--- monsterlegends/index.php
+++ monsterlegends/index.php
@@ -2,7 +2,7 @@
 $title = 'Monster Legends';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'mnelomcjjafkegoklmgfjjcihlhhbfbk';
-$searchExtensionID = 'ghmaonjmlbiblegifkhlbippglakmalg';
+$searchExtensionID = 'paakjlmamkejplmojlonlpkbchdoijom';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
