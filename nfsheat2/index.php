353da8ad7225ff019c3d3a52764711bdc9f6e018
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-15 09:13:55 -0400

    Update index.php

--- nfsheat2/index.php
+++ nfsheat2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Need For Speed Heat';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'koboddnpmecmdidlpffaapaaddoiokep';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
