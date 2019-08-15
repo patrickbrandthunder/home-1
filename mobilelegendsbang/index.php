d0d7d4ab2ba393c7f4aa51beb5fbf5a71cd2927f
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-15 10:06:34 -0400

    Update index.php

--- mobilelegendsbang/index.php
+++ mobilelegendsbang/index.php
@@ -2,7 +2,7 @@
 $title = 'Mobile Legends Bang Bang';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'bedgncfnlbofgkhfjgehebnmemkgjogn';
-$searchExtensionID = 'gpojlbncijcimmijphfgihkiondkopjc';
+$searchExtensionID = 'enpollehkdhkblnefbdebiegnljoaike';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
