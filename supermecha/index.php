a0bc04c630ae0c14bb204fbba4d715fbdc56f6a1
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-16 15:41:27 -0400

    Update index.php

--- supermecha/index.php
+++ supermecha/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Super Mecha Champions';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ahnagfpaobdegfinogmcbgdpjjnibpeh';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
