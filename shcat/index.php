cc83b58b1880d4cc02048a1805d01fe8a61cc49b
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-16 09:59:19 -0400

    Update index.php

--- shcat/index.php
+++ shcat/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'American Shorthair Cats';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kmhpbcggjlgegpfpbnfdbhnghchpijbi';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
