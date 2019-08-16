ab05757111b4235df8e193edd7a7f798a8f087b3
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-16 10:29:22 -0400

    Update index.php

--- trailboss/index.php
+++ trailboss/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'TrailBoss BMX';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jfpahmkdamejnabpeglhpabjbboiolhi';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
