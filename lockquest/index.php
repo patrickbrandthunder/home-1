9c39533040aa349f463082b2b2019d415048ef8a
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-17 15:48:33 -0400

    Update index.php

--- lockquest/index.php
+++ lockquest/index.php
@@ -1,7 +1,7 @@
 <?php
-$title = 'Lock's Quest';
+$title = 'Locks Quest';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'epacjaonpibcfpeoplhkpjajnmjgpppb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
