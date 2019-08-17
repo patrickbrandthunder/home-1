ec4324c316e2e61f0a22cd7a9570e2f766e57b5d
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-17 12:50:54 -0400

    Update index.php

--- zerocity/index.php
+++ zerocity/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Zero City: Zombie Shelter Survival';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jhcgafijneoaejaobeibbmkbfhdjgeap';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
