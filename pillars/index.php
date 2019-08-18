1a7af248aa202d24caafc48aa9ad93e2283b05d3
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-18 09:27:17 -0400

    Update index.php

--- pillars/index.php
+++ pillars/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Pillars of Eternity Complete Edition';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bjddpekjjhpmkpkbgcpmapplldajnine';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
