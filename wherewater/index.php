f4cee999506096387ae0e719e00a602a36e368a5
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-18 10:11:22 -0400

    Update index.php

--- wherewater/index.php
+++ wherewater/index.php
@@ -1,7 +1,7 @@
 <?php
-$title = 'Where's My Water';
+$title = 'Wheres My Water';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bdabfopglekefkhjkfgedhjggkicdmfe';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
