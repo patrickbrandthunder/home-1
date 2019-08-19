40bbacc5942bffb638bcecfb5915424fc5959104
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-19 16:39:17 -0400

    Update index.php

--- blazblue2/index.php
+++ blazblue2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'BlazBlue';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bahefhipeolppckaifjdeoefhlocbcpi';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
