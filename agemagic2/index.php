1b8b83be41d789bc022eebcbedfd4202c25d1406
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-19 10:46:38 -0400

    Update index.php

--- agemagic2/index.php
+++ agemagic2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Age of Magic';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bfjdabmbnamekjgnckjiabgjnnikfdmk';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
