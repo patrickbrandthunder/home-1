45f88432b2dd3f3e9137219c30dcc06a4658c127
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-19 16:52:11 -0400

    Update index.php

--- evertale/index.php
+++ evertale/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Evertale';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'molpnmemaknoofjjjgcndombieegggpj';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
