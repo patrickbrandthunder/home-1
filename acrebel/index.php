b142332d8951d0cdae67818f060b4050772b2bf7
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-19 16:11:49 -0400

    Update index.php

--- acrebel/index.php
+++ acrebel/index.php
@@ -1,7 +1,7 @@
 <?php
-$title = 'Assassin's Creed: Rebellion';
+$title = 'Assassins Creed: Rebellion';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'niiolmecmkjoknmmoghkelamenbnenkg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
