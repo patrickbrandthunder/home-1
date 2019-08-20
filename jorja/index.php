eca689c43295bbe3d2da0136de3cbbd62ea88d74
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-20 17:10:32 -0400

    Update index.php

--- jorja/index.php
+++ jorja/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Jorja Smith';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pfdpkdmdamafeodlphcgmaaidbpljlnb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
