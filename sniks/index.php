47993027919a4c12b0105807cc3fdbc65cc6a264
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-20 17:02:47 -0400

    Update index.php

--- sniks/index.php
+++ sniks/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'SNIKS';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mdnogekjgdkjnhjgpedmnknbecphffmk';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
