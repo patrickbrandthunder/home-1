7c654d6455f82aad023ba0eb38a6478fe6d2f8e6
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-20 17:18:12 -0400

    Update index.php

--- raceoff/index.php
+++ raceoff/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Hot Wheels Race Off';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ghoclembjokodlmemgjoebnilehokmeb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
