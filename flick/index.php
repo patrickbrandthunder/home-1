d569cd581b36328d3d6809eb4c31500f42e23805
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-21 10:36:34 -0400

    Update index.php

--- flick/index.php
+++ flick/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Flick Quarterback';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hkojhfpnpdfofekdepgbilfglhmocgkk';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
