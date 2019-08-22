89c8f331141fcee24b7af1828bb951f70ad1f748
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-22 14:27:01 -0400

    Update index.php

--- kongblast2/index.php
+++ kongblast2/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Banana Kong Blast';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'iohaabhjmdcgghdeojldbofnlfmlmpjn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
