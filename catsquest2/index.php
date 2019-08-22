0dc5fe2124ecea9728f67ee8adfd6fbf296b2d27
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-22 14:44:06 -0400

    Update index.php

--- catsquest2/index.php
+++ catsquest2/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Cats Quest 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dipjcockjalacbfpngkonopphjlphnpd';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
