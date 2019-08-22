a0f3ad750011d965e0fbed96b4aabfdaf74494fb
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-22 15:09:52 -0400

    Update index.php

--- deadzed/index.php
+++ deadzed/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dead Zed';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'llgmnokeeodaghckcaekokemibkehkab';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
