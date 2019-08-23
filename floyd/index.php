0faac160c9d114d704b4a93d73e667c6a4bab7f3
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-23 09:59:17 -0400

    Create index.php

---
+++ floyd/index.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'floyd a1 bentley';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
