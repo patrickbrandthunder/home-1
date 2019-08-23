d1a4a37ebae93168ffce31abe9f596368736cfda
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-23 10:22:37 -0400

    Update index.php

--- stickfight/index.php
+++ stickfight/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Stick Fight The Game Mobile';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'apkpkckbdpnfhekenkikpnlnckekjjgk';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
