a1d48454dfc36b2119384a2a74b3f1e0ab9116ee
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-23 10:04:45 -0400

    Update index.php

--- floyd/index.php
+++ floyd/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'floyd a1 bentley';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mmbdaelcmocpjmillnbbfckabnncnojg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
