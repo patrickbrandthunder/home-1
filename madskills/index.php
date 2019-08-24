9ebce40129e136c22518f7f0d066bd324a08af62
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-24 10:00:31 -0400

    Update index.php

--- madskills/index.php
+++ madskills/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mad Skills BMX';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kgbeggkoibhijjahhgcndnpijdgcoiea';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
