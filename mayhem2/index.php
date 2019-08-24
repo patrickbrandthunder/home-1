b433be9fd3dbf0e2f20cd62dc2fc9cd35fb14a80
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-24 12:23:35 -0400

    Update index.php

--- mayhem2/index.php
+++ mayhem2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mayhem Combat';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'adphinofhlcjehnalliblblnncgpjdge';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
