8362654c1f03a07ffaba02c016aef92bcbc9d9c8
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-24 09:02:41 -0400

    Update index.php

--- almostgone/index.php
+++ almostgone/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'The Almost Gone';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jglchgmkgceggiomkdjmnhdijjmleacn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
