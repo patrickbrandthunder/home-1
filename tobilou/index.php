dd716594abea2f2bd2f71b7af7b00d29c709ebc1
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-24 08:54:37 -0400

    Update index.php

--- tobilou/index.php
+++ tobilou/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Tobi Lou';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jgcdllbhkbndhfcpjegaomibmcgkgjol';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
