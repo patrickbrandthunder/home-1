0437f2d36d3feb131a51e1370b3d3af027a758b7
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-25 18:06:00 -0400

    Update index.php

--- skaterxl/index.php
+++ skaterxl/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'SKATER XL';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'chdfbiofboaemllilamoddjlkjanpjka';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
