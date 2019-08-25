370f87bf20ae5671dbe9f275e4cc64323c5fc986
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-24 20:40:32 -0400

    Update index.php

--- wildscapes2/index.php
+++ wildscapes2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Wildscapes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'famfcjnklampkipdkbmkmcfpgjmigmhg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
