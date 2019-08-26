02147ff1c0cdfd6e8e04532b5c7d2f9163c88d5c
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-26 16:07:33 -0400

    Update index.php

--- rushwars2/index.php
+++ rushwars2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Rush Wars';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'idkhnkcaflpjkapdlleefdpldkcopned';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
