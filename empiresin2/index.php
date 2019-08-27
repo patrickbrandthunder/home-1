df573efa0554c038c9a051068349d4ea0978cb88
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-27 16:49:16 -0400

    Update index.php

--- empiresin2/index.php
+++ empiresin2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Empire Of Sin';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pbbkpeghjloodkffgmhbomjlhbomcllb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
