9335d0280cc549d4b9d8e268a8966515c6ac29db
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-28 10:51:52 -0400

    Update index.php

--- witchisle2/index.php
+++ witchisle2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Witchs Isle';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'konkaopgocaomaaggcjidaaebldmdogb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
