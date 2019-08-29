278d933701889dcc0ae4718f91bfe2fa1e5e2e26
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-29 11:28:37 -0400

    Update index.php

--- bendy/index.php
+++ bendy/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bendy in Nightmare Run';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dpfjifogfbbdmcfdjgnjjfcojmlielom';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
