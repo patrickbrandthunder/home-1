4f62f25b1c04120d8006bd9861add7193b0a0828
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-30 16:25:38 -0400

    Update index.php

--- bistro/index.php
+++ bistro/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'My Secret Bistro';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pkmnhieolnjgkfglcopligppkhnalbnn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
