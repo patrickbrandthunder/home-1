299e8609a51e0351218332eb51bc85f48c4b2594
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-30 15:29:49 -0400

    Update index.php

--- legolegacy/index.php
+++ legolegacy/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'LEGO Legacy: Heroes Unboxed';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'alihbehedbhnjkdhfmngfophdoiaadob';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
