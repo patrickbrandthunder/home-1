95a35c789eecd4c6ba9449a07a79fcb931ae0bcd
pjmurphy777 <pjmurphy77@gmail.com>
2019-08-30 11:26:29 -0400

    Update index.php

--- goodpizza/index.php
+++ goodpizza/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Good Pizza Great Pizza';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'memgpgboaknjigaplolgmgkcfhofmjok';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
