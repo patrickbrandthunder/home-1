f38b66ff7f279f00913bf568193e47d59bc8320d
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-02 09:37:22 -0400

    Update index.php

--- carmens/index.php
+++ carmens/index.php
@@ -2,7 +2,7 @@
 $title = 'Carmen Sandiego';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'mjmndiejflkdkkhdmpjijmpdgfcgmaak';
-$searchExtensionID = 'ckflbkkgidijgpkkphiihlcckfhbmipb';
+$searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
