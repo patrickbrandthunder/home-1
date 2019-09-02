e3274e7563871cb96d01729a4be0d74adb38adf8
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-02 09:34:01 -0400

    Update index.php

--- Balenciago/index.php
+++ Balenciago/index.php
@@ -2,7 +2,7 @@
 $title = 'Balenciago';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'kcoipablopkpidnellmceeilgjgkdacm';
-$searchExtensionID = 'abimepoegpcdgigmggdppaamigbkfcce';
+$searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
