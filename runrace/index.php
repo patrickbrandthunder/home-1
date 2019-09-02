995a015c103334150b9a714d9dbdc939eaad36f1
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-02 09:36:00 -0400

    Update index.php

--- runrace/index.php
+++ runrace/index.php
@@ -2,7 +2,7 @@
 $title = 'Run Race 3D';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'lmopmnjanemoaibhlodlodkhlnnblmkn';
-$searchExtensionID = 'phikkphjojdpkholaldlaifhlapkmbmn';
+$searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
