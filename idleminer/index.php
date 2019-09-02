0e4e52b54a7099d86ff89469aa8a8dd848920cd5
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-02 09:37:00 -0400

    Update index.php

--- idleminer/index.php
+++ idleminer/index.php
@@ -2,7 +2,7 @@
 $title = 'Idle Miner';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'fpogdcnpfladelmfdegllkadlbfegdki';
-$searchExtensionID = 'lnabflefpegkjbalaehgldgbdnphlpce';
+$searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
