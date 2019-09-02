b97330c1aa30d5888eee0d727e3e36e2db5c6133
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-02 09:34:28 -0400

    Update index.php

--- disneyworld/index.php
+++ disneyworld/index.php
@@ -3,7 +3,7 @@
 $title = 'Disney World';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'cekilknoaibaakoppkfmcmphcldamlkk';
-$searchExtensionID = 'pnjadmfkbohedmfhpleifjdghlicocle';
+$searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
