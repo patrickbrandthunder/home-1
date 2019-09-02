e568db5371587e1b36906aa134fb2cdda9bb1ac7
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-02 09:39:05 -0400

    Update index.php

--- pickups/index.php
+++ pickups/index.php
@@ -2,7 +2,7 @@
 $title = 'Pickups';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ponigckkbhpekcadbdpmdipeljekibcm';
-$searchExtensionID = 'hepdeocgiacicoibbeajgigmiodinajc';
+$searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
