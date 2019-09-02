5d493450fb586fd9f6ea710df09b78acadaea537
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-02 09:33:30 -0400

    Update index.php

--- quando/index.php
+++ quando/index.php
@@ -2,7 +2,7 @@
 $title = 'Quando Rondo';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'dmmiaalhljpiemanpkldagepcmmjdonp';
-$searchExtensionID = 'lpnkmjjgdammpdhfchanpnfennjfganb';
+$searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
