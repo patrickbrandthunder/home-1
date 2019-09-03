c72c7c9683808d6656b768c7b6418ad79c53767a
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-03 15:59:42 -0400

    Update index.php

--- warhammero/index.php
+++ warhammero/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Warhammer Odyssey';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ljlphjoejgjjdidkocefghonedaacclm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
