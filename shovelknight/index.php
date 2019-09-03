22db67cf166dad9fdabc92a87f5c74465cf21d53
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-03 16:29:28 -0400

    Update index.php

--- shovelknight/index.php
+++ shovelknight/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Shovel Knight Dig';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gmaobflhcbebocadhohckahclpdpjdob';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
