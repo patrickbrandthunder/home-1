2d5182f966de35bedf8ff543f43a13be33d2edab
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-03 10:50:11 -0400

    Update index.php

--- raymanrun/index.php
+++ raymanrun/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Rayman Jungle Run';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pjalnfokgmmnkifmonanakgkcjglcfim';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
