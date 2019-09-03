2755c2d33331d48fa38133d277891cee2ac4e746
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-03 11:20:05 -0400

    Update index.php

--- rungun/index.php
+++ rungun/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Run and Gun BANDITOS';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nhjkagbgcddlmjiboagkjjgdaimgfcjm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
