256752c112e1c99ba6be70d225bf213d95c32be1
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-04 10:29:48 -0400

    Update index.php

--- astralchain/index.php
+++ astralchain/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Astral Chain';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'naamaldpcjpjlhaadjopeplnbkjhmemd';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
