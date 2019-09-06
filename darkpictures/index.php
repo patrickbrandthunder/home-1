ca70efd5726bf2a90acf53eb0484e370423ad27e
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-06 09:16:26 -0400

    Update index.php

--- darkpictures/index.php
+++ darkpictures/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dark Pictures: Man of Medan';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lmmhjfpigkfmcdkbphlibbpmpbbmmakd';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
