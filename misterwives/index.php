a72fd5edb1b028a5558596001d88d7a9e4526a7f
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-06 09:01:15 -0400

    Update index.php

--- misterwives/index.php
+++ misterwives/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'MisterWives';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cnncpalchijmilpdcipbbcipbgojiahb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
