ba50ac672cde2c84daa12acbbd798f69f3813d9f
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-08 16:04:26 -0400

    Update index.php

--- driftit/index.php
+++ driftit/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Drift It';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lfjogldlolknpnlkpphnoljojkhooiom';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
