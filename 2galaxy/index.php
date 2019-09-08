c08d7b3a407a9bb04d016c4bb3886366f5f40d2b
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-08 09:30:54 -0400

    Update index.php

--- 2galaxy/index.php
+++ 2galaxy/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Second Galaxy';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'odejdanchfjojficbmgmgeclhdpnnoga';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
