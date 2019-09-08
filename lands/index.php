17e8fffdcde04f904180d5a2f4ac99efc9e0640a
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-08 09:54:49 -0400

    Update index.php

--- lands/index.php
+++ lands/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Euclidean Lands';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hnnhfdhafgnlpnmcgohnhlhebclbnpbb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
