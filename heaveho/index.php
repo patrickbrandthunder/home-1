751510a4e7fb8f99f80e225bd4b7db686977631f
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-08 09:24:15 -0400

    Update index.php

--- heaveho/index.php
+++ heaveho/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Heave Ho';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nmgknkboeceeakcohjnjdbnkimmmneeh';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
