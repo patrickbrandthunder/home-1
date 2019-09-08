42797190b9c4c06acdd53a8376736aa6b5a48150
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-08 17:34:51 -0400

    Update index.php

--- youngmal/index.php
+++ youngmal/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Young Mal';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mjbfbjlebfoojennekkfhbgagnkajnga';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
