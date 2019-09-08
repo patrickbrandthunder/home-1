5a1cb3b8b9b7269ea541e15d0ba202fd541d807f
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-08 09:38:13 -0400

    Update index.php

--- smalltown/index.php
+++ smalltown/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Small Town Terrors';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lifnbplepnncgomcckdhmiimabpcjend';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
