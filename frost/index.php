d68853dd0ebd7bc68c5ec81b4fcc6e1733740a35
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-08 09:48:49 -0400

    Update index.php

--- frost/index.php
+++ frost/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Frost';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mpnpllneadjkpclgegbdaebojfjemheo';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
