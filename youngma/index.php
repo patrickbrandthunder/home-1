7bae809202d7349b171b14027a726b13f3047b7b
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-08 09:01:32 -0400

    Update index.php

--- youngma/index.php
+++ youngma/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Young M.A.';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cldimjocgbdeiabbaphglffplifbjikn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
