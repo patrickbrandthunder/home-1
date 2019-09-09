f9f74dfa1eb84f4874058c05050e337c2e404046
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-09 10:47:31 -0400

    Update index.php

--- widower/index.php
+++ widower/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Widowers Sky';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kmebfjepllbgfjnjbenhmdcnpjeodcma';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
