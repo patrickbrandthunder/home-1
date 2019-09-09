016fc415db6d2aff6d6910874ad6a9cdefd26bc7
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-08 21:55:46 -0400

    Update index.php

--- doeboy/index.php
+++ doeboy/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Doe Boy';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = 'cknilnljcgdpgkohhhjkdhdhegaohamg';
+$extensionID = 'lfhndaedlghebnobpbeopoklainbipij';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
