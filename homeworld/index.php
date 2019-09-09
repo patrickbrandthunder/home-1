531f9d6e813b063b03432dd988bac40faaa37144
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-09 10:54:59 -0400

    Update index.php

--- homeworld/index.php
+++ homeworld/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Homeworld Mobile';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gebfgfimjpjefpidmgfmjkhmjpifeglp';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
