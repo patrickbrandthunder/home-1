df6ddfb5c46381b1256b3c8d5441a9eb96153038
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-09 16:56:11 -0400

    Update index.php

--- mythgard/index.php
+++ mythgard/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Mythgard';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'edmnoiejcacdojeaackameoddjkkehke';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
