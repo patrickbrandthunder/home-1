7d9783efc06eeb5071a096ca90806e37e48b7fb4
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-09 08:28:51 -0400

    Update index.php

--- riverl/index.php
+++ riverl/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'River Legends';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mclaodhbcimaepfkpmfpkimoaahimkcm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
