fe119db5a33acab8e6c6d34d9321c1ff17ccb0be
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-10 12:34:27 -0400

    Update index.php

--- yooka/index.php
+++ yooka/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Yooka-Laylee';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'licajpdlkmkceihkoeggmkkgmkkbmbil';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
