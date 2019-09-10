0bd6f3a29a0df120a5e6fa5a99b412084983ec1b
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-10 12:18:23 -0400

    Update index.php

--- jedi/index.php
+++ jedi/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Jedi Outcast';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cmocjadgegacbmhdnlfccpllkembecag';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
