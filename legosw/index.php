b7691eff60213127642ff88c52c1c700fc17f491
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-10 11:07:32 -0400

    Update index.php

--- legosw/index.php
+++ legosw/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'LEGO Star Wars Battles';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'oldphnnklcggoemjihgjidkimohjeokm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
