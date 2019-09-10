28b3f90b66dc3d4f3a39a5fb0c7203f447a1bd4f
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-10 17:25:58 -0400

    Update index.php

--- heros/index.php
+++ heros/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Hero Siege';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dpakeplohbnckaelmahjcegcnkilknng';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
