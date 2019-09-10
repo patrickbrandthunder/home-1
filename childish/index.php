0d867cbd5f654b88677bef28ab3191608cf6aeb3
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-10 14:20:30 -0400

    Update index.php

--- childish/index.php
+++ childish/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Childish Major';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kofoihmpfclmpkecbkbifdhpfjjegbeh';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
