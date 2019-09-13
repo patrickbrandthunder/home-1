237a25915fa3555aa7073a27050f30f0fef1059d
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-13 19:46:42 -0400

    Update index.php

--- airlinesmgr/index.php
+++ airlinesmgr/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Airlines Manager';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ikenlhojaipnlgbkaemjgmamnkmmadjl';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
