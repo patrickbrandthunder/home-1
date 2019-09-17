a97c696e4a7be68a3fc4016c5e490e5795a546dd
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-17 15:27:43 -0400

    Update index.php

--- feemeow/index.php
+++ feemeow/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'FeeMeow';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hfpoaokcpfofcchbbpipieblndgfjmig';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
