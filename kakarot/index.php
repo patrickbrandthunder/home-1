984f1b0541ae587ce94b14744ad4c6c6b0e87303
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-17 13:36:14 -0400

    Update index.php

--- kakarot/index.php
+++ kakarot/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dragon Ball Z: Kakarot';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dkfpogmjmlabjdcbnofoehpjbpkedmaf';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
