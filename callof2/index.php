bd0794f7146a366938d6c874dfa9690354b192e8
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-17 13:27:09 -0400

    Update index.php

--- callof2/index.php
+++ callof2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Call of Cthulhu1';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hlbfjdamjeobpbkhlmjdllobglphialk';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
