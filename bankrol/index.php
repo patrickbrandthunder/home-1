40cf57450bdb1e601391dd509ef0daa38772659e
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-17 11:06:02 -0400

    Update index.php

--- bankrol/index.php
+++ bankrol/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'bankrol hayden';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'blonbnpklhamljpkllnffeffemdfbcde';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
