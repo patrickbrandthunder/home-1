d2e46f5fe905bd5a7b36b9ec24cde336a062f9c0
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-17 15:11:11 -0400

    Update index.php

--- fallcolors/index.php
+++ fallcolors/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'fall colors reflection';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kcalcobmpainpmonndfmjdglinaldfpb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
