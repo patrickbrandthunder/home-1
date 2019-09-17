97f95f4195184f2a0301a2811ce6f26d22093d53
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-17 13:23:29 -0400

    Update index.php

--- blackxmas/index.php
+++ blackxmas/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Black Christmas';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mlkefdgmpffpnbmbdghpkfjnpidjebco';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
