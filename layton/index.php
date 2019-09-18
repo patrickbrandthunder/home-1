5abf693ea7a4462f9d846d52f646be3053969cca
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-18 14:43:04 -0400

    Update index.php

--- layton/index.php
+++ layton/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Professor Layton and the Curious Village';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ipkbebdeacbfjngffoedajfnnlcioaoe';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
