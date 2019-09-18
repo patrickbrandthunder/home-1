fb93fd4945f6b64f91c85dab452e501321beba74
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-18 08:48:30 -0400

    Update index.php

--- collect/index.php
+++ collect/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Collect Cubes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gkigplljmkgebhojljbjifhgdpojbnml';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
