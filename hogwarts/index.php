92571cde5a4ac2f6f20776cf115a6b7a3d1e95aa
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-18 14:30:46 -0400

    Update index.php

--- hogwarts/index.php
+++ hogwarts/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Harry Potter Hogwarts Mystery';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ihofdpdfgfkclkponeadlpgldphpkbfa';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
