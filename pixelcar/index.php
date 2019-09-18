d78b72ae7a392e66d3fe174b1030a55bc964dcaf
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-18 16:41:18 -0400

    Update index.php

--- pixelcar/index.php
+++ pixelcar/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Pixel Car Racer';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'opgjmldcnngkdbgknapkiiecepblflca';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
