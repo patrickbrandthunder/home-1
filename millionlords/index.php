8f803ac4ab3c5a062e513227fec30ed3380dda30
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-18 11:15:07 -0400

    Update index.php

--- millionlords/index.php
+++ millionlords/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Million Lords';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'eafnhjciedgihjooknoajcamahiiolhh';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
