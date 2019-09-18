17f6b61708cfc3726893dbd41445a6be2a2c0a22
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-18 09:06:24 -0400

    Update index.php

--- warren/index.php
+++ warren/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Elizabeth Warren';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ipjlgdlogafhglgcnpcbhdbglgpanfep';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
