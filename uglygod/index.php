8fc7cb5dabb2c5a6efc0dcebab496a22874db41f
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-19 11:09:16 -0400

    Update index.php

--- uglygod/index.php
+++ uglygod/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Ugly God';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bindcdllloifhcjmjdebcdamdgjdkoea';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
