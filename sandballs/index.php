2106058fe76c38593db30e9aab2d1dc219473d42
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-19 10:27:00 -0400

    Update index.php

--- sandballs/index.php
+++ sandballs/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Sand Balls';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jnphckmeomelpinampcpaagbpldgknfh';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
