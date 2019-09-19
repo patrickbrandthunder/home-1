aae0875f7fb17239eefac0de2c9d223b66cc0d8f
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-19 12:06:29 -0400

    Update index.php

--- warcommand/index.php
+++ warcommand/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'War Commander Rogue Assault';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bapbdajajihcdfehlpiankhikblglenm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
