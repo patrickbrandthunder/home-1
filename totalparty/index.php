3e03d302a00f936fe19be13b618833957e9ca281
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-19 10:50:03 -0400

    Update index.php

--- totalparty/index.php
+++ totalparty/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Total Party Kill';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = 'ncbcenghddcloacoailclhgkpdioflog';
+$extensionID = 'djookncmjfmfcjbephganpdjmenibdeg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
