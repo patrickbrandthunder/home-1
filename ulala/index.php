79ee816891ab3508e72d99a5849aaa629aba0bcc
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-20 11:28:49 -0400

    Update index.php

--- ulala/index.php
+++ ulala/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Ulala: Idle Adventure';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jinlcjfbnhjhcfhbocbdkmaipolknnbd';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
