747aabceda939fa7cf69406c1b9dad9830c985d9
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-20 13:16:29 -0400

    Update index.php

--- spacehobo/index.php
+++ spacehobo/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Space Hobo';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ddaoligfpdhdmdlmgdhmhjikjfpenbmb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
