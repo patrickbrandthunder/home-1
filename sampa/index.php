9a8b2238ff887a6c3063d2f229360b4af0a3f28d
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-20 13:02:33 -0400

    Update index.php

--- sampa/index.php
+++ sampa/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Sampa The Great';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'canphammadfhoiafnkdalamkfnjmohmc';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
