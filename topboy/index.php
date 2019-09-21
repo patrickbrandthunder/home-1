6bbd4495c3d4f81408dbfdf11455c78cff881d5c
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-21 09:08:53 -0400

    Update index.php

--- topboy/index.php
+++ topboy/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Top Boy Netflix';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kbooiiaonpfdlofcololgkllldemajki';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
