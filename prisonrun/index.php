8164a4d7c3ad59214fbd72d612ef66222657e3d6
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-22 17:58:04 -0400

    Update index.php

--- prisonrun/index.php
+++ prisonrun/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Prison Run And Gun';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cipfdhcbjgaipncmemoilbnnibmllnpf';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
