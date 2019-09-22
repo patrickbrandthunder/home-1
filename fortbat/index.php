ecbb52757c0d401f5b28e31abeb4d58b58357ee2
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-22 09:52:30 -0400

    Update index.php

--- fortbat/index.php
+++ fortbat/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'BATMAN x FORTNITE';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'blbhjapnefgigponailccoehofmjmoip';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
