c9baee8870ae14f1cf2e6f73d155b492ec0a41a6
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-23 12:27:10 -0400

    Update index.php

--- speeddemons/index.php
+++ speeddemons/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Speed Demons';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mlafgmogmmjbmafnomomhennoaiadcgn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
