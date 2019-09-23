b07a746262f125cffae7615d6fa52465bd803879
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-23 08:53:12 -0400

    Update index.php

--- bounties/index.php
+++ bounties/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Legendary Bounties';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kggfeanepmgafkkjhidoacmbaahpoaie';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
