c9cfe18375f11fd780da202695416a6eaa0f2b96
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-23 15:46:27 -0400

    Update index.php

--- goosegame2/index.php
+++ goosegame2/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Untitled Goose Game';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mmdghahiobihkkipifgbpfkfbmcajlbo';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
