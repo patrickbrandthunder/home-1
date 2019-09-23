126b15b4adead810d1c0dc1da194c614f65b7378
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-23 08:31:42 -0400

    Update index.php

--- cricket/index.php
+++ cricket/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Cricket Through the Ages';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ibdbljnmfpbkdcnkciidejbcappiainm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
