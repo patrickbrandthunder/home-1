a49f7db67ac5a39bf8fa8d953af176f13c545d82
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-24 13:34:04 -0400

    Update index.php

--- horsehaven/index.php
+++ horsehaven/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Horse Haven';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'oefpdmhmllhlfoaghbnlhihmfongdlen';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
