c412ba930b222221813872a78fa4297d15a35138
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-24 16:10:08 -0400

    Update index.php

--- minigolf/index.php
+++ minigolf/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Mini Golf King';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hnfinoenghclfddlhllplldfgbajmghh';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
