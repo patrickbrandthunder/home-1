8a1c569dfad782adffdc94e3cf66aca60b553529
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-25 08:54:47 -0400

    Update index.php

--- touchgrind/index.php
+++ touchgrind/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Touchgrind BMX';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hfobfpcdhmpghjhjlnajenejngajmkjk';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
