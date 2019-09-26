cf9f10f20d32beebfb89c6ea44bf84c1cf8f134b
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-26 10:09:14 -0400

    Update index.php

--- gangstarr/index.php
+++ gangstarr/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Gang Starr';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fjipchlioacgnoigjembahdncopgibpk';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
