684b07d89ab071c5c530b15950cd662429f0a159
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-26 10:13:54 -0400

    Update index.php

--- djdrama/index.php
+++ djdrama/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'DJ Drama';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jdjeecdalcajiegfcedcjcnfehejokfi';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
