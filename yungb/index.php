66356460a8e68ff8adc4c65efc8eeaada34bd0de
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-27 10:36:07 -0400

    Update index.php

--- yungb/index.php
+++ yungb/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Yung Bleu';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gmghnahialloiolhiaphfckmepeeiced';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
