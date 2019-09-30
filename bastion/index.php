66f5df238ee339adcde010477bf018d8e16209e2
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-30 10:47:55 -0400

    Update index.php

--- bastion/index.php
+++ bastion/index.php
@@ -3,7 +3,7 @@
 $title = 'Bastion';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'Bastion';
-$searchExtensionID = '';
+$searchExtensionID = 'ioeeapfbjbjgjoedfkcdfbjpmfcahlkl';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
