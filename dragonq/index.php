98125280af77d2687df37cdb1b719de02e3a1df9
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-30 15:44:23 -0400

    Update index.php

--- dragonq/index.php
+++ dragonq/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Dragon Quest Of The Stars';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'djhpphfhjdofknnhhbdbkmigdackfjof';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
