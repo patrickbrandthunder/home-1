0a8c700d30154d43db738a7edceaed62482206de
pjmurphy777 <pjmurphy77@gmail.com>
2019-09-30 09:38:03 -0400

    Update index.php

--- panzers/index.php
+++ panzers/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Clash Of Panzer';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cflcgbajliikaomhnlgefnoiigdjcaao';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
