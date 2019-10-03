982b5a7cd08c56a2e66683ae554b27ba7556084d
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-03 10:13:56 -0400

    Update index.php

--- hotlava/index.php
+++ hotlava/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Hot Lava';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ppbanfahmfmdoanpkmeccagjfonlkaic';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
