4f5ab652d818c186462443fb2ee9c41fba9c6088
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-03 10:21:42 -0400

    Update index.php

--- trapboy/index.php
+++ trapboy/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Trapboy Freddy';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'egmecpakfmamncghinjnnbpdcihgajfl';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
