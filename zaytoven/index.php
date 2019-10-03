b9218780b134167adcddbf16d6f7b964a5fd5d42
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-03 10:29:47 -0400

    Update index.php

--- zaytoven/index.php
+++ zaytoven/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Zaytoven';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hioafciccnallkoajnclmhlhehefefgh';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
