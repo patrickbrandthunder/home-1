fd675f9a7448c91a7605cf264f2a5153dadd64d5
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-03 10:09:54 -0400

    Update index.php

--- casanova/index.php
+++ casanova/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Casanova';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lccipebclimgjngnoodiblophcmnbeka';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
