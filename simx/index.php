4176fef9d942e5a27c7da6f32af41dd4ad146099
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-04 11:36:18 -0400

    Update index.php

--- simx/index.php
+++ simx/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'SimXSantana';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cpgpcjjajkpmhdhlcgolhmeccllipceo';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
