15d5bb22167ae36633070ebc2fa9000bb2a95c56
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-04 13:04:31 -0400

    Update index.php

--- smashc/index.php
+++ smashc/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Smash Champs';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'iecmjheddhbloefejndkhaanplkefigm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
