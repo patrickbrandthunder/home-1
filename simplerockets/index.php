7797a228a9fad26aab375793c83d98be525e7446
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-04 13:15:20 -0400

    Update index.php

--- simplerockets/index.php
+++ simplerockets/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'SimpleRockets 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ighjlfgooafgpihjediemdigkchgfcef';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
