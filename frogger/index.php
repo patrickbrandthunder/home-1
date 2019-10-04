218ed6a26017e5184167a86f6da86712e6aaf8d4
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-04 14:27:00 -0400

    Update index.php

--- frogger/index.php
+++ frogger/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Frogger In Toy Town';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ngdpnicaohoepphfikaknfpoeobidgal';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
