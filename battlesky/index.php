2963b2757baa8a5436544686d7b13e1b8c227bc1
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-04 15:23:13 -0400

    Update index.php

--- battlesky/index.php
+++ battlesky/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'BattleSky Brigade';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pcjhioepfkmhcgeehamhdjhcnbfnbgjl';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
