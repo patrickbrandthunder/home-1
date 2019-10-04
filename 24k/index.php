817c7643edba1982c1e152837fc98384d563c53d
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-04 11:27:27 -0400

    Update index.php

--- 24k/index.php
+++ 24k/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = '24KGoldn';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ifjdilmpdjdjpllljfildmjglgfooboo';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
