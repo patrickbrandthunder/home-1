c566e942cae2fdbce6e68d9d55cf8845fdbb7737
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-05 18:39:27 -0400

    Update index.php

--- codmobile/index.php
+++ codmobile/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Call of Duty Mobile';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = 'kjdjnjdnbhmnggebfdockggccoefadma';
+$extensionID = 'fmglgpcahadacgbgpgbakfhhflaedgag';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
