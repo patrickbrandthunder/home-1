8392ef7258602344910e49826927af64bab9caf1
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-06 09:19:01 -0400

    Update index.php

--- intercept/index.php
+++ intercept/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Agent Intercept';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'eblphpanognhoilpmdliibbamneanfkj';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
