070019f407f570797d341bebb792acc695fc398f
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-06 09:50:21 -0400

    Update index.php

--- wtg/index.php
+++ wtg/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'What The Golf';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bdomfnnplgfgfiiodmoidgmjgfjiapme';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
