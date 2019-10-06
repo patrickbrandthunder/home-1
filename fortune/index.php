62d8a0651e663d11483ab8c14b9fac38d5bfa35b
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-06 09:26:00 -0400

    Update index.php

--- fortune/index.php
+++ fortune/index.php
@@ -1,7 +1,7 @@
 <?php
-$title = 'Leo's Fortune';
+$title = 'Leos Fortune';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bioppdhjjjaeakcmdkmdjllbfdggbmmg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
