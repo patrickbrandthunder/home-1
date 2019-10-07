1d36f5d249d11e9fd49e8247c6892e13f095b555
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-07 15:59:17 -0400

    Update index.php

--- zooba/index.php
+++ zooba/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Zooba Action Game';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'oinaeiohgpcohdmbbejbioonooddkjkc';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
