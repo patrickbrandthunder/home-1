7fef4ae1d924b31ee68fd9cbdd0c7b32d9a89e17
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-07 16:54:05 -0400

    Update index.php

--- kmichelle/index.php
+++ kmichelle/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'K. Michelle';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lmecogikbcgaojjpjhlnpkhpiacmdfpa';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
