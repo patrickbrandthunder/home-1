0af69d90ccb92c70cae225248ee9dfba66e4e9c0
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-07 14:24:30 -0400

    Update index.php

--- cursev/index.php
+++ cursev/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Marvel Knights Curse of the Vampire';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ajckoidmlkcnenlffdldgeobldgggohm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
