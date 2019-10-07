49557baa14c1caf992130527ae709fd5e63a650d
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-07 16:51:34 -0400

    Update index.php

--- kingsraid/index.php
+++ kingsraid/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Kings Raid';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pkacbdljbnbhbkknighhphdbffmngboa';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
