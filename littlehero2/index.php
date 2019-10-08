865fc771a18a4192dc4da1cc97d0bde8cf879ad0
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-08 14:02:40 -0400

    Update index.php

--- littlehero2/index.php
+++ littlehero2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Little Town Hero ';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'aiedfomggnhlnhaonkcendnajkpmfpkg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
