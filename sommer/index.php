d162693b74e3b3c77e9d196ded58d0dc71cb2c06
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-08 16:15:10 -0400

    Update index.php

--- sommer/index.php
+++ sommer/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Sommer Ray';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lbkfdopphbfjhefalkdjceneajhjidmn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
