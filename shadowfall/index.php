4aefb22ffd3434363183bc2ee5df22fc0e27ac59
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-08 07:41:32 -0400

    Update index.php

--- shadowfall/index.php
+++ shadowfall/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Apex Legends Shadowfall';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'epfaalemknneliigamhphfmffpifobge';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
