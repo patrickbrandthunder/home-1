84dd4d65a3d3aef405fa272d0562c9704a3ad453
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-09 15:36:09 -0400

    Update index.php

--- contra/index.php
+++ contra/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Contra: Rogue Corps';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'caiepjjbfmhecahellbigefnfohpdeen';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
