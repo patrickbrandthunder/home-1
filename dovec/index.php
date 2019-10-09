0ae294e139037bee28dcfbaa31634dea93e48129
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-09 15:42:19 -0400

    Update index.php

--- dovec/index.php
+++ dovec/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Dove Cameron';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cbnndeeeoknogghigfpflplkilkceejh';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
