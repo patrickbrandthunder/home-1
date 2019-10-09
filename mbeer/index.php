07fc9f3255c9e5613562ddee00a4dc613bfeb6eb
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-09 16:29:22 -0400

    Update index.php

--- mbeer/index.php
+++ mbeer/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Madison Beer';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'icdjkapjnbdmppijoigkkffofcpmgmdj';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
