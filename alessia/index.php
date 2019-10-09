d1a9e8908da8979fafacab7ae068c601b02b975d
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-09 16:45:56 -0400

    Update index.php

--- alessia/index.php
+++ alessia/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Alessia Cara';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pagnmgmepkfdpokamdbifcpjadoiehgc';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
