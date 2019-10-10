c34b54f67aa9f07380b55d074ab9f3df05e86f91
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-10 11:07:47 -0400

    Update index.php

--- blackblueshards2/index.php
+++ blackblueshards2/index.php
@@ -2,7 +2,7 @@
 $title = 'Black Blue Shards 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'bglakhilkhcagkgefkidanahmlfihpgk';
-$searchExtensionID = '';
+$searchExtensionID = 'kliiambfijiclfllpnnkajokjheoapfe';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
