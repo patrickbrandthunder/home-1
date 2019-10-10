7d7aca2da8b0bdacf1481fa2056c2257f47fd1ff
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-10 15:08:25 -0400

    Update index.php

--- nlechoppa/index.php
+++ nlechoppa/index.php
@@ -2,7 +2,7 @@
 $title = 'NLE Choppa';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'iliopbobmbijgbbcaoipakjlmaghhbkc';
-$searchExtensionID = 'fbolncikgkgnoojhpfmecjoebdloggei';
+$searchExtensionID = '';
 $testSearch2  = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
