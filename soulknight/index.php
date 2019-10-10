974130e628c7e07f8fe5afc9399e14cf4d44cb71
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-10 16:43:12 -0400

    Update index.php

--- soulknight/index.php
+++ soulknight/index.php
@@ -2,7 +2,7 @@
 $title = 'Soul Knight';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'epnbhbfnbhdleboidigifgjabhkjfhhi';
-$searchExtensionID = 'kcdmohfpkegipfilcnkhafnkmciiggmc';
+$searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
