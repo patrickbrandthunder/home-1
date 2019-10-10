c610f90668aaba5c3a9f5b40fedac1ba2f30c2e4
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-10 14:46:18 -0400

    Update index.php

--- babyjesus/index.php
+++ babyjesus/index.php
@@ -2,7 +2,7 @@
 $title = 'Baby Jesus';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'dlkgihjfbkdicpgmfjleecnamelnolba';
-$searchExtensionID = '';
+$searchExtensionID = 'dmbbbpdecbikoifimkjmioielacppkgi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
