9c67e36d5820bf54ab43276d2f8707cabfb931e3
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-11 09:32:41 -0400

    Update index.php

--- grannychap2/index.php
+++ grannychap2/index.php
@@ -2,7 +2,7 @@
 $title = 'Granny Chapter 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'gddalcehkkjjldidljeeakjommmajpaj';
-$searchExtensionID = '';
+$searchExtensionID = 'eaajnjineiclfocnpmedeilmipbnjkpk';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
