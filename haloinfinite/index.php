6ddafa22935e3867cfb4416ee806e1e28769b304
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-11 08:51:59 -0400

    Update index.php

--- haloinfinite/index.php
+++ haloinfinite/index.php
@@ -2,7 +2,7 @@
 $title = 'HALO Infinite';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'jeckdegmelkhgganpbhkhhphonpgomel';
-$searchExtensionID = '';
+$searchExtensionID = 'hkjdjjlbdniapbgodjcmgjalepcdjipn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
