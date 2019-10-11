59847cf79c623ac4845d9ccf2e30038d4b1447ca
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-11 14:52:22 -0400

    Update index.php

--- creative2/index.php
+++ creative2/index.php
@@ -3,6 +3,7 @@
 $title = 'Creative Destruction 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'heibbhdhbgnolgabfgnbeghabmlefcah';
+$searchExtensionID = 'lbolpkgihnhlhflmoehaneoeifdimmde';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
