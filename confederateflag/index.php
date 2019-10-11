68b87feab5ff7c0981c4c6ae9ad4908e6b39f730
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-11 14:46:38 -0400

    Update index.php

--- confederateflag/index.php
+++ confederateflag/index.php
@@ -3,7 +3,7 @@
 $title = 'Confederate Flag';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'iabondibenfnndnfhfdmkkiiehppdhmm';
-$searchExtensionID = '';
+$searchExtensionID = 'iicpmokpiepcmfodnaodffekdkidgafi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
