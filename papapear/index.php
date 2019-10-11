e6f77e80e026df3764ec81d2a7a840c8c97f7905
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-11 10:39:38 -0400

    Update index.php

--- papapear/index.php
+++ papapear/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Papa Pear Saga';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'obikacehjgehjckcchenafgojjaipond';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
