98eb8fcf7fe7a304b8a52f805835ba5e9df3203d
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-10 20:55:07 -0400

    Update index.php

--- marioodyssey/index.php
+++ marioodyssey/index.php
@@ -2,7 +2,7 @@
 $title = 'Mario Odyssey';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ocdopjdihddkemckkdnkambnjhmhfjhe';
-$searchExtensionID = 'jmdfanlmknjdfpkkbghabicgejnnmdjk';
+$searchExtensionID = 'kipmhbbcpampnfplkgnepbaleookbppc';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
