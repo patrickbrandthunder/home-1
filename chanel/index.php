63303276e830666f52d4ea9ed3231b6659df2e68
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-12 07:28:29 -0400

    Update index.php

--- chanel/index.php
+++ chanel/index.php
@@ -2,7 +2,7 @@
 $title = 'Chanel';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'hijlpgochlfkpnoehkdbgpnlcafjdbcp';
-$searchExtensionID = '';
+$searchExtensionID = 'iekdlabjdglmealikcbgjekiiphmjige';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
