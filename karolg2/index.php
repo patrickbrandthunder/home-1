fabd26dcd3fbea1d0a8270f16c94806929ad382e
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-12 13:26:04 -0400

    Update index.php

--- karolg2/index.php
+++ karolg2/index.php
@@ -3,7 +3,7 @@
 $title = 'Karol G 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'hpgodnaipdffpanlchcafcmakmlghodj';
-$searchExtensionID = 'fiaabaiedagicdcapdkjlpgeadhlkeci';
+$searchExtensionID = 'knjljfhlokffhgppofebhcbmpkcabmhp';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
