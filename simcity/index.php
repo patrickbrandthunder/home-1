367cff1b5d894719da2c4345f6fb026960409ede
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-12 12:42:39 -0400

    Update index.php

--- simcity/index.php
+++ simcity/index.php
@@ -3,7 +3,7 @@
 $title = 'SimCity BuildIt';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'noanmoaoclkikdbkakhihigdmfccpigo';
-$searchExtensionID = '';
+$searchExtensionID = 'kknpalcgeigaobpodchadnjncogkpjno';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
