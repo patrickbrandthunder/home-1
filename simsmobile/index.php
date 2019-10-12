ac589559c289bd3aa849dad6d58a3baaf59fc9bd
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-12 12:42:58 -0400

    Update index.php

--- simsmobile/index.php
+++ simsmobile/index.php
@@ -2,7 +2,7 @@
 $title = 'SIMS Mobile';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'gnbookopbfhmapiknmepndkpmllacfii';
-$searchExtensionID = '';
+$searchExtensionID = 'kknpalcgeigaobpodchadnjncogkpjno';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
