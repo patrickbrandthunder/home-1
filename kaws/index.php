01f94c8ecb6956ba7b2f34e6609890f929db32a2
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-13 15:59:37 -0400

    Update index.php

--- kaws/index.php
+++ kaws/index.php
@@ -2,7 +2,7 @@
 $title = 'KAWS';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'hnbgjjdflaffiojkhllhkpmengnjdaep';
-$searchExtensionID = '';
+$searchExtensionID = 'idnelcbpklcgngmglgdpchpnogiinpmn';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
