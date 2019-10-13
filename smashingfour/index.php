899ddc4296ef7bdc463195dae768a80d572f4d44
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-13 13:02:56 -0400

    Update index.php

--- smashingfour/index.php
+++ smashingfour/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Smashing Four';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'aikehgpfmghegdfaomcdnnmnlglmkinb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
