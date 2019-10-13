c3ddeaa4debbb3eb7b1e8542902f6fc66d327a4a
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-13 09:17:04 -0400

    Update index.php

--- minionv/index.php
+++ minionv/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Minion Valley';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ikgaojggkeppmgigkpibkbfhajdndgoj';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
