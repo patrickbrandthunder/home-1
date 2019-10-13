c447c7fc0f0b15b447b5878eb6b1f0c71e48e265
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-13 13:34:17 -0400

    Update index.php

--- dannyd/index.php
+++ dannyd/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Danny Duncan';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'iodgmjahdkkhigfecihpaaiboceldpnk';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
