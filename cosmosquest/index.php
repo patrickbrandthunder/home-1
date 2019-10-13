ca27669cdf9ec32e3f70a14703c1fca7d9920067
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-13 12:41:01 -0400

    Update index.php

--- cosmosquest/index.php
+++ cosmosquest/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Cosmos Quest';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hapcimnkajnblcnialldkimfgaekaepm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
