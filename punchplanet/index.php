a4faa98cef7cda85aa310b69f2763370c735ae30
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-13 13:44:06 -0400

    Update index.php

--- punchplanet/index.php
+++ punchplanet/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Punch Planet';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lgecgibliilafniecmhaiijpdbhkneng';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
