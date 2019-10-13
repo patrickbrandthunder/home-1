e59d2abd6cfdf8da3a5aadeb16b902c76e70088b
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-13 14:54:51 -0400

    Update index.php

--- bleaks/index.php
+++ bleaks/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bleak Sword';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'eholnmohacnmhfmgmcdeplcmbflfabaf';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
