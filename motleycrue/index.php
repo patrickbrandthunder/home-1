dcbdb074128bac27a467ee636ddd1652686f3468
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-14 08:24:40 -0400

    Update index.php

--- motleycrue/index.php
+++ motleycrue/index.php
@@ -2,7 +2,7 @@
 $title = 'Motley Crue';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'jpeicnccfbjfjkchlhbliphbkpfbdbof';
-$searchExtensionID = '';
+$searchExtensionID = 'obbijacdmdkfkhdgcpncilfbhfenkjme';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
