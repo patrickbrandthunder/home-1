e641475ddd2ee7c183f0e1e392c765e077b0dfe9
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-14 10:45:42 -0400

    Update index.php

--- nhl20/index.php
+++ nhl20/index.php
@@ -3,7 +3,7 @@
 $title = 'NHL 20';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ohadnokhbgkeffdplablpmeaclnajlci';
-$searchExtensionID = '';
+$searchExtensionID = 'kohebpdcefnodhckafpmdjcaknbdocki';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
