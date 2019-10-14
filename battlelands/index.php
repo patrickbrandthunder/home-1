6d5af57f66f7bdec66739c5585e700b50b188da4
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-14 09:45:09 -0400

    Update index.php

--- battlelands/index.php
+++ battlelands/index.php
@@ -2,7 +2,7 @@
 $title = 'Battlelands Royale';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'enabmmgoplkhhoofkbcgkbdfneffgefd';
-$searchExtensionID = '';
+$searchExtensionID = 'aopicggjecifcpckiajaoefifcopbmhi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
