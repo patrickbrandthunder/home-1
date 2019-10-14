2e483ce57baa7855096c8b2a9aa51f39ea799ee0
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-14 09:54:35 -0400

    Update index.php

--- blackpurple/index.php
+++ blackpurple/index.php
@@ -2,7 +2,7 @@
 $title = 'Black Purple Shards';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'kjgodipmpmghghampkfbidjmdofmongj';
-$searchExtensionID = '';
+$searchExtensionID = 'cmklebpjdlnfedcojegafbaghcdfcgdl';
 $tid = basename(__DIR__);
 $testSearch2  = true;
 include('../homepage.php');
