23253f7225c5eabf877273b20b6864507001ba2a
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-14 08:04:16 -0400

    Update index.php

--- bobross/index.php
+++ bobross/index.php
@@ -2,7 +2,7 @@
 $title = 'Bob Ross';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'cbepdkocbfknpoilekddogbelekmnpcm';
-$searchExtensionID = '';
+$searchExtensionID = 'akoikeefhmpceepcnjcligdifjmgjghi';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
