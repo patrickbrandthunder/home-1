21414c4b420071fe219d2d06bccf46e70955b8a3
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-14 09:46:13 -0400

    Update index.php

--- corgis/index.php
+++ corgis/index.php
@@ -2,6 +2,7 @@
 $title = 'Corgis';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'bcijiahpikgljfdmjflheeinkhpkekkj';
+$searchExtensionID = 'dddkgecodfmeenhcdbailenoaoddpfna';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
