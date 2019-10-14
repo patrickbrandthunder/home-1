c4fe0623df9acbee1bfaee7a8f0199b6dd08670e
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-14 14:37:59 -0400

    Update index.php

--- superm/index.php
+++ superm/index.php
@@ -3,7 +3,7 @@
 $title = 'SuperM';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ofkaekfjlnnfeifbdgjklokfdmkgfmig';
-$searchExtensionID = '';
+$searchExtensionID = 'jingndbogdcnklpoebockhonhpnpoili';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
