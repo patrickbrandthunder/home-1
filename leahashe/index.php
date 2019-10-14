a38ef36e08e1e680e3db2b60b973ca504833a390
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-14 08:03:51 -0400

    Update index.php

--- leahashe/index.php
+++ leahashe/index.php
@@ -2,7 +2,7 @@
 $title = 'Leah Ashe';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'phllahlfglpldjgcoehomkbngcghkken';
-$searchExtensionID = '';
+$searchExtensionID = 'okcinkbfmccncilmdgkjhkmoijippkao';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
