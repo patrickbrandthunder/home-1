f425de4db96d0354ce2b8cc17fdb4edd1702013d
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-15 09:36:14 -0400

    Update index.php

--- hedgehogs/index.php
+++ hedgehogs/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Hedgehogs';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mjfombcdcfibpeckkceoeljfakgcigjh';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
