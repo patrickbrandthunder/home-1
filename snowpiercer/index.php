c4c67d444fb036266823c5f48f63721e4827a3fc
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-15 10:02:24 -0400

    Update index.php

--- snowpiercer/index.php
+++ snowpiercer/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Snowpiercer';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jfkeahonblmlikbkjnopbjoloejdilfa';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
