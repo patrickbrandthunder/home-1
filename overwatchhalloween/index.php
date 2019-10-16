207a8da8cff5d301260e484db09e17d00d66a5c9
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-16 15:41:06 -0400

    Update index.php

--- overwatchhalloween/index.php
+++ overwatchhalloween/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Overwatch Halloween';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ngbcbghfpnmmiplgnihlgcmbphndaibk';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
