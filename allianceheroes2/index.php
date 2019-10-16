f9c4269aa5c153086646474bbc70a0fce37b8d40
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-16 10:45:17 -0400

    Update index.php

--- allianceheroes2/index.php
+++ allianceheroes2/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Alliance: Heroes of the Spire';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gjpdifhgpndehelnmlbjndhkinmpjdmg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
