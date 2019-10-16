eba37f13694180408615c5a7743388fec34d41c9
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-16 16:12:16 -0400

    Update index.php

--- questland/index.php
+++ questland/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Questland Hero Quest';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dafoogijgchckbgdfdbbbeikpgiociga';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
