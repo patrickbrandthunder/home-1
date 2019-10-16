7641b15d5b58c76bb981a88be709bd42921f2239
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-16 16:51:16 -0400

    Update index.php

--- inmost/index.php
+++ inmost/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'INMOST';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'oopfkajnalnhhldnhbbcpgeofebbomen';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
