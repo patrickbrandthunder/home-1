0f2f7fbff56da9636603063d5536590d43e37a8f
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-17 08:37:18 -0400

    Update index.php

--- ynwmelly/index.php
+++ ynwmelly/index.php
@@ -2,7 +2,7 @@
 $title = 'YNW Melly';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'hehcabkhbibpookckajnaedihbpmbnad';
-$searchExtensionID = '';
+$searchExtensionID = 'pbadgpphdpeldbjaeibbimpblkjfjalg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
