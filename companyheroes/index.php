517ea17671b5f8923039cb860cc703d27d6854e6
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-17 16:09:41 -0400

    Update index.php

--- companyheroes/index.php
+++ companyheroes/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Company of Heroes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bpbpechgkhlimpgagchhpnmkffakebld';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
