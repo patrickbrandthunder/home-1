9108a88f0c840b42928a40ff20d88d9881517027
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-17 15:48:08 -0400

    Update index.php

--- runeterra2/index.php
+++ runeterra2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Legends of Runeterra';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cihkijcolgjhkadlfjlnaodphimiakhi';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
