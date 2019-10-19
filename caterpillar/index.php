cc71a8ab33ca8d93f1bf011c903e69ce46310be2
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-19 14:09:37 -0400

    Update index.php

--- caterpillar/index.php
+++ caterpillar/index.php
@@ -3,6 +3,7 @@
 $title = 'Caterpillar Farming';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ohpalfllcedaapnidfongkckdnjappel';
+$searchExtensionID = 'jdcffgjmkboelhkciigbdamniekofeba';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
