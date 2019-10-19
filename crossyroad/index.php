9cc633b7a20157c6a3836078fc1a7f9fa6098f07
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-19 09:40:59 -0400

    Update index.php

--- crossyroad/index.php
+++ crossyroad/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Crossy Road Castle';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'makdligppoobmlbdhjafidhhfiodjjnl';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
