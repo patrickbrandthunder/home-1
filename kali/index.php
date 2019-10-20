feffc62a4708b78b3a6401c53154ad7db652fe56
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-20 15:14:34 -0400

    Update index.php

--- kali/index.php
+++ kali/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Kali Uchis';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kbdiddhaicplebennbnedmlbbdebmmcb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
