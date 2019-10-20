ddf02bcbc309c958f2ecb53e1d59bcf5110f1e95
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-20 10:09:03 -0400

    Update index.php

--- speedrush/index.php
+++ speedrush/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Adrenaline Speed Rush';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nhedopjlkplhejdbdolbommghgjlbajh';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
