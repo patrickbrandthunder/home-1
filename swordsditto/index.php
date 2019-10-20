3f2b84c3d2e18dabd9df294d5b8381aa36febfbd
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-20 14:22:33 -0400

    Update index.php

--- swordsditto/index.php
+++ swordsditto/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Swords Of Ditto';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lopchdlgnaflbjgjiicfgnfekpeddpfb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
