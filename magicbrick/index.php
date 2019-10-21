05062d8ceb8c0bbe62809e47680920b2159aa15a
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-21 10:03:32 -0400

    Update index.php

--- magicbrick/index.php
+++ magicbrick/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Magic Brick Wars';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'calccmlgjpnaldcfegnegfomlehoongc';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
