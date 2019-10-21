07c38c28f0ec823b557751f6be22fe90b7d24ba3
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-21 14:14:36 -0400

    Update index.php

--- pacmanparty/index.php
+++ pacmanparty/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Pac Man Party Royale';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ldiefnchpjbloinifepkkecnackfgcdm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
