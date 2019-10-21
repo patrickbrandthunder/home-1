d3300b9aedda33dd3071878fad588473d7a188de
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-21 10:19:16 -0400

    Update index.php

--- manifold/index.php
+++ manifold/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Manifold Garden';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ojpmclhjhplokfcmmfpokngbgokjnkjf';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
