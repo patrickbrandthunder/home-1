50126a270647a2ba93925fcb97bae2838ee9894f
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-22 10:33:12 -0400

    Update index.php

--- muvluv/index.php
+++ muvluv/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'MUV LUV';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'khodlbdicfaddgkbhhnhhdnfidpkfglo';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
