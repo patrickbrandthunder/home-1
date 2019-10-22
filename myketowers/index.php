c9185f4a962a3c2ca34007132a048e3fc327ad82
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-22 11:07:17 -0400

    Update index.php

--- myketowers/index.php
+++ myketowers/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Myke Towers';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mccckhejhcgfmgocjmlbjhcpomjaddal';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
