d9df1f94c9a591697170b265b885790d15bdcb21
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-22 16:30:13 -0400

    Update index.php

--- 24hrs/index.php
+++ 24hrs/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = '24hrs';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jdegkgflhpoeadgebpehbpnkmclgflmi';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
