f280029a8d4e25658a7e05c37db636e9c4fc5551
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-22 19:20:16 -0400

    Update index.php

--- octopath/index.php
+++ octopath/index.php
@@ -2,6 +2,7 @@
 $title = 'Octopath';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'konhebflphgecoikjjbholkijcagciml';
+$searchExtensionID = 'iicpkipjkjjicgplmncfbgmkpnajeceg';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
