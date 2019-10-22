40ad97773937b831342e8c4ff30321624d8e682e
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-22 16:39:57 -0400

    Update index.php

--- apex2/index.php
+++ apex2/index.php
@@ -2,7 +2,7 @@
 $title = 'Apex Legends Season 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'mkikdijiodbgkckbkepokagcggkockhh';
-$searchExtensionID = '';
+$searchExtensionID = 'afdphlpchedbkafdnoaocaalkhilmngf';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
