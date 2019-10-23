434969acc41af576e55d75cdcb4abf13abae4d69
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-23 08:49:07 -0400

    Update index.php

--- playboi/index.php
+++ playboi/index.php
@@ -2,7 +2,7 @@
 $title = 'Playboi Carti';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'eppopcepfndpfogepdkpdclekodlfoih';
-$searchExtensionID = 'fkjpccjaidcomaoclghpaoondiafcnci';
+$searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
