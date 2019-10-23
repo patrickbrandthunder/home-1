30070ff15ffbc84b7ea3d166ca3c88ec8cfcf062
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-23 15:02:35 -0400

    Update index.php

--- lolwildrift2/index.php
+++ lolwildrift2/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'League of Legends: Wild Rift';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ghlnpnbmhibcdpikhbkjbckdpnckdbmp';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
