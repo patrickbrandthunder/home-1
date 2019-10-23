8d3b63a7373d45a3152f38ed01e00f3bfa8281a1
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-23 08:49:17 -0400

    Update index.php

--- playboi2/index.php
+++ playboi2/index.php
@@ -2,7 +2,7 @@
 $title = 'Playboi';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'meihjpgfdfjlimcpegpolbblcjifcomf';
-$searchExtensionID = 'fkjpccjaidcomaoclghpaoondiafcnci';
+$searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
