b087c76f2544a93c6f2822c176a9cb45614970c4
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-23 10:07:25 -0400

    Update index.php

--- farruko/index.php
+++ farruko/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Farruko';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nfgnlpiamjabnhokdpokppbckafogepm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
