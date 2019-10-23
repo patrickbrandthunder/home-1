882616f67744dc38294c25ce74e7a83c1a2b6913
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-23 12:40:15 -0400

    Update index.php

--- ballastic/index.php
+++ ballastic/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Ballastic Baseball';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gfipffldfnedhckghjbiaidibcbngigd';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
