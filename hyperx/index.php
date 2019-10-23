2e911a16ca4b410880b655c28b66aedd87a2543e
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-23 08:40:11 -0400

    Update index.php

--- hyperx/index.php
+++ hyperx/index.php
@@ -2,7 +2,7 @@
 $title = 'HYPERX';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'agkbgnekpdplclclopfkpnmocahidodi';
-$searchExtensionID = '';
+$searchExtensionID = 'aeeflmagljcebjlfcgpoippgncpkdmlj';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
