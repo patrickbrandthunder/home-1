a493e8223fe9f0c361e7cae15f32a35d1b4aa76b
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-23 10:17:30 -0400

    Update index.php

--- nodal/index.php
+++ nodal/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Christian Nodal';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ifnckmhiaenbfdjpclonfcbamlcmmlof';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
