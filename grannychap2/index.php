ee6bf69e893db83b743f5185387980a6959a311b
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-24 08:28:48 -0400

    Update index.php

--- grannychap2/index.php
+++ grannychap2/index.php
@@ -4,5 +4,6 @@ $title = 'Granny Chapter 2';
 $extensionID = 'gddalcehkkjjldidljeeakjommmajpaj';
 $searchExtensionID = 'eaajnjineiclfocnpmedeilmipbnjkpk';
 $tid = basename(__DIR__);
+$testSearch2 = true;
 include('../homepage.php');
 ?>
