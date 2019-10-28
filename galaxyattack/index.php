75c45ac9c412ba99b4947e19d6442d2086353855
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-28 08:03:08 -0400

    Update index.php

--- galaxyattack/index.php
+++ galaxyattack/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Galaxy Attack Space Shooter';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mmnijjjlmdgeppihlmdncklhobbhfcjd';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
