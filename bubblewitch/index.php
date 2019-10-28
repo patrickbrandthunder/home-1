01c13efab3a2d7ac2756eef13c2cce735d28120c
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-28 08:23:40 -0400

    Update index.php

--- bubblewitch/index.php
+++ bubblewitch/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Bubble Witch 3';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ddncknjkbglibbbdnohegjnfmfednoam';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
