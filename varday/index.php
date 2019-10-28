a74fca9d3a71fd2e94aac4342cc262df872a6783
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-28 09:58:51 -0400

    Update index.php

--- varday/index.php
+++ varday/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Various Daylife';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'commmmedcohhjjdmciaieodihffpmcnj;
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
