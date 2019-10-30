26ef89f1b2754543cd3e5a6fd792faba820635bd
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-30 10:26:16 -0400

    Update index.php

--- discoe/index.php
+++ discoe/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Disco Elysium';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'akfhoiibfdjbomkdkbcncildmdobndpm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
