e224b95ac1cd72df4c7deb0f131223428cbaf2ce
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-30 10:23:34 -0400

    Update index.php

--- fortnitechapter2/index.php
+++ fortnitechapter2/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Fortnite Chapter 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lmiafflnopekccghjjglplhogbclgela';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
