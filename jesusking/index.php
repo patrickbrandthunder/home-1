fe568006280e4cf19b27decf97c133edc3e55f3f
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-30 10:37:06 -0400

    Update index.php

--- jesusking/index.php
+++ jesusking/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Jesus is King';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'aciigbaabnojobfaphlegjgnokjcikkf';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
