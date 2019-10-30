6d038886fa42a40975ce3a52ac264d68d0d6e33e
pjmurphy777 <pjmurphy77@gmail.com>
2019-10-30 16:48:57 -0400

    Create index.php

---
+++ ladytramp/index.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'Lady And The Tramp';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
