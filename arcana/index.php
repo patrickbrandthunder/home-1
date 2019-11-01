06c549774ae10fc69f5bfa42aff5e523386a4107
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-01 11:57:53 -0400

    Update index.php

--- arcana/index.php
+++ arcana/index.php
@@ -3,6 +3,7 @@ $title = 'The Arcana';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'bhpafldbfpnppjajiaffklpadkjfcend';
 $searchExtensionID = 'khfiihmapapmccbodkcaodjfbmkinpfp';
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
