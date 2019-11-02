c99a486b11162ff6cfd32f9fb24d4555a6169a4b
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-02 19:20:48 -0400

    Update index.php

--- pocketgod/index.php
+++ pocketgod/index.php
@@ -3,6 +3,7 @@ $title = 'Pocket God';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = '';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
