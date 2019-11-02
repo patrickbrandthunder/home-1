aad6a4bff8702bb09cfac6f60b07c817a1f96e03
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-02 08:21:16 -0400

    Update index.php

--- languinis/index.php
+++ languinis/index.php
@@ -3,6 +3,7 @@ $title = 'Languinis';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'dkfmfpmamahnedbipemnjihpdgmcjifa';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
