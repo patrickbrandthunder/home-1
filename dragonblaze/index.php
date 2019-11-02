4f0a213ee50bf5e0eccb7d4c676eca837f25f748
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-02 19:53:24 -0400

    Update index.php

--- dragonblaze/index.php
+++ dragonblaze/index.php
@@ -4,6 +4,7 @@ $title = 'Dragon Blaze';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'hepmajlmfijpihldbidniecpdpeocobe';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
