c868bac61c9c22d4d337732e7a0f2994f45bf4c2
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-02 18:37:29 -0400

    Update index.php

--- nbayoungboy/index.php
+++ nbayoungboy/index.php
@@ -3,6 +3,7 @@ $title = 'NBA Youngboy';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'fbkgdpiefjobjhedpbapdihfnehokjfm';
 $searchExtensionID = 'coecopoaaciaaehlidhnoagcpidhiloh';
+$testSearch2 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
