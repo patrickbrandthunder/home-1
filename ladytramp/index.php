4bd539a96eba9dfad579ea79154d7c2cd3bd4312
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-02 12:30:23 -0400

    Update index.php

--- ladytramp/index.php
+++ ladytramp/index.php
@@ -3,6 +3,7 @@ $title = 'Lady And The Tramp';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'jmneellhjgnghjckjbiapnfnmdnjelbi';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
