4f0a7f5e032951792bde774b3df3b4d8ba7a907e
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-02 08:58:10 -0400

    Update index.php

--- yoshicrafted/index.php
+++ yoshicrafted/index.php
@@ -3,6 +3,7 @@ $title = 'Yoshi Crafted World';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'eokldkcnlcflkafpmpdfbakcldjdfdoh';
 $searchExtensionID = 'ibchlhbbojdjcoancpilkbocbdnbmieh';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
