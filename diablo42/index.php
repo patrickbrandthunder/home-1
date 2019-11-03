81aba06b140c6104e782553f956b2a2fd5e1f28d
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-02 20:00:10 -0400

    Update index.php

--- diablo42/index.php
+++ diablo42/index.php
@@ -1,6 +1,6 @@
 $title = 'Diablo 4';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jimeobmapnjkmpcflfnacjkjdeojcgip';
 $searchExtensionID = '';
 $bingSearch = true;
 $tid = basename(__DIR__);
