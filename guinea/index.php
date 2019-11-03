7f618e96df121632adc74f287a203df290daa6b8
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-03 07:53:34 -0500

    Update index.php

--- guinea/index.php
+++ guinea/index.php
@@ -3,6 +3,7 @@ $title = 'Guinea Pigs';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'gpephcgbdhdcoinihbjghahhggcihhpm';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
