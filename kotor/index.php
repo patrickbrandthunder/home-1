e16b23298383fe60aaa894fa12f218bdee440ae9
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-03 15:51:26 -0500

    Update index.php

--- kotor/index.php
+++ kotor/index.php
@@ -3,6 +3,7 @@ $title = 'Star Wars Kotor';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'majalohopicaabmbfjphoejpcehefmfk';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
