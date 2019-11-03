e7b63e9c58c27d7b139d03ba559b18b3ea0426b5
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-03 08:13:19 -0500

    Update index.php

--- liltjay/index.php
+++ liltjay/index.php
@@ -3,6 +3,7 @@ $title = 'Lil Tjay';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'jdplkkjojffdnompjdepdpgnecnhgacm';
 $searchExtensionID = 'oonecegidpifahjpmnnmdjhhenemipkm';
+$testSearch2 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
