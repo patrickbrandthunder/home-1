060e7453a0ddc9c22d722b287d005064d0974173
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-03 08:28:11 -0500

    Update index.php

--- jaydayoungan/index.php
+++ jaydayoungan/index.php
@@ -3,6 +3,7 @@ $title = 'JayDaYoungan';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'miiniiephfmngakkmkkpofpebdjfpeib';
 $searchExtensionID = 'hjnpopcocndepfelaompchjanipoijhp';
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
