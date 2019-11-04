753f94b09bbac30709726bc87ce571e8c0a0016b
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-04 08:38:36 -0500

    Update index.php

--- harvester/index.php
+++ harvester/index.php
@@ -3,6 +3,7 @@ $title = 'Harvester International';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'jiaemglbfoghdiaibbmkjemfbpnhpcho';
 $searchExtensionID = 'lgiimdbonnmdnpjjpgdcoaiomfehmlik';
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
