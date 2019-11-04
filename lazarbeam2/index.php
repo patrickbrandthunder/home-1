bb3c3be781fc5b0203aced4f3d11a53e1b20a2bf
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-04 08:38:00 -0500

    Update index.php

--- lazarbeam2/index.php
+++ lazarbeam2/index.php
@@ -3,6 +3,7 @@ $title = 'Lazar Beam 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'cpjadiiighmadfgfielfcdpakgighech';
 $searchExtensionID = 'olofjponfflnnnepakmjghfgbjehjfgn';
+$testSearch2 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
