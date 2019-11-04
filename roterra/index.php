ccf432db0e0a1b8ede814971cc1d9ecf1364a137
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-04 10:13:09 -0500

    Update index.php

--- roterra/index.php
+++ roterra/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Roterra Extreme';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'imepahgofaedgcplniambmgelopjcgfc';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
