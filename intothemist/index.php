1af2364c77e14607f2355f732fb0425bcf0703bb
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-05 09:25:23 -0500

    Update index.php

--- intothemist/index.php
+++ intothemist/index.php
@@ -3,6 +3,7 @@ $title = 'Into THe Mist';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'dlaclmmkgakhfgehfanfedhbfekicdji';
 $searchExtensionID = 'hgmdkggaclnchnbfgnaflfgflfgmdblc';
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
