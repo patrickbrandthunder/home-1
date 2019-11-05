96fc7d9f38aad96f52d92852d1a3096380eef72f
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-05 16:36:18 -0500

    Update index.php

--- snot/index.php
+++ snot/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'SNOT RAPPER';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ebbdpbaobpakoocngmmpkpnjpihbekch';
 $searchExtensionID = '';
 $bingSearch = true;
 $tid = basename(__DIR__);
