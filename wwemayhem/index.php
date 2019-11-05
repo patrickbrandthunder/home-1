38f1428823435b91e013a2912edb67cf73c1a81b
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-05 07:58:45 -0500

    Update index.php

--- wwemayhem/index.php
+++ wwemayhem/index.php
@@ -3,7 +3,7 @@ $title = 'WWE Mayhem';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'oikifchokenekjdkndcdpoccndhealkm';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
