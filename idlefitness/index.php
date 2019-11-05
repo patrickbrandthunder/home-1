f62d37f86e93f16abae3d3ef0eb548facbaac559
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-05 09:09:55 -0500

    Update index.php

--- idlefitness/index.php
+++ idlefitness/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Idle Fitness Gym Tycoon';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bndgchbdoegllggpdlapbdogefhfnleb';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
