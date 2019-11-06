f1922d76329ae5136c061bb9f60693eb9d7318ad
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-06 07:54:51 -0500

    Update index.php

--- wandavision/index.php
+++ wandavision/index.php
@@ -4,7 +4,6 @@ $title = 'WandaVision';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ejaiadncnlckjijbmmbcaadcjlgcnajh';
 $searchExtensionID = '';
-$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
