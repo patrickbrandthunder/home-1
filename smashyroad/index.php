017addda49b5bfc1b85083f9333c8f8f3f4e337f
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-06 07:59:27 -0500

    Update index.php

--- smashyroad/index.php
+++ smashyroad/index.php
@@ -4,7 +4,6 @@ $title = 'Smashy Road';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'lhkilbcikfohphbmdoaiaofaacadgigf';
 $searchExtensionID = '';
-$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
