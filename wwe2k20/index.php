c113a0ad4701fa2cdf51d7cee595033e247eaab8
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-07 09:40:35 -0500

    Update index.php

--- wwe2k20/index.php
+++ wwe2k20/index.php
@@ -3,6 +3,7 @@ $title = 'WWE 2K20';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'lidkcdooldacloakbdobjebmocmjhfec';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
