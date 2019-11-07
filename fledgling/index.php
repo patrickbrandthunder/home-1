2a430beab1da0578007f02689f75e346ce083fe9
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-07 12:18:33 -0500

    Update index.php

--- fledgling/index.php
+++ fledgling/index.php
@@ -2,6 +2,7 @@
 $title = 'Fledgling Heroes';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'mmejpdcikecdocdgenicaongmkgoohjb';
+$bingSearch = true;
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
