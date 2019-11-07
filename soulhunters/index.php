8105efd64f6907592ac55c62d1c1c01f7d83a352
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-07 09:42:46 -0500

    Update index.php

--- soulhunters/index.php
+++ soulhunters/index.php
@@ -3,6 +3,7 @@ $title = 'Soul Hunters';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'megimnabdinjdcfnchobnibaeofccnlj';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
