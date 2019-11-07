7d5b4ff0e4f18b5b02bb71222e85c2fb75bd51d2
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-07 08:40:56 -0500

    Update index.php

--- sheff/index.php
+++ sheff/index.php
@@ -3,7 +3,7 @@ $title = 'Sheff G';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'neafpdkpmljgcffhmhppooliinaelggj';
 $searchExtensionID = '';
-$testSearch2 = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
