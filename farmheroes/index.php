c20f650848251faec17dd7b815ead18a1dc0c0dc
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-07 14:52:00 -0500

    Update index.php

--- farmheroes/index.php
+++ farmheroes/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Farm Heroes Super Saga';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bilpomoddagmnkdncbpgdhcpblhpgpma';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
