594bd4268d379663a088afc72d3545af35dedeb4
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-07 09:36:09 -0500

    Update index.php

--- avengersendgame/index.php
+++ avengersendgame/index.php
@@ -2,7 +2,8 @@
 $title = 'Avengers Endgame';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'gjckacgfnncmdaffnakpnldjlcpbpjhi';
-$searchExtensionID = 'eipbkbopogkfobcjcjogeneafljgmcnm';
+$searchExtensionID = 'eipbkbopogkfobcjcjogeneafljgmcnm';$bingSearch = true;
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
