a12e080e9fa4fd504a5204c2a2f6a9f16ef41360
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-07 09:27:05 -0500

    Update index.php

--- rickross/index.php
+++ rickross/index.php
@@ -2,6 +2,7 @@
 $title = 'Rick Ross';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'hckddpfolcjdljjcfelggekhfppdhhbg';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
