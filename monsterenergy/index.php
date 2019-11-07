a3895ec2cb546439d8d685105fe063b84f463f4f
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-07 09:44:51 -0500

    Update index.php

--- monsterenergy/index.php
+++ monsterenergy/index.php
@@ -3,6 +3,7 @@ $title = 'Monster Energy';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'jmijfcjibeealcleocojbjmmmfigidgo';
 $searchExtensionID = 'dphhhjanabegegfdkkmlmmjahepogegp';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
