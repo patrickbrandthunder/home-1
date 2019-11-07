31dd02bb582658951d26d5e5a2fe9b47749ba7cb
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-07 08:49:32 -0500

    Update index.php

--- creativedestruction/index.php
+++ creativedestruction/index.php
@@ -3,7 +3,7 @@ $title = 'Creative Destruction';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'pbnafmcfgdnifkmaococgefdpolgogej';
 $searchExtensionID = 'lbolpkgihnhlhflmoehaneoeifdimmde';
-$testSearch2 = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
