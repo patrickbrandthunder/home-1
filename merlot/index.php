b7b69e0b57c985aa412d2055382d2ebdcc37969c
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-07 09:20:12 -0500

    Update index.php

--- merlot/index.php
+++ merlot/index.php
@@ -2,6 +2,7 @@
 $title = 'Merlot';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'gljmnjjcalbkjfkpcgkgfkmplhnajjod';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
