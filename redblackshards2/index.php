9bf34dc15f7d412e884f924bb44aa979d94bea7f
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-07 08:48:14 -0500

    Update index.php

--- redblackshards2/index.php
+++ redblackshards2/index.php
@@ -3,7 +3,7 @@ $title = 'Red Black Shards 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'clikgakbjeilhekilfdggijmfhcgnipl';
 $searchExtensionID = 'gnknfmabohlenbkaoddbmbgojenempbb';
-$testSearch2 = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
