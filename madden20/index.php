13089ffbf5c5a5c9bb0434b5acd0e487873a6cfd
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-07 08:51:30 -0500

    Update index.php

--- madden20/index.php
+++ madden20/index.php
@@ -3,7 +3,7 @@ $title = 'MADDEN NFL 20';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'dlehckffnbnpgieimfjnamgbnojdkchk';
 $searchExtensionID = 'pllgcpambeabdmnghecgcenpcnakplhg';
-$testSearch2 = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
