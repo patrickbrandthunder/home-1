98677e79e05c39a8f0428ea7e0d75f6de3b3046f
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-07 09:32:32 -0500

    Update index.php

--- edithf/index.php
+++ edithf/index.php
@@ -3,6 +3,7 @@ $title = 'What Remains of Edith Finch';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'jkcigghmdajckbgkocpocoddipipbkcg';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
