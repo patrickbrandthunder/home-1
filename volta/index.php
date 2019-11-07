5931b95e83eb052949671170f08e1fbaabea8b6c
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-07 09:29:43 -0500

    Update index.php

--- volta/index.php
+++ volta/index.php
@@ -2,6 +2,7 @@
 $title = 'FIFA 20 VOLTA';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'cbajbhndpdmpaaggmmadckaagcggmlpk';
+$bingSearch = true;
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
