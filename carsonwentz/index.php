e0593f7434296d2404863c34e333c46f2c8a5586
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-08 11:15:39 -0500

    Update index.php

--- carsonwentz/index.php
+++ carsonwentz/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Carson Wentz';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'njldiejaaiiojmedmcejlalmiallblbk';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
