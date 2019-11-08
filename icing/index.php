97f7ca8ae9dd468bd19321e5b8ecb435710aa3f4
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-08 07:51:54 -0500

    Update index.php

--- icing/index.php
+++ icing/index.php
@@ -4,7 +4,7 @@ $title = 'Icing On The Cake';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'omiinklkbljeigcgnhbnkefmiknpkece';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
