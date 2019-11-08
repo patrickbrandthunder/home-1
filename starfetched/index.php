411046553c2a0e6cd8019d6348d0513d98225a34
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-08 11:17:19 -0500

    Update index.php

--- starfetched/index.php
+++ starfetched/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Star Fetched';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'faighnbmecoblfpifedoljklgkfnfide';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
