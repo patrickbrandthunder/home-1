0e71913ae6ec9c4526ea6a24dc08c1f04927b895
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-08 07:40:59 -0500

    Update index.php

--- nioh2/index.php
+++ nioh2/index.php
@@ -3,7 +3,7 @@ $title = 'NIOH 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'capcdijfefgpnbannbgolollaejcnjjm';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
