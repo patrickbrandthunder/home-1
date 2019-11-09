9c663f28f4e4ecaad37d0faa461e82fd4f48ab31
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-09 08:34:45 -0500

    Update index.php

--- diablo4/index.php
+++ diablo4/index.php
@@ -4,7 +4,7 @@ $title = 'Diablo 4';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'eojolagkbelpgdonciocgomlpaaeflij';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
