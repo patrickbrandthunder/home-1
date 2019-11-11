5064d894685075a216b0a4f3ae22178f435af991
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-11 08:31:44 -0500

    Update index.php

--- zombieland/index.php
+++ zombieland/index.php
@@ -3,7 +3,6 @@ $title = 'Zombieland Double Tapper';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'cbgfmlkcodmlkgeakccnpbdhghhfghon';
 $searchExtensionID = '';
-$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
