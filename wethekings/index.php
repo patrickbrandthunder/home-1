91e06fb4c536c2b52b2d88bff79a80fbf2587eec
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-11 10:10:00 -0500

    Create index.php

---
+++ wethekings/index.php
@@ -0,0 +1,9 @@
+
+<?php
+$title = 'We The Kings';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
