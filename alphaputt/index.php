e4ba7ce654a4dd627b7c846b1879fc16e44c9677
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-11 16:41:56 -0500

    Update index.php

--- alphaputt/index.php
+++ alphaputt/index.php
@@ -2,8 +2,9 @@
 <?php
 $title = 'Alphaputt';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'klgdfbdocebmbnklgpjbndonideohidi';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
