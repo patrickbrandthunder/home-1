1ab6afd0263358e8cd8170db99ddbc199c3a7e6f
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-11 10:05:02 -0500

    Update index.php

--- runefactory4/index.php
+++ runefactory4/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Rune Factory 4';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fogjcklamikgbmdnkgomeiacmlebiome';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
