0d861cf8087a55129b9dc029cd2696e83945e7a5
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-11 08:33:15 -0500

    Update index.php

--- towaga/index.php
+++ towaga/index.php
@@ -3,7 +3,6 @@ $title = 'Towaga';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'fmlanjejbjbokkdfidkmigobbffimacc';
 $searchExtensionID = '';
-$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
