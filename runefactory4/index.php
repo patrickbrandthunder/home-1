30fe64152a0fbbdf4e7287277f56002ab42961f1
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-11 10:01:21 -0500

    Create index.php

---
+++ runefactory4/index.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'Rune Factory 4';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
