1a981f82ba6fc18a401edd6b9bec5dd787258dc4
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-12 07:48:19 -0500

    Update index.php

--- scrosby/index.php
+++ scrosby/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Sidney Crosby';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kipcijgndgpfndceomofegioahjblgai';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
