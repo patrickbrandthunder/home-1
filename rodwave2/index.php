06d6d00524d85c8a0596733373be61084d9cc8e3
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-12 09:05:23 -0500

    Update index.php

--- rodwave2/index.php
+++ rodwave2/index.php
@@ -3,7 +3,7 @@ $title = 'Rod Wave';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'bjpkeecnbjkbjncahlimifcodhbfmnkm';
 $searchExtensionID = 'bccdkomplmbaigimibngkibleipmbhaj';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
