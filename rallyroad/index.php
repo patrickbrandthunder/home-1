3b8272658d09f3b7229b6db17b7afb5ed61383b9
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-12 11:00:01 -0500

    Update index.php

--- rallyroad/index.php
+++ rallyroad/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Rally Road';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ngodmnpjdpdabnecnmicacigmpcgaold';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
