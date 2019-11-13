0ebac6845fb5e6ec5dde3ffedfcf2567a8f5cd56
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-13 13:20:23 -0500

    Update index.php

--- epicseven/index.php
+++ epicseven/index.php
@@ -2,7 +2,7 @@
 $title = 'Epic Seven';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'dkbogahhhmheifkkdaemkaiilgnenenm';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
