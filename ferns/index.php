8ae5cb6881250b4fa4d808c46ac6293905266cfa
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-14 08:01:57 -0500

    Update index.php

--- ferns/index.php
+++ ferns/index.php
@@ -3,6 +3,6 @@ $title = 'Ferns';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'bnebobgphlkomjfjdbhcpgliekjphhkb';
 $tid = basename(__DIR__);
-$bingSearch = true;
+$testSearch3 = true;
 include('../homepage.php');
 ?>
