fcfee6c281cc79606420f04c8fc2e9a2e595cee2
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-14 15:19:18 -0500

    Update index.php

--- bakermayfield/index.php
+++ bakermayfield/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Baker Mayfield';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'epjjmjckfmnimeaihocedidjiogebpco';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
