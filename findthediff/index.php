fae974ad43df079251d16020b77e0eea1f1ddc14
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-14 08:06:39 -0500

    Update index.php

--- findthediff/index.php
+++ findthediff/index.php
@@ -2,7 +2,7 @@
 $title = 'Find The Difference';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'dfbcnakjlmebkpenakchkhnodekndame';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
