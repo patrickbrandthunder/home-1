29267433d1bf2dfb0a3dde19b93b0f35bdb9a13d
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-14 15:10:38 -0500

    Update index.php

--- supermegamini/index.php
+++ supermegamini/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Super Mega Mini Party';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'phdechlloioofocchdobbjjbeadkipam';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
