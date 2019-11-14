3b19096755d8ca0b11cc9707a27e61020fbfb6a0
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-14 08:06:55 -0500

    Update index.php

--- eveecho2/index.php
+++ eveecho2/index.php
@@ -3,7 +3,7 @@ $title = 'EVE Echoes';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'nmncjnbffbdejmjignhccmpjehkjbaie';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
