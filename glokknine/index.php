470452e2cbbf8d02a12d7dde022896c15f8afc0b
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-15 10:48:02 -0500

    Update index.php

--- glokknine/index.php
+++ glokknine/index.php
@@ -1,7 +1,8 @@
 <?php
 $title = 'Glokknine';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bhbcgcjjaabdemlbgohofdliglpkbnod';
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
