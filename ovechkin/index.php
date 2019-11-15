1d7bbc65bff23fcbb81af6f1e2edbfda06abcd62
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-15 08:42:38 -0500

    Update index.php

--- ovechkin/index.php
+++ ovechkin/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Alexander Ovechkin';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ndjkdhkhhignbklncaifcpafjkihjgea';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
