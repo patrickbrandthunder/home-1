2b20fc486d5caf965f0684d057c1596b66f1f4ad
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-15 14:57:52 -0500

    Update index.php

--- legend11/index.php
+++ legend11/index.php
@@ -2,7 +2,7 @@
 $title = 'Legendary Eleven';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'felmofhflkabiacocjhomkgdaoodncjh';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
