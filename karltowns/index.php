69c9bcb4ca024b15932b2b1c34c58f2d2c60d395
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-18 08:45:36 -0500

    Update index.php

--- karltowns/index.php
+++ karltowns/index.php
@@ -3,7 +3,7 @@ $title = 'Karl Anthony Towns';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'deiiinnpaghgmpkkbaijidfmfbbjcjgh';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
