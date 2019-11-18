62370ae1ca583b6fbe54dff274a20bd881fc09c0
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-18 08:46:54 -0500

    Update index.php

--- wethekings/index.php
+++ wethekings/index.php
@@ -4,7 +4,7 @@ $title = 'We The Kings';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'bbmjfpojimndnjobmndegiejjjkipaen';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
