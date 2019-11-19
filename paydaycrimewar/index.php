8a93fc40292bebfee5373c96b1f888232ecfbdc4
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-19 15:27:23 -0500

    Update index.php

--- paydaycrimewar/index.php
+++ paydaycrimewar/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Payday Crime War';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dcdngccbaphipdljjhjjdmpfaiapmgeh';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
