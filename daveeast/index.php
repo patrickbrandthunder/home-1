357db726150ce07491077fac78bd67fc297807e2
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-19 15:07:30 -0500

    Create index.php

---
+++ daveeast/index.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'Dave East';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
