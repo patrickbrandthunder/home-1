f5a1e41955c68f8192c350787100a71155d3c2a1
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-19 14:41:23 -0500

    Update index.php

--- rebelracing/index.php
+++ rebelracing/index.php
@@ -4,7 +4,7 @@ $title = 'Rebel Racing';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'aandodajnbkhndjnmanijfcdiklamodj';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
