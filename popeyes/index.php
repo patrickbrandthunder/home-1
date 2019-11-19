c708296dfb3e5fb9579a5a742fa5adb70e397316
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-19 14:39:57 -0500

    Update index.php

--- popeyes/index.php
+++ popeyes/index.php
@@ -3,7 +3,7 @@ $title = 'Popeyes Chicken';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'poieblapmkcmfgeiinigaehdihhpeadm';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
