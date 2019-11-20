ab2ad3d44d52756cbea153f7384c18dafa654cdc
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-20 14:10:33 -0500

    Update index.php

--- gtavice/index.php
+++ gtavice/index.php
@@ -3,6 +3,7 @@ $title = 'GTA Vice City';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'dnhgihapmilgagibkmomkdeghnijlbji';
 $searchExtensionID = 'dfjnoiejgbhdmoeabljkieohmpfhgbhh';
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
