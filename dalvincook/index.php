1f5b5c5d6230562bf64692f3517c754f46aa581b
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-20 14:08:54 -0500

    Update index.php

--- dalvincook/index.php
+++ dalvincook/index.php
@@ -3,7 +3,7 @@ $title = 'Dalvin Cook';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'pncajmmfgngmcnbeddiiobgliolddaef';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
