24eba05a1f4fc357774afb4a22c08706f2f3362a
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-20 10:15:21 -0500

    Update index.php

--- chevy/index.php
+++ chevy/index.php
@@ -3,6 +3,7 @@ $title = 'Chevy';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'iafcjgjdfgaaihdfjhjlcddcofcgamif';
 $searchExtensionID = 'nhpgbhcojjdaoamjkmgchfchofeeoicc';
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
