d60a8b5cefdd808237e1001a8c4e8989454359ff
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-20 14:11:43 -0500

    Update index.php

--- prestonplays/index.php
+++ prestonplays/index.php
@@ -3,6 +3,7 @@ $title = 'Preston Plays';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'kflbiklchjampgpjijmeggncaajhgghe';
 $searchExtensionID = 'jdbaocojhcbhljbhpglkgakdeakngpcp';
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
