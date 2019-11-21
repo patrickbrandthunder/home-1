8fcec7cb01f0726e620b5c03a3f0c27433c07e32
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-21 16:05:47 -0500

    Update index.php

--- amaricooper/index.php
+++ amaricooper/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Amari Cooper';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bkdddkamdlkaadepdflmeohojcmpoacc';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
