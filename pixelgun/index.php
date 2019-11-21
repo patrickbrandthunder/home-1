a54660d05e44f538ac582009e6f27e2d74107b22
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-21 08:29:35 -0500

    Update index.php

--- pixelgun/index.php
+++ pixelgun/index.php
@@ -3,6 +3,7 @@ $title = 'Pixel Gun 3D';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'cfclkpebehhmkgpkpghmacaoaiaidcbd';
 $searchExtensionID = 'fmldnakmikmhpnimmeehihogfbppebof';
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
