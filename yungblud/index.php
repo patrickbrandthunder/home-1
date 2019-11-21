8cf04884d6a9b60b01fb6066a4f709cbdfa8c467
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-21 09:02:13 -0500

    Update index.php

--- yungblud/index.php
+++ yungblud/index.php
@@ -3,6 +3,7 @@ $title = 'Yungblud';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'kjkamcffponfalmgihhkmjekemjecfoi';
 $searchExtensionID = 'fedleafpbkfpanigefjbihejmgopjcpk';
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
