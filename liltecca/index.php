97d1c2897ace79c1e4b264ccb75b4ae9fea440fb
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-21 08:39:57 -0500

    Update index.php

--- liltecca/index.php
+++ liltecca/index.php
@@ -4,6 +4,7 @@ $title = 'Lil Tecca';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'felphgmbogkandgablkolnneonfkondc';
 $searchExtensionID = 'hbagojfndigkagkfabmfljaidkdgnfpo';
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
