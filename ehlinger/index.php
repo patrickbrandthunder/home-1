71b4773a4bae03d900de626c705894b690d55627
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-21 09:10:03 -0500

    Update index.php

--- ehlinger/index.php
+++ ehlinger/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Sam Ehlinger';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fkaccfafklfjoebcbajmojdopiigbljm';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
