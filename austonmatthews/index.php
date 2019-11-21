04130aadfdea7e294a89a73e0dd801d1d90c6ac1
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-21 08:26:07 -0500

    Update index.php

--- austonmatthews/index.php
+++ austonmatthews/index.php
@@ -3,7 +3,7 @@ $title = 'Auston Matthews';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'fpkeaibajbmjggnjbfnkndniodahfmge';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
