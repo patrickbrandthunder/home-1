5495505bac8a66fe7e5fa0fd9fa1612160e9dc71
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-21 09:04:11 -0500

    Update index.php

--- subaruwrx/index.php
+++ subaruwrx/index.php
@@ -3,6 +3,7 @@ $title = 'Subaru WRX';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'bbhmgpneeebiejgeglhnjkhdlkplaieh';
 $searchExtensionID = 'ieegmdjehmmljkafdjdgndghplkhadab';
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
