d674151cc08d3981703839cc5de3cc8aed35ea29
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-21 11:54:41 -0500

    Update index.php

--- andrewluck/index.php
+++ andrewluck/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Andrew Luck';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lgcipgmcobbicdaflkaobhmgenmkcani';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
