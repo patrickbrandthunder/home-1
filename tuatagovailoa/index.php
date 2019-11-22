48a2638b781313dbf42df19005e0115434e07c56
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-22 15:59:04 -0500

    Update index.php

--- tuatagovailoa/index.php
+++ tuatagovailoa/index.php
@@ -2,8 +2,9 @@
 <?php
 $title = 'Tua Tagovailoa';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'foenmjeledcfnicnanecbelapholmcje';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
