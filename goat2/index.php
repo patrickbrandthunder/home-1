b7b6f74c38b52a2b8b6b4238078c22b60dcba1f4
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-22 15:40:36 -0500

    Update index.php

--- goat2/index.php
+++ goat2/index.php
@@ -3,6 +3,7 @@ $title = 'Goat Simulator';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'igaekohpnhmibdgalilekbbihplipnmo';
 $searchExtensionID = 'inapbondfcddeikkclhfhoddgkinnoam';
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
