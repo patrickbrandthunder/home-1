1372925eca84cbb6f0578b5d255bedd08d456f7d
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-22 15:41:45 -0500

    Update index.php

--- monstax/index.php
+++ monstax/index.php
@@ -3,6 +3,7 @@ $title = 'Monsta X';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'mbmpmbjlenhpnaoagamfljadaajhgjha';
 $searchExtensionID = 'pgplfbnghjfpbjnhdmmoonibgnddhlfe';
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
