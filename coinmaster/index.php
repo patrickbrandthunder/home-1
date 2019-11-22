be82665dcdc17b267209ea05a2017a3e8a5fe7f4
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-22 15:40:47 -0500

    Update index.php

--- coinmaster/index.php
+++ coinmaster/index.php
@@ -3,6 +3,7 @@ $title = 'Coin Master';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'lbdhflfllhpalbgpihfaihgboehmegap';
 $searchExtensionID = 'folodlanokmgajkjngaablakgdonghlm';
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
