5e13c6322de8ae6061cb51533bd5ae7505667dc5
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-25 09:04:47 -0500

    Update index.php

--- lavender/index.php
+++ lavender/index.php
@@ -3,6 +3,7 @@
 $title = 'Lavender';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'dafoehicjnkoidplihcjbelollckpmco';
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
