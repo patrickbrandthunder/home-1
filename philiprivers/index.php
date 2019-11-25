fad2c55d44a6c7d8eec5f88678c105fd18a704e7
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-25 10:23:26 -0500

    Update index.php

--- philiprivers/index.php
+++ philiprivers/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Philip Rivers';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kimbldaalcdikagffammbedahhdoaaeb';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
