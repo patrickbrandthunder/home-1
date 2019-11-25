efed7e223a8822229687e96ef9d4d3b27e72f7f8
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-25 08:56:55 -0500

    Update index.php

--- damianlillard/index.php
+++ damianlillard/index.php
@@ -3,7 +3,7 @@ $title = 'Damian Lillard';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'pmclkcmclddokpknjplbgmjppakpccfe';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
