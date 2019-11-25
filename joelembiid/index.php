c0766109d8917824f264fa9c5577a1e467f31185
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-25 08:56:16 -0500

    Update index.php

--- joelembiid/index.php
+++ joelembiid/index.php
@@ -2,7 +2,7 @@
 $title = 'Joel Embiid';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'bjpopnmhjffmlmjkbncoflmfgcaimlgd';
-$bingSearch = true;
+$testSearch3 = true;
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
