a844c790885aa7099a444e5ad2b13c841820072c
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-27 16:53:58 -0500

    Update index.php

--- battlebreakers2/index.php
+++ battlebreakers2/index.php
@@ -3,7 +3,7 @@ $title = 'Battle Breakers';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'jeeiegnjddcnncimijnbijihebndlpgh';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
