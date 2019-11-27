b017acc8aabaf1040bab00b854616289d59637e9
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-27 10:16:21 -0500

    Update index.php

--- connormcdavid/index.php
+++ connormcdavid/index.php
@@ -2,7 +2,7 @@
 $title = 'Connor McDavid';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'gkgkicanhgfemkjmlecehblnecpkfipk';
-$bingSearch = true;
+$testSearch3 = true;
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
