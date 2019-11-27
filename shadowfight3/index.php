288316c2437e9c57658c467943b86abe459a9640
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-27 10:16:39 -0500

    Update index.php

--- shadowfight3/index.php
+++ shadowfight3/index.php
@@ -3,7 +3,7 @@ $title = 'Shadow Fight 3';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'hkcnajboincmpeekldkhcblcbeapidmf';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
