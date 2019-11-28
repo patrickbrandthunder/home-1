9e9d6c6b468deb81131cba9f451664bb225815eb
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-28 13:44:27 -0500

    Update index.php

--- popsmoke/index.php
+++ popsmoke/index.php
@@ -4,6 +4,7 @@ $title = 'Pop Smoke';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'pepaolaciibdopfkbnechdomiofjekio';
 $searchExtensionID = '';
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
