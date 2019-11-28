7d37dafbbf43200472df3b2c6b7bf12204780d65
pjmurphy777 <pjmurphy77@gmail.com>
2019-11-28 13:40:13 -0500

    Update index.php

--- halflifealyx/index.php
+++ halflifealyx/index.php
@@ -3,7 +3,7 @@
 $title = 'HALF LIFE ALYX';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'nonncpnddefhjmcblcpihliglaaafmjm';
-$bingSearch = true;
+$testSearch3 = true;
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
