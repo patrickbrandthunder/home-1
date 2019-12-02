baef34c33c85f31fe9d0d2586b9137b5821a6dac
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-02 09:11:14 -0500

    Update index.php

--- daveeast/index.php
+++ daveeast/index.php
@@ -3,7 +3,7 @@ $title = 'Dave East';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'cjoccogaggghhdhmheilkjhdohkaokgf';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
