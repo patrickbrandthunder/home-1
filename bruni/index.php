1353a350bf828743b9df75da70a37847f13a883d
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-02 09:44:16 -0500

    Update index.php

--- bruni/index.php
+++ bruni/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Bruni Salamander Disney Frozen 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nioalgjloaijepcjcmnikmomacmongnb';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
