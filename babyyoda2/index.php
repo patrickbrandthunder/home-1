6374d189799b0ad691c3b4f869135f1d0266e39b
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-02 16:14:30 -0500

    Update index.php

--- babyyoda2/index.php
+++ babyyoda2/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'BABY YODA';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lfljmbiodoojalajeklklhmadagklifg';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
