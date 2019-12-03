20cdd51ff3b6b2763ae5e302aa9a3a12bf388791
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-03 11:08:10 -0500

    Update index.php

--- rideoutheroes2/index.php
+++ rideoutheroes2/index.php
@@ -2,8 +2,9 @@
 <?php
 $title = 'Ride Out Heroes';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'leecpghkbfgpccohkcbahalgmgdapcnc';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
