c38b62150286b7efabdd2f9e2b4e897303cebd92
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-04 14:41:23 -0500

    Update index.php

--- sigmatheory/index.php
+++ sigmatheory/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Sigma Theory';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'peoejejlfjpdmpbdiplbfhibblcfeaep';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
