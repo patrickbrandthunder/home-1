73134aa022de31acac6fc9c883e9d35c24064c28
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-04 09:04:01 -0500

    Update index.php

--- fournette/index.php
+++ fournette/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Leonard Fournette';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nklmgbpifjpahoipmobjpmlkhbppmlpf';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
