302a348b53349a62d24d111ada705ce4bc2b852e
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-04 14:46:52 -0500

    Update index.php

--- milkychance/index.php
+++ milkychance/index.php
@@ -1,7 +1,8 @@
 <?php
 $title = 'Milky Chance';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nfallehmkgedicdfomfjdkcnmiejogdl';
+$bingSearch = true;
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
