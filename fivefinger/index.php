2b930567010f25762b63740a767ed3e0584a1d52
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-04 09:12:30 -0500

    Create index.php

---
+++ fivefinger/index.php
@@ -0,0 +1,9 @@
+
+<?php
+$title = 'Five Finger Death Punch';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
