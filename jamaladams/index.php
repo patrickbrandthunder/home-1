23cc4d04500454d3e3d429c6324d6c09bf2b9517
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-05 09:56:32 -0500

    Update index.php

--- jamaladams/index.php
+++ jamaladams/index.php
@@ -3,7 +3,7 @@ $title = 'Jamal Adams';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ajahclapajohlngpoboabjaaeepcbmck';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
