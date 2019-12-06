8eedc82e43acc09e6259951bc71509bff1257726
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-05 21:06:39 -0500

    Update index.php

--- gacha/index.php
+++ gacha/index.php
@@ -3,7 +3,7 @@ $title = 'Gacha Life';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'gdhihoindbjlmjmomhbjjngkhamjfejf';
 $searchExtensionID = 'hnapokjidkifknjpfdebhfkhccnofgeo';
-$testSearch3 = true;
+$testSearch2=611
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
