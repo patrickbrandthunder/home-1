6dcc191ca84e6c751ab8258f85c085f3fd3c1b62
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-06 08:45:05 -0500

    Update index.php

--- slipknot/index.php
+++ slipknot/index.php
@@ -3,7 +3,7 @@ $title = 'SLIPKNOT';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'flkmaiablfmmecdbaijoncebkkekkgmg';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
