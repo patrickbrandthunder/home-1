7ca5b65f5acd76d9dad4f104e6a603a81a0ffc6c
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-06 08:44:53 -0500

    Update index.php

--- godsmack/index.php
+++ godsmack/index.php
@@ -3,7 +3,7 @@ $title = 'GODSMACK';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'oajbcdijlgbjdjdmleadeogjbpekbnpd';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
