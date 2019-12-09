0819db68ccc772772ee58f54995b90d6691de29c
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-09 10:43:57 -0500

    Update index.php

--- dakprescott/index.php
+++ dakprescott/index.php
@@ -3,7 +3,7 @@ $title = 'Dak Prescott';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'dbmkighbhbcpjkjhemffldgfbcljhpke';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
