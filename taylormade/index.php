55d88c3a05fa4cfef432ca0794a797a690af862f
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-10 18:47:55 -0500

    Update index.php

--- taylormade/index.php
+++ taylormade/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'TaylorMade';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'idiapboimanjfpfkbhhcjkmecjeofcfl';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
