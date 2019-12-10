6ec5858244523d042268b62e96919acc5cfa5e8b
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-10 18:54:40 -0500

    Update index.php

--- johncarlson/index.php
+++ johncarlson/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'John Carlson';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'blmkhongppibbkbopbgmfoiocgjbkcap';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
