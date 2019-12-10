9508dd9ec5e8b60411b388e86526163f0e30305e
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-10 10:42:56 -0500

    Update index.php

--- taylorhall/index.php
+++ taylorhall/index.php
@@ -2,7 +2,7 @@
 $title = 'Taylor Hall';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'gjgedaaikhgkmmobfbeililjcoaihaoi';
-$searchExtensionID = '';
+$searchExtensionID = '$bingSearch = true;';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
