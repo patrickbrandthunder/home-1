1fe4e6f2c6241a3a64390a780658bc319e17fdc5
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-10 20:26:34 -0500

    Update index.php

--- shovelknightshowdown/index.php
+++ shovelknightshowdown/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Shovel Knight Showdown';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bkkkgpdmomkiafoknegmldpomdfkieki';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
