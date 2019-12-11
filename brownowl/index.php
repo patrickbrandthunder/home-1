4a96d3e1d149dd0ded0026d7a1f4760b2766b1fd
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-10 19:51:20 -0500

    Update index.php

--- brownowl/index.php
+++ brownowl/index.php
@@ -2,7 +2,7 @@
 $title = 'Brown Owl';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'mhhclddhhkpencjiakjpnebdnempbglo';
-$bingSearch = true;
+$testSearch3 = true;
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
