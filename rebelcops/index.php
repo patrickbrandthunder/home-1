a733f21590ad2226faa5c566cd9dfc17cd678028
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-11 08:08:17 -0500

    Update index.php

--- rebelcops/index.php
+++ rebelcops/index.php
@@ -2,7 +2,7 @@
 $title = 'Rebel Cops';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ebphfccblfdchcgeghplfgoijmoolklo';
-$bingSearch = true;
+$testSearch3 = true;
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
