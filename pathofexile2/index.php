ba92a94037f4a662a111f1bdcb0ba1090baec5ac
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-11 08:08:45 -0500

    Update index.php

--- pathofexile2/index.php
+++ pathofexile2/index.php
@@ -3,7 +3,7 @@ $title = 'Path Of Exile 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ignehgmoadafjgdkjgbnialmdchgmjmc';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
