29812d08874049dfb416516fd01adc39f89cdef2
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-12 09:16:27 -0500

    Update index.php

--- lanterns/index.php
+++ lanterns/index.php
@@ -3,7 +3,7 @@ $title = 'Japanese Lanterns';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'aglhhimclnalppepkkcaknigeiifanej';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
