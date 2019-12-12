5cd3ae664bb6c1a78648b274264a34bd87c2d1e4
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-12 09:17:00 -0500

    Update index.php

--- oladipo/index.php
+++ oladipo/index.php
@@ -3,7 +3,7 @@
 $title = 'Victor Oladipo';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'knopdokhideekcfoomhachdjpjbbmlob';
-$bingSearch = true;
+$testSearch3 = true;
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
