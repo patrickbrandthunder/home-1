740623094c67552d7c7e4e2775164e537405d5c4
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-13 10:46:59 -0500

    Update index.php

--- mattryan/index.php
+++ mattryan/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Matt Ryan';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'adgedbojhgdogpiblbkooohcnjmiiggm';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
