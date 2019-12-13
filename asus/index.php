f5396ac5308a447245d0a27372dbddfb41343373
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-13 15:08:49 -0500

    Update index.php

--- asus/index.php
+++ asus/index.php
@@ -3,7 +3,7 @@ $title = 'ASUS';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'jbmdebhlohenoehpnmjhilljhbdmpiel';
 $searchExtensionID = 'lmphbmjdbmaobcfhaldoeddjpjipjlac';
-$testSearch3 = true;
+$testSearch2=632;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
