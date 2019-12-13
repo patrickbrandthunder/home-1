958d3b2954d1963660c65730c92ceac8e7e991f2
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-13 15:06:11 -0500

    Update index.php

--- diamonds/index.php
+++ diamonds/index.php
@@ -4,7 +4,7 @@ $title = 'Diamonds';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'pfnecgbhmiaglgmjojjlhcdifmbgamfg';
 $searchExtensionID = 'hkehfijcgbfjjjjbijmiodjlgeakllic';
-$testSearch3 = true;
+$testSearch2=630;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
