80af4f1318bcf79f50789637e83e0b3cdfe1db19
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-13 14:26:46 -0500

    Update index.php

--- nikejordan/index.php
+++ nikejordan/index.php
@@ -3,7 +3,7 @@ $title = 'NIKE Jordan';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'pjcmpeabekodekekcigjgpbkjhmkeibk';
 $searchExtensionID = 'dniadglaajfakibjcomibljohijefhna';
-$testSearch3 = true;
+$testSearch2=621;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
