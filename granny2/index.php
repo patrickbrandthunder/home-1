ff70981b095500861a292854d0c8c6ef08405251
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-13 22:39:44 -0500

    Update index.php

--- granny2/index.php
+++ granny2/index.php
@@ -4,6 +4,6 @@ $title = 'Granny 2';
 $extensionID = 'godmciliomdegojnlmjejmaljogeggdi';
 $searchExtensionID = 'eaajnjineiclfocnpmedeilmipbnjkpk';
 $tid = basename(__DIR__);
-$testSearch3 = true;
+$testSearch4=205;
 include('../homepage.php');
 ?>
