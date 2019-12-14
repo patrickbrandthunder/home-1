0ccff6f2b30090c1d228b56d21af51e7d39d438b
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-14 10:44:00 -0500

    Update index.php

--- tavares/index.php
+++ tavares/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'John Tavares';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hdididgmpiinegpaihakbipedcenlcpc';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
