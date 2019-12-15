0a5b790b4b2f33edd59f6b477015f1f7ebf75b9c
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-15 17:48:15 -0500

    Update index.php

--- stevenuniverse/index.php
+++ stevenuniverse/index.php
@@ -2,8 +2,9 @@
 <?php
 $title = 'Steven Universe';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mkjifonanaimmophoafdaheefhnfeked';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
