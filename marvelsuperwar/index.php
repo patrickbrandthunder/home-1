cbb8a2e0fd8a1db38335a5a7560b8a13ec4b7a64
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-15 10:11:15 -0500

    Update index.php

--- marvelsuperwar/index.php
+++ marvelsuperwar/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Marvel Super War';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'afiljjcjibapalpkjcenppndhjeiaamg';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
