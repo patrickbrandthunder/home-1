04baac0f080a52a3432420af13f40d92d4522133
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-15 09:15:25 -0500

    Update index.php

--- eichel/index.php
+++ eichel/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Jack Eichel';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'aihicgmhdifjjlnloccmejfjbemgfakh';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
