8e706765f4e5592df07160b0dec190dd1cd4c32e
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-15 08:53:04 -0500

    Update index.php

--- pastmak/index.php
+++ pastmak/index.php
@@ -2,7 +2,7 @@
 $title = 'David Pastmak';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'odeolllbfjddmfmildombjjkccmgmfbo';
-$searchExtensionID = '';
+$searchExtensionID = 'odeolllbfjddmfmildombjjkccmgmfbo';
 $bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
