d344b4907dcfce1361cd724f6b4ad0e656142fd5
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-16 11:20:16 -0500

    Update index.php

--- urbandecay/index.php
+++ urbandecay/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Urban Decay';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jkdklpndlaehceohpadhnbndmjgafmkj';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
