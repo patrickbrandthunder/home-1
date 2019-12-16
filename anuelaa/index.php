1ab618f3da536a118e5968d4bdcfff3c0780d8b6
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-16 09:44:36 -0500

    Update index.php

--- anuelaa/index.php
+++ anuelaa/index.php
@@ -3,7 +3,7 @@ $title = 'AnuelAA';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'moagopkpbilnjhlmpplnbagkhcdipfbk';
 $searchExtensionID = 'gcemifplhbpgfoeiiacojlfdchjmcgjh';
-$testSearch3 = true;
+$testSearch4=205;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
