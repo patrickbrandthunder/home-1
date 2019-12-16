b9d4df28cc21773ce0b362e72577f3daf210bc58
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-16 09:46:20 -0500

    Update index.php

--- silverado/index.php
+++ silverado/index.php
@@ -3,7 +3,7 @@ $title = 'Chevy Silverado';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ihclfiodhaofjkignbnbdkipnkongegh';
 $searchExtensionID = 'jloehpcldebklpicnopgbiohgmlcogdn';
-$testSearch2=619;
+$testSearch2=618;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
