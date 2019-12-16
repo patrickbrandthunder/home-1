3220d505f25aebdccdfc71066b180d06ac1e72b2
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-16 10:10:12 -0500

    Create index.php

---
+++ greyhound/index.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'Greyhound Dogs';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
