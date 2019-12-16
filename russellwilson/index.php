4ad8903b7b5828cfe633d1251f9f068cda192623
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-16 10:05:58 -0500

    Update index.php

--- russellwilson/index.php
+++ russellwilson/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Russell Wilson';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dkkhmdhheleifabfgddlighealpinngc';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
