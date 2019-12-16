1c6e092f272f21207cc20ef75643043cdb2a8dc0
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-16 16:48:18 -0500

    Update index.php

--- nocap/index.php
+++ nocap/index.php
@@ -3,7 +3,7 @@ $title = 'NoCap';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'djaimbdfadbhfhhjbfcnoicipgjfbljl';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
