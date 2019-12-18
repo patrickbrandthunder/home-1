c22f86cb887ec0ac6ffe3db8cd13988daff4ac25
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-18 09:10:11 -0500

    Update index.php

--- fivefinger/index.php
+++ fivefinger/index.php
@@ -4,7 +4,7 @@ $title = 'Five Finger Death Punch';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'lblohbpdmicdmbemojlgjpdkpoafhanp';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
