173279b340667f2b060bddcea49e99103e8c83ee
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-18 11:04:48 -0500

    Update index.php

--- pathofexile/index.php
+++ pathofexile/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Path Of Exile Mobile';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ghdofhoiemcmadjbomahidfjoghpojem';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
