febf8f0ac98e238c0f1624651461fdc31e6debe5
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-18 16:06:13 -0500

    Create index.php

---
+++ snackworld/index.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'Snack World: The Dungeon Crawl';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
