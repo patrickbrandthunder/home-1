eca13c8c5b50d1e742ea7ac45eeb97de7db0e3b1
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-18 16:29:03 -0500

    Update index.php

--- snackworld2/index.php
+++ snackworld2/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Snack World Dungeon Crawl';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dfcgkngdcbappkcgjeajajaioflecdlo';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
