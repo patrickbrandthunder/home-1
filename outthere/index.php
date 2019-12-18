3af874bcb7395b8035c67d2de00d37e4bfbe00a9
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-18 16:32:34 -0500

    Update index.php

--- outthere/index.php
+++ outthere/index.php
@@ -2,8 +2,9 @@
 <?php
 $title = 'Out There: Omega Edition';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jhekdbgogckmlidklgaalajhjimkkbgk';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
