b88612c6043c73160a2fdbe18cd50e758f56ce89
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-19 08:55:34 -0500

    Update index.php

--- pinggolf/index.php
+++ pinggolf/index.php
@@ -3,7 +3,7 @@ $title = 'PING Golf';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'hefamdpgolhpaiicmlghihngcbnhdjlk';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
