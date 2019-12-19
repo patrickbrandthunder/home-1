b2dd7079a4d389e8c375a6d66f235a6469a87842
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-19 08:57:10 -0500

    Update index.php

--- azzyland/index.php
+++ azzyland/index.php
@@ -3,7 +3,7 @@ $title = 'Azzyland';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'pklbhncolleaofgmdnnemfjjipfkgjdm';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
