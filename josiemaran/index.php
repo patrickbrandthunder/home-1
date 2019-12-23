d0a09935f46c3ef4c0188becc8bbac0318ed3b1b
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-23 08:57:02 -0500

    Update index.php

--- josiemaran/index.php
+++ josiemaran/index.php
@@ -3,7 +3,7 @@ $title = 'Josie Maran';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'pbgmjpojcemmkigpiaoplpndfjcoeinf';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
