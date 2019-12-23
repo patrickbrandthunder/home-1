6b16627e2d4b5bca8aa4b0e81098fc8f8d22cca2
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-23 08:55:48 -0500

    Update index.php

--- craftsman/index.php
+++ craftsman/index.php
@@ -4,7 +4,7 @@ $title = 'Craftsman';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'fjoennmdikicimbjbkldnjmmhnfenkoi';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
