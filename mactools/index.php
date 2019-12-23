796775f019cea73545aebbc50a509990feb4d25f
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-23 08:57:39 -0500

    Update index.php

--- mactools/index.php
+++ mactools/index.php
@@ -3,7 +3,7 @@ $title = 'MAC Tools';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'anaejmldcjgnmkeeindopehokbjgljdm';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
