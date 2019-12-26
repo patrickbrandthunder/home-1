c1e146d9c504ddb026a37322921201973b39f870
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-26 15:49:49 -0500

    Update index.php

--- binnington/index.php
+++ binnington/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Jordan Binnington';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'eeclcgohjjhioefhfnmaimmmhjhnaejd';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
