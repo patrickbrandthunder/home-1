44eb4b7357e7d546363684612216f396a91614a0
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-27 14:24:25 -0500

    Create index.php

---
+++ doncic/index.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'Luka Doncic';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
