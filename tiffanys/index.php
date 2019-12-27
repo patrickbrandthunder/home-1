090084e3d66b53503ba386ff4e3a507e61c3bf7c
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-27 15:01:13 -0500

    Update index.php

--- tiffanys/index.php
+++ tiffanys/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Tiffanys';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kkopdnbjdklcfklhfdjiebffjolchpjd';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
