e8e55d678e3ae1fa8bdf8b81ca1d4464899cf705
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-27 09:50:01 -0500

    Update index.php

--- tuukka/index.php
+++ tuukka/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Tuukka Rask';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kefpmbbbkkkchecgkjnckofddilamjoi';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
