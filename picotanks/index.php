4e9ffc4f96e8ab14e7d0da0ec1db6ad49b0a6def
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-27 15:32:38 -0500

    Update index.php

--- picotanks/index.php
+++ picotanks/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Pico Tanks';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jmenaoclmjddmgcebkbmfgpkcbbmaelj';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
