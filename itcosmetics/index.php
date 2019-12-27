7092abce76be85651ef364f421518c7f1dcc2850
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-27 09:40:44 -0500

    Update index.php

--- itcosmetics/index.php
+++ itcosmetics/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'IT Cosmetics';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ifncpnbiicachacindhnjlcckjhedipf';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
