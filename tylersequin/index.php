37601b945a303b34a0a7be3a185bdf436cbf9c46
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-29 09:28:14 -0500

    Update index.php

--- tylersequin/index.php
+++ tylersequin/index.php
@@ -3,7 +3,7 @@ $title = 'Tyler Sequin';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'pppekfhmpckeeocbkacighpfjmhmhkpf';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
