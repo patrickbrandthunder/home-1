6626793aa888d32ef42ed11d72d9bf85ea8c8eb3
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-30 09:26:47 -0500

    Update index.php

--- springfalls/index.php
+++ springfalls/index.php
@@ -3,7 +3,6 @@ $title = 'Spring Falls';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'jeegdbhjjpegfknjaobpbohejfadicmo';
 $searchExtensionID = '';
-$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
