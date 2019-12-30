e73a561ba2cb8f28d2658bb82f45a325f05c81a8
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-30 16:09:35 -0500

    Update index.php

--- naraka/index.php
+++ naraka/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Naraka Bladepoint';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'mjmnfdgchnkfoahlpmfpmlcgeplkkbod';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
