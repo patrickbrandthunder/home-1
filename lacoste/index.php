6516d2126aff1666a39c2ddae8444d2a593307ab
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-30 15:32:55 -0500

    Update index.php

--- lacoste/index.php
+++ lacoste/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'LaCoste';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pldooddbncjpopmgcidcnhhblolikfbd';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
