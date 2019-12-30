f2938bd23e008c6ddb23830386a3b3cda1e508b4
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-30 09:27:12 -0500

    Update index.php

--- doughty/index.php
+++ doughty/index.php
@@ -3,7 +3,6 @@ $title = 'Drew Doughty';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ffegebngonjjcdbkhldffcfhidgccpeb';
 $searchExtensionID = '';
-$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
