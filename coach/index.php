fd45756c39437e577b783f5b106d86c8cf51a8a6
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-30 09:55:58 -0500

    Update index.php

--- coach/index.php
+++ coach/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Coach';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gmbdnfjjcapklmpiibdnbhegfjcmddmn';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
