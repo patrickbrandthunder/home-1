2946c068fe3de27a0ded2acce84e68826e2f8587
pjmurphy777 <pjmurphy77@gmail.com>
2019-12-30 09:26:17 -0500

    Update index.php

--- toews/index.php
+++ toews/index.php
@@ -3,7 +3,6 @@ $title = 'Jonathan Toews';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'hmpbehgelfdfchanenicaalfcpigepah';
 $searchExtensionID = '';
-$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
