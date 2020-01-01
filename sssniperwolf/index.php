a602ce8adc6ef32a1dbc60d00d576b3481b8ffcc
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-01 10:46:22 -0500

    Update index.php

--- sssniperwolf/index.php
+++ sssniperwolf/index.php
@@ -3,7 +3,7 @@ $title = 'SSSNIPERWOLF';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ejjglmmjmejhjclbdjepogeeddhoknae';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
