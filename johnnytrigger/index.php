37270aac47019b129a9f2650e14d9804a30aebb6
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-01 18:21:59 -0500

    Create index.php

---
+++ johnnytrigger/index.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'Johnny Trigger';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
