d787230e6a336782ce90069c065e3792e46607ae
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-02 07:44:20 -0500

    Update index.php

--- dolce/index.php
+++ dolce/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Dolce Gabbana';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'clnhfeodoembghlfkfgbmmdldiolbdmf';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
