368ff4403adf45a5045183497b8811eac6e37292
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-03 10:02:09 -0500

    Update index.php

--- mrbeast/index.php
+++ mrbeast/index.php
@@ -3,7 +3,7 @@ $title = 'MrBeast';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'adonckbidpmkmfoidjnlkpiadebljcmh';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
