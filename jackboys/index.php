609012b97d47fa50434d79e2d2fd4b6b64b81a9f
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-03 14:44:48 -0500

    Update index.php

--- jackboys/index.php
+++ jackboys/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Travis Scott Jackboys';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kehdccpckemjghfbfgjkghcmpngjfpje';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
