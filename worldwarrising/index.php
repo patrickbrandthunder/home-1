1e88d3738fa00e8994c293314a05aac92e8c06c0
Michael Kaply <345868+mkaply@users.noreply.github.com>
2020-01-03 14:55:51 -0600

    Missing quotation mark

--- worldwarrising/index.php
+++ worldwarrising/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'World War Rising';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = 'dgjojpooanmjkonngjenckbgcmdhjbli;
+$extensionID = 'dgjojpooanmjkonngjenckbgcmdhjbli';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
