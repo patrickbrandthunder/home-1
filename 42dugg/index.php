dd724fd02c9951cc2eba60b5222a7142b1c5389d
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-06 14:06:37 -0500

    Update index.php

--- 42dugg/index.php
+++ 42dugg/index.php
@@ -2,6 +2,7 @@
 $title = '42 Dugg';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
