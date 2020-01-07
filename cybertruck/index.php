cee704b2505dc87fe2f27345abacbdd2eb7a1a64
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-07 10:15:15 -0500

    Update index.php

--- cybertruck/index.php
+++ cybertruck/index.php
@@ -3,7 +3,7 @@ $title = 'Tesla Cybertruck';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'bandipmliommmifhjffjlpipogiofbpa';
 $searchExtensionID = '';
-$testSearch2=616;
+$testSearch2=618;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
