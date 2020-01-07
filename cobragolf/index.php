b9f17f15a1f25090d16e9f6b750c956860db6e8f
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-07 10:58:37 -0500

    Update index.php

--- cobragolf/index.php
+++ cobragolf/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Cobra Golf';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'enpkpfgkhggkdihadnhkdbigagpjlhan';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
