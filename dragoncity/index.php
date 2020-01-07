b20cc2a14677342022417d020d6c5e8995a5ca2d
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-07 10:02:18 -0500

    Update index.php

--- dragoncity/index.php
+++ dragoncity/index.php
@@ -3,7 +3,7 @@ $title = 'Dragon City';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'fhgpghikhcnbnnbackopnadjagfgjkhe';
 $searchExtensionID = 'oihflpgmomgndfohonoihjfjdmapmdgk';
-$testSearch3 = true;
+$testSearch4=212;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
