bd3a449682fb1850c4eb14056062e6fe011ff93e
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-07 16:11:58 -0500

    Update index.php

--- mizuno/index.php
+++ mizuno/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Mizuno Golf';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'inefjpkbnejhfhlgcomnpcfcgcogeafh';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
