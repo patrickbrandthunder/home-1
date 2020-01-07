c4d4c9e217b9db7ce97752af8ff7d09413a82576
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-07 09:23:24 -0500

    Update index.php

--- spongebob/index.php
+++ spongebob/index.php
@@ -3,7 +3,7 @@ $title = 'SpongeBob Krusty Cook-Off';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'fmijpanffmefepojoijpnhdckekemiae';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
