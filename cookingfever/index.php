3c7bdd64382d43efd426cfd1fe0edffef3c00932
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-07 10:11:23 -0500

    Update index.php

--- cookingfever/index.php
+++ cookingfever/index.php
@@ -3,6 +3,7 @@ $title = 'Cooking Fever';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'malcgocpjkkmlmanpkofcdlplfkobpnl';
 $searchExtensionID = 'mjfbdmpigopmgdieifooicajgdifgdpl';
+$testSearch2=625;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
