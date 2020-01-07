ea2345b097070be21aa0645c7d97345995aef10a
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-07 16:38:51 -0500

    Update index.php

--- 2020calendar/index.php
+++ 2020calendar/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = '2020 Calendar';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'abjcaehchngekdibhehdnfpjanjaagkl';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
