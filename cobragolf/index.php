de071f5b9ab601b859fda64aaa628f1dfec2a619
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-07 10:51:07 -0500

    Create index.php

---
+++ cobragolf/index.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'Cobra Golf';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
