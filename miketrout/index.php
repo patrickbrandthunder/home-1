1dbe44ca30ab1f5c047c1a13d0cfeeb70f0a2377
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-07 14:38:32 -0500

    Update index.php

--- miketrout/index.php
+++ miketrout/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Mike Trout';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'eeobnkiodbmfcpmnimfjhnoahngiofjl';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
