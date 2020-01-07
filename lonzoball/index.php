d7fbe46808f21be49ee253ffb39c3c8f7df4f79e
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-07 14:29:57 -0500

    Update index.php

--- lonzoball/index.php
+++ lonzoball/index.php
@@ -1,7 +1,8 @@
 <?php
 $title = 'Lonzo Ball';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kkeodfbbbppheedndeomelfbbicnodok';
+$bingSearch = true;
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
