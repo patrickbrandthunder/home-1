7ed2032d7c65c49a494a3a3602a913e4dbe12f0b
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-07 09:23:43 -0500

    Update index.php

--- winterfest/index.php
+++ winterfest/index.php
@@ -3,7 +3,7 @@ $title = 'Fortnite Winterfest';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'elojnbjpnfoflolbliabamahialllkbc';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
