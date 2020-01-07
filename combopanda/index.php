6243ec7cb8a8991d73e8e04de19789e6b4eadf87
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-07 16:52:27 -0500

    Update index.php

--- combopanda/index.php
+++ combopanda/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Combo Panda';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jjpoeckadjelooppelkilffodgjifnjd';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
