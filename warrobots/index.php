427c96693cb7086c1617c0a8637d5b78bea36025
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-07 10:08:26 -0500

    Update index.php

--- warrobots/index.php
+++ warrobots/index.php
@@ -3,7 +3,7 @@ $title = 'War Robots';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'lchbnbfdooghfifkmncanemjjhfcandb';
 $searchExtensionID = '';
-$testSearch3 = true;
+$testSearch4=212;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
