9051493684e3e3732957d08bfbb68248a74d624b
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-07 16:20:36 -0500

    Update index.php

--- footballstrike/index.php
+++ footballstrike/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Football Strike';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hccdnhhcicfdmggjjojibkcckgakacng';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
