b933281481197ea5bfc211038ba77ce15665923d
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-07 14:16:24 -0500

    Update index.php

--- partynextdoor/index.php
+++ partynextdoor/index.php
@@ -2,8 +2,9 @@
 <?php
 $title = 'Partynextdoor';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kiabdnmkcjjjldcimgnbpebpncgeemja';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
