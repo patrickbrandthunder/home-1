e995bbba0c4e7bdbf3406d37aba177777f8b25e0
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-07 13:29:22 -0500

    Update index.php

--- elieen/index.php
+++ elieen/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Eileen Fisher';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cbkdjgcgjlbdcicombgelompfgeanfef';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
