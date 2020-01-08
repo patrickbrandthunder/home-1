fc63883b821b1bb7bb1453b05e6ab8eb05355cd4
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-07 19:14:46 -0500

    Update index.php

--- hotelempire/index.php
+++ hotelempire/index.php
@@ -2,8 +2,9 @@
 <?php
 $title = 'Hotel Empire Tycoon';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fccbncaejiajcbfednabpfhehmmeanlk';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
