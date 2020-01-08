05b57edd5310621480ab6eb84416f3a90a41500c
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-08 09:15:24 -0500

    Update index.php

--- snapon/index.php
+++ snapon/index.php
@@ -3,7 +3,7 @@ $title = 'Snap On';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'hpfokikhignmnemmkbbfaboamcphmine';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch2=625;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
