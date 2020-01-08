3a9c1c5dde60f3fe94862220e82734dfec089a0b
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-08 09:17:08 -0500

    Update index.php

--- tarte/index.php
+++ tarte/index.php
@@ -3,7 +3,7 @@ $title = 'Tarte';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'akpfpgknbeodgfccbddkgjnaakamcpee';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch3 = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
