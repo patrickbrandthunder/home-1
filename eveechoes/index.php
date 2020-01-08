336aa8721a21a126160e168d3d5cad3429b99ec3
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-08 09:18:56 -0500

    Update index.php

--- eveechoes/index.php
+++ eveechoes/index.php
@@ -4,7 +4,7 @@ $title = 'Eve: Echoes';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'dfpfomlgbfmigehdnfjhhdalmpihnhjd';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch4=212;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
