0f7e88d909b100025e6290360978d252dba10ed6
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-09 16:21:26 -0500

    Update index.php

--- asphalt9/index.php
+++ asphalt9/index.php
@@ -3,7 +3,7 @@ $title = 'Asphalt 9';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'dmpbpnckbogcmjhljchpcccbiecheemo';
 $searchExtensionID = 'mkodknfoblipdfibfijajnfbnjopjnaf';
-$testSearch3 = true;
+$testSearch2=635;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
