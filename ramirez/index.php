a493b57695bdab8ed77a3a8aabbe6de163a3e7d5
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-09 10:47:05 -0500

    Update index.php

--- ramirez/index.php
+++ ramirez/index.php
@@ -2,8 +2,9 @@
 <?php
 $title = 'Jose Ramirez';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kdgccebniclbkachhpiggfihbbcjmnfi';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
