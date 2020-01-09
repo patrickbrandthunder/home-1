8f6de1710862b6e133a74cfa33cf7ee6b28a1c5c
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-09 16:18:35 -0500

    Update index.php

--- bloons/index.php
+++ bloons/index.php
@@ -3,7 +3,7 @@ $title = 'Bloons TD 6';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'kegahdeaehnebgibdlikhlilldajpdnc';
 $searchExtensionID = '';
-$testSearch3 = true;
+$testSearch2=635;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
