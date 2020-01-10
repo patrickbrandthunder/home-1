ff18a61ff6b6e9da08242e5014f7676202dc2a94
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-10 09:16:58 -0500

    Update index.php

--- aesop/index.php
+++ aesop/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Aesop';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lhjjdjboiogokhklckifbjbhkikemaec';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
