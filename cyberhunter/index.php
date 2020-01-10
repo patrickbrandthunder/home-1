74340210931c3e46e09ceeb0acf0ae127ddd3bcc
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-10 13:44:00 -0500

    Update index.php

--- cyberhunter/index.php
+++ cyberhunter/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Cyber Hunter';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dcddaflfojaobjngbnjnbebmcloohnbl';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
