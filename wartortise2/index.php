fd5530db0f0d4a179d6ff54d5e4af41550923099
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-12 08:59:30 -0500

    Update index.php

--- wartortise2/index.php
+++ wartortise2/index.php
@@ -4,7 +4,7 @@ $title = 'War Tortise 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'imapfelhppnddieaenkjehcpialnfobj';
 $searchExtensionID = '';
-$bingSearch = true;
+$testSearch4=212;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
