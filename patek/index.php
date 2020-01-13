b523b389879b44d35f85afca95e78847e451e8a2
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-13 15:39:32 -0500

    Update index.php

--- patek/index.php
+++ patek/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Patek Philippe';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bghidbmgdfflolddoffliclhoanopkgf';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
