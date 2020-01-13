b7bbc255afb564471bff38cdb0b7db0e493cdee2
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-13 10:56:17 -0500

    Create index.php

---
+++ lindor/index.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'Francisco Lindor';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
