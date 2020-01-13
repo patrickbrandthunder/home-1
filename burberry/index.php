9d9cd2af701da57447bd52f29d57099acc63e21d
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-13 11:55:37 -0500

    Update index.php

--- burberry/index.php
+++ burberry/index.php
@@ -1,7 +1,8 @@
 <?php
 $title = 'Burberry';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'llcbbhelbpggfdpemhbachfgmjmfebah';
+$bingSearch = true;
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
