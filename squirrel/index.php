763cb5f40b643ce3316f914f59378ec26ad20a04
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-13 10:32:25 -0500

    Update index.php

--- squirrel/index.php
+++ squirrel/index.php
@@ -1,7 +1,8 @@
 <?php
 $title = 'Squirrel Girl';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bfcdimigmmnjlibcohfgbilcfhlkigjm';
+$bingSearch = true;
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
