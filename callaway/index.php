6b2364f0be64ece909e4b35bd8329bccb50628c5
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-13 15:22:50 -0500

    Update index.php

--- callaway/index.php
+++ callaway/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Callaway Golf';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jknhjlpiclfilmlpijckcpfkjffogcel';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
