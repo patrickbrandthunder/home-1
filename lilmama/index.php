1ef3038341d7478fa62b2a178dde346bb77cbe1c
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-13 15:07:42 -0500

    Update index.php

--- lilmama/index.php
+++ lilmama/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'LIL MAMA';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bajhnhbngnganioecnccbhijlngpdlfh';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
