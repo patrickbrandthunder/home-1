685351dc03bb8271c20d46b1172d14d2aa67fd21
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-13 15:28:45 -0500

    Update index.php

--- stormzy/index.php
+++ stormzy/index.php
@@ -2,8 +2,9 @@
 <?php
 $title = 'Stormzy';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lknmbidkldbpkhlpkohhociodoacmjfd';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
