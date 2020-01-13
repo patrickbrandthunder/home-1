33bd6918384d0c906c5d36c1eb462208ae1fb09b
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-13 08:54:21 -0500

    Update index.php

--- holtby/index.php
+++ holtby/index.php
@@ -3,7 +3,6 @@ $title = 'Braden Holtby';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'chmhmgjkmoaebpimfioedfiihmcpbggo';
 $searchExtensionID = '';
-$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
