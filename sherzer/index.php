748e7bdae3e550fbf6d1bef54449c143de431d18
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-13 12:00:02 -0500

    Update index.php

--- sherzer/index.php
+++ sherzer/index.php
@@ -1,7 +1,8 @@
 <?php
 $title = 'Max Sherzer';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'capiejnipbfbegejjofoiehpildpgiac';
+$bingSearch = true;
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
