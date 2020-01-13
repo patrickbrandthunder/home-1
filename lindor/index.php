a8cab4b124cd5dd396e4053ebb1d13c83ca0b6eb
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-13 10:59:57 -0500

    Update index.php

--- lindor/index.php
+++ lindor/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Francisco Lindor';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ohepmemicbebibmjkonpchmcfmoobbap';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
