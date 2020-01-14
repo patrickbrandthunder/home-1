218582e4e5774343d5c1290100db1232e7d60038
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-14 10:06:31 -0500

    Update index.php

--- unleashlight/index.php
+++ unleashlight/index.php
@@ -1,7 +1,8 @@
 <?php
 $title = 'Unleash The Light';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gadpnnhkbijhpcdebobfnhbndamiabnh';
+$bingSearch = true;
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
