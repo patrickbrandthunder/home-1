7a40766640793e82f130c8e4945f32e9c409d64d
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-14 16:14:29 -0500

    Update index.php

--- yelich/index.php
+++ yelich/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Christian Yelich';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ajmdbdihdohlolijpgbbfkookedolapm';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
