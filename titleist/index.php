67b8c3d807ba6d610bb8ee8ab41aacdb2d0f7890
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-14 16:28:27 -0500

    Update index.php

--- titleist/index.php
+++ titleist/index.php
@@ -1,8 +1,9 @@
 <?php
 $title = 'Titleist';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jgdcmlfmblmeonnamfmlflmbpdgfnkjg';
 $searchExtensionID = '';
+$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
