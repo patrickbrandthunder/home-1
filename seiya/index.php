697d4a121a9547e99332574feff7a0f7cd20fc19
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-15 18:28:56 -0500

    Update index.php

--- seiya/index.php
+++ seiya/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Saint Seiya Awakening';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'opmkmaibjkgmkchncideblmicjadfkpb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
