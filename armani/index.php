1d144ad2aceeeae65835daf955a7a937908cedb5
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-15 16:07:50 -0500

    Update index.php

--- armani/index.php
+++ armani/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Armani';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'amnkafcgdnfpcomdladkkipcbgemcaij';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
