615ce11120f6fb5dacb7983bb65cb3fd62ef5601
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-15 11:41:11 -0500

    Update index.php

--- warface/index.php
+++ warface/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Warface';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ljfhdnmidiigaondfhjobiogabfmgofn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
