0ab33383ae42efe2c34f773a90bc00b368be6db3
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-15 18:17:32 -0500

    Update index.php

--- westofdead/index.php
+++ westofdead/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'West Of Dead';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dhddhonkjmdjghkpgiocgmolafpalppd';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
