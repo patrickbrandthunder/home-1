13c160fb6232c37e397ab2b74aaa5cde726ed565
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-15 19:33:11 -0500

    Update index.php

--- lanvin/index.php
+++ lanvin/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Lanvin';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ncgenlblmcgfnjbjmgpclnhaahjekfbd';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
