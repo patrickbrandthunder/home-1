ab134c814425a59f661ee75b83e7ff9c8456f5ac
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-16 09:12:31 -0500

    Update index.php

--- tokyomirage/index.php
+++ tokyomirage/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Tokyo Mirage Sessions';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kefloekpopelpmgmjagpgcoohegaacdd';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
