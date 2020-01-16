9bd0708fff6c69287ed97c000ebacafdc9baf3bf
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-15 19:03:18 -0500

    Update index.php

--- saintlaurent/index.php
+++ saintlaurent/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Saint Laurent';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ajmojgghdbdokdddkddaoddijdnokkon';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
