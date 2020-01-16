349cc742c9414f2b321aaf0f5b0a837dfbfc9c08
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-15 21:32:21 -0500

    Update index.php

--- schitts/index.php
+++ schitts/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Schitts Creek';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'knfnjfaecpfbaipaldigknpkpocjmmhi';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
