bffa5100604cfb1356f59036a2e5472d8047bbfc
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-20 11:41:09 -0500

    Update index.php

--- afkarena/index.php
+++ afkarena/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'AFK Arena';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jkafofnjhaclmojicioobmgncebighla';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
