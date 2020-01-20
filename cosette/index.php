b732a36ae94977471d0b2d0e4bbf2871b7dac77b
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-20 11:21:01 -0500

    Update index.php

--- cosette/index.php
+++ cosette/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Cosette Rinab';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dgcibnfdgffdknecbojcconpccijoamg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
