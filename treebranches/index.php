1cd83b0feb7fde71e21c2293fc35db92343aabd6
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-20 12:23:20 -0500

    Update index.php

--- treebranches/index.php
+++ treebranches/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Tree Branches';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'anihhgblehioalncpcljkelmlbnhceic';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
