3873e54b12c6dbac2b97446c67f8c3fdc9b17f9d
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-20 11:59:19 -0500

    Update index.php

--- smifwessun/index.php
+++ smifwessun/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Smif N Wessun';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ehahegffihkgdabbhbbhmhhejmohglga';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
