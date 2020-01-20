b56fce90e0db0639706c71873925a242183b3328
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-20 11:12:30 -0500

    Update index.php

--- kingscastle/index.php
+++ kingscastle/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Kings Of The Castle';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hnlpdpdbollgpkoehnefkfkklkcckaed';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
