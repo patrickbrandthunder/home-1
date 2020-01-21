afcee3589fe6611012f4bca85052e85062f5a336
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-21 10:37:07 -0500

    Update index.php

--- rulessurvival/index.php
+++ rulessurvival/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Rules of Survival';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'abcopobdnkeomcmomoffkcdbfnhfhekf';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
