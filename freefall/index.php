aca30af7ed5585c522f7217cb258e62dd2217140
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-20 20:45:25 -0500

    Update index.php

--- freefall/index.php
+++ freefall/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Frozen Free Fall';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kpiinpogckhkeeifahnnhpencncaillj';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
