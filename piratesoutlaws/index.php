09762c6b323348edebf1c45c1dc7e7548f1bcea5
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-21 17:57:45 -0500

    Update index.php

--- piratesoutlaws/index.php
+++ piratesoutlaws/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Pirates Outlaws';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'lkihjenkgkhjkemfomfajilmolblkncm';
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
