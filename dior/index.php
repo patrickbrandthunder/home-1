2758ae632da57a9a4127461314d2892abc24d0f7
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-23 13:18:06 -0500

    Update index.php

--- dior/index.php
+++ dior/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Christian Dior';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'dogofhddjfacalkjeonlohlnalilghad';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
