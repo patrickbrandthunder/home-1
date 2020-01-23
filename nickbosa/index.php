db81902dff7545456df0eb608e9d1295287fceaf
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-23 12:56:31 -0500

    Update index.php

--- nickbosa/index.php
+++ nickbosa/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Nick Bosa';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'oaianiicfmjbcioklelppljnanejffhg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
