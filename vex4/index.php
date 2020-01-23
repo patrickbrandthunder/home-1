c01c0193808c1a7912fdeea678737c6ce02a1351
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-23 09:47:52 -0500

    Create index.php

---
+++ vex4/index.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'VEX 4';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('.
