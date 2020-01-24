7c4b1486255844429f6984f00f70c1a12b5cc5e4
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-24 11:53:29 -0500

    Update index.php

--- kareemhunt/index.php
+++ kareemhunt/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Kareem Hunt';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ejjdngmfchdijcnbjjgakbbdgchmlioa';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
