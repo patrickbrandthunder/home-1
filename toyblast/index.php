f8adf7260a7536290047140d6136896438fa57d0
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-24 15:46:06 -0500

    Update index.php

--- toyblast/index.php
+++ toyblast/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Toy Blast';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fiablblhbfonjklihfoeehlajegjfjld';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
