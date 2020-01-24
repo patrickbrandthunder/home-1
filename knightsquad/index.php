93208670e6697950158986ac68d5ed0a99eec52c
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-24 15:11:51 -0500

    Update index.php

--- knightsquad/index.php
+++ knightsquad/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Knight Squad';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kampaicmmnlmgbgdkpcacnngbepjnkga';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
