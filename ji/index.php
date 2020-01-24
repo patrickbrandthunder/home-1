713cd4b5bda1736aea3e2b38be33d6f2406de186
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-24 15:40:26 -0500

    Update index.php

--- ji/index.php
+++ ji/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'JI';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cbdihcilnjimjgjleghokjppgnkehcpg';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
