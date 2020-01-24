df9bf47506fd6a3670277d5bcc7fa1bb95c8e82e
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-24 11:01:49 -0500

    Create index.php

---
+++ kareemhunt/index.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'Kareem Hunt';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
