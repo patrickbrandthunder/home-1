eec1fcb545c1bb79ffa88121d807bb5efd70de2f
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-24 10:49:38 -0500

    Update index.php

--- machado/index.php
+++ machado/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Manny Machado';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'fbfhmimnjjjclofcghhkligfilblhfie';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
