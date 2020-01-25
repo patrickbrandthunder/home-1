86c80d9a51ef33c0291e045279516f5c6517ed97
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-25 11:57:51 -0500

    Update index.php

--- trollface/index.php
+++ trollface/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'TrollFace Quest Horror 2';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kicpidocjcbilomonbeojncljcdemjke';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
