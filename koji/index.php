51be607953d35fde98dd1a19ed92f7122c9cc4fc
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-25 11:00:16 -0500

    Update index.php

--- koji/index.php
+++ koji/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Koji NISHIDA';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'nolppfnlcffonbjehangeofbckmodpad';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
