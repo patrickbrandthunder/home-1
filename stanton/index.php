b98142e9e079984b337f28463cf9422c8bd86547
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-25 10:39:49 -0500

    Update index.php

--- stanton/index.php
+++ stanton/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Giancarlo Stanton';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ggmodgbbjbiimcgehfngbeggcagnngek';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
