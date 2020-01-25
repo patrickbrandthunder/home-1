da8bbd36d725cc7a6c9e74528db805b07a3f8ab3
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-25 11:20:12 -0500

    Update index.php

--- justblack/index.php
+++ justblack/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Just Black';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pbpbgbonaiahikcnidkkegngakpbcpge';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
