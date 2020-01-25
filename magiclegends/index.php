50d29e81c5534bdd5078778ee86163e5a886bd18
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-25 11:40:04 -0500

    Update index.php

--- magiclegends/index.php
+++ magiclegends/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Magic Legends';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'jbkekkeickoaoaiphonbclnfhedmkphn';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
