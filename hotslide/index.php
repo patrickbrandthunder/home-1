172651236eae4fde23696c1d20956e13f884c63d
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-25 12:57:22 -0500

    Update index.php

--- hotslide/index.php
+++ hotslide/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Hot Slide';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'kicnelggifjpihnacnijgpbngkollpea';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
