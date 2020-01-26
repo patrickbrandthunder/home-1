32ddafc39ae4de3a53d31a2dbf98815e6d40a71d
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-26 08:04:59 -0500

    Update index.php

--- vex4/index.php
+++ vex4/index.php
@@ -1,7 +1,8 @@
 <?php
 $title = 'VEX 4';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = 'noghdnkkeahehapbalakimenanflnnbj';
+$extensionID = '';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
-include('.
+include('../homepage.php');
+?>
