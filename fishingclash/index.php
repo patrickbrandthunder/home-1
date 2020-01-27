1f7fb880a584e06f157b16f134baab1c214c723c
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-27 18:28:39 -0500

    Update index.php

--- fishingclash/index.php
+++ fishingclash/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Fishing Clash';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'pdopodkdnlpghclnfpledhlkohhclapb';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
