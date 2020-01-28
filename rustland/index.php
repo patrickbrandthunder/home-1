da7a1710706926bee4c5dff17b41c39746118401
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-28 11:52:07 -0500

    Update index.php

--- rustland/index.php
+++ rustland/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'RUSTLAND GRAND PRIX';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'ddjooopeniediedodgcdmmbgdleaepga';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
