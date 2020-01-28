a3dcbe48d03fe68d36d89221f440107942fb33d4
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-27 19:53:27 -0500

    Update index.php

--- basquiat/index.php
+++ basquiat/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Basquiat';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'hpeeapploafhpaojmpaijpkjaelapfog';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
