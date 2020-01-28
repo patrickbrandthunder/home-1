db494e609b75f940b62d89edb94b4d29f70ff4a6
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-28 10:30:44 -0500

    Create index.php

---
+++ 2020rat/index.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'Chinese New Year 2020 Rat';
+// After extension is loaded to the Chrome store, place the extension ID here.
+$extensionID = '';
+$searchExtensionID = '';
+$tid = basename(__DIR__);
+include('../homepage.php');
+?>
