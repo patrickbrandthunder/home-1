e19280e9982bd0c73de7c3f1541120065c9ee9da
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-30 09:46:46 -0500

    Update index.php

--- altuve/index.php
+++ altuve/index.php
@@ -3,7 +3,6 @@ $title = 'Jose Altuve';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'pbneikbefjgonjkbffofpceinchiknjk';
 $searchExtensionID = '';
-$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
