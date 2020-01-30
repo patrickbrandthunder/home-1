eb3836a0d37baad6a35c1654d2d3fd3818655560
pjmurphy777 <pjmurphy77@gmail.com>
2020-01-30 09:44:21 -0500

    Update index.php

--- legobuilder/index.php
+++ legobuilder/index.php
@@ -3,7 +3,6 @@ $title = 'LEGO Builders Journey';
 // After extension is loaded to the Chrome store, place the extension ID here.
 $extensionID = 'ghdhlgfbjccedgelbnkbmbnhelfkffna';
 $searchExtensionID = '';
-$bingSearch = true;
 $tid = basename(__DIR__);
 include('../homepage.php');
 ?>
